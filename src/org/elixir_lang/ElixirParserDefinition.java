59e3622a2e6b1a6f4713ec4f8f754794d5d43df0
Luke Imhoff <Kronic.Deth@gmail.com>
2014-08-02 18:25:13 -0500

    ParserDefinition

---
+++ src/org/elixir_lang/ElixirParserDefinition.java
@@ -0,0 +1,75 @@
+package org.elixir_lang;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.Language;
+import com.intellij.lang.ParserDefinition;
+import com.intellij.lang.PsiParser;
+import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IFileElementType;
+import com.intellij.psi.tree.TokenSet;
+import org.elixir_lang.parser.ElixirParser;
+import org.elixir_lang.psi.ElixirFile;
+import org.elixir_lang.psi.ElixirTypes;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Reader;
+
+/**
+ * Created by luke.imhoff on 8/2/14.
+ */
+public class ElixirParserDefinition implements ParserDefinition {
+    public static final TokenSet COMMENTS = TokenSet.create(ElixirTypes.COMMENT);
+    public static final TokenSet WHITE_SPACES = TokenSet.create(TokenType.WHITE_SPACE);
+
+    public static final IFileElementType FILE = new IFileElementType(Language.<ElixirLanguage>findInstance(ElixirLanguage.class));
+
+    @NotNull
+    @Override
+    public Lexer createLexer(Project project) {
+        return new FlexAdapter(new ElixirLexer((Reader) null));
+    }
+
+    @NotNull
+    public TokenSet getWhitespaceTokens() {
+        return WHITE_SPACES;
+    }
+
+    @NotNull
+    public TokenSet getCommentTokens() {
+        return COMMENTS;
+    }
+
+    @NotNull
+    public TokenSet getStringLiteralElements() {
+        return TokenSet.EMPTY;
+    }
+
+    @NotNull
+    public PsiParser createParser(final Project project) {
+        return new ElixirParser();
+    }
+
+    @Override
+    public IFileElementType getFileNodeType() {
+        return FILE;
+    }
+
+    public PsiFile createFile(FileViewProvider viewProvider) {
+        return new ElixirFile(viewProvider);
+    }
+
+    public SpaceRequirements spaceExistanceTypeBetweenTokens(ASTNode left, ASTNode right) {
+        return SpaceRequirements.MAY;
+    }
+
+    @NotNull
+    public PsiElement createElement(ASTNode node) {
+        return ElixirTypes.Factory.createElement(node);
+    }
+}
