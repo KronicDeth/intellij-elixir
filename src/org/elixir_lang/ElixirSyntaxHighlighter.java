b9114ae4133410cf05eac0b7ea89bf4debf052b3
Luke Imhoff <Kronic.Deth@gmail.com>
2014-08-02 19:53:40 -0500

    SyntaxHighlighter

---
+++ src/org/elixir_lang/ElixirSyntaxHighlighter.java
@@ -0,0 +1,55 @@
+package org.elixir_lang;
+
+import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import org.elixir_lang.psi.ElixirTypes;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.io.Reader;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+/**
+ * Created by luke.imhoff on 8/2/14.
+ */
+public class ElixirSyntaxHighlighter extends SyntaxHighlighterBase {
+    public static final TextAttributesKey COMMENT = createTextAttributesKey(
+            "ELIXIR_COMMENT",
+            DefaultLanguageHighlighterColors.LINE_COMMENT
+    );
+
+    static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey(
+            "ELIXIR_BAD_CHARACTER",
+            HighlighterColors.BAD_CHARACTER
+    );
+
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @NotNull
+    @Override
+    public Lexer getHighlightingLexer() {
+        return new FlexAdapter(new ElixirLexer((Reader) null));
+    }
+
+    @NotNull
+    @Override
+    public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+        if (tokenType.equals(ElixirTypes.COMMENT)) {
+            return COMMENT_KEYS;
+        } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHAR_KEYS;
+        } else {
+            return EMPTY_KEYS;
+        }
+    }
+}
