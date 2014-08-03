bcba843bfb4cd819ce4afc8b028a494ae2bca9b5
Luke Imhoff <Kronic.Deth@gmail.com>
2014-08-02 19:58:36 -0500

    SyntaxHighlighterFactory

---
+++ src/org/elixir_lang/ElixirSyntaxHighlighterFactory.java
@@ -0,0 +1,18 @@
+package org.elixir_lang;
+
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by luke.imhoff on 8/2/14.
+ */
+public class ElixirSyntaxHighlighterFactory extends SyntaxHighlighterFactory {
+    @NotNull
+    @Override
+    public SyntaxHighlighter getSyntaxHighlighter(Project project, VirtualFile virtualFile) {
+        return new ElixirSyntaxHighlighter();
+    }
+}
