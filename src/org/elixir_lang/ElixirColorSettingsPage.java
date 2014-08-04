5930740fd22d57767a1a9b9101c63df49a8a8bfa
Luke Imhoff <Kronic.Deth@gmail.com>
2014-08-03 23:27:21 -0500

    Color Settings Page
    Custom the colors for Comments and Numbers.

---
+++ src/org/elixir_lang/ElixirColorSettingsPage.java
@@ -0,0 +1,70 @@
+package org.elixir_lang;
+
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Map;
+
+/**
+ * Created by luke.imhoff on 8/3/14.
+ */
+public class ElixirColorSettingsPage implements ColorSettingsPage {
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
+            new AttributesDescriptor("Comment", ElixirSyntaxHighlighter.COMMENT),
+            new AttributesDescriptor("Number", ElixirSyntaxHighlighter.NUMBER)
+    };
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return ElixirIcon.FILE;
+    }
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getHighlighter() {
+        return new ElixirSyntaxHighlighter();
+    }
+
+    @NotNull
+    @Override
+    public String getDemoText() {
+        return "# Only non-base-10 numbers (and these '#' comments) are supported right now.\n" +
+                "0b10 # binary\n" +
+                "0B10 # deprecated binary\n" +
+                "0o12345670 # octal\n" +
+                "012345670 # deprecated octal\n" +
+                "0x1234567890abcdefABCDEF # hexadecimal\n" +
+                "0X1234567890abcdefABCDEF # deprecated hexadecimal\n";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public AttributesDescriptor[] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @NotNull
+    @Override
+    public ColorDescriptor[] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "Elixir";
+    }
+}
