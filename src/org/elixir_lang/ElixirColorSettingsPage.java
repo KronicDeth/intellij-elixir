322216d5d3e86db8452af4db5cccaf36a53be112
Luke Imhoff <Kronic.Deth@gmail.com>
2014-10-16 18:14:08 -0500

    Use pygment example_elixir.ex.ex for Color Settings Page demo text
    Add copy of pygment's example_elixir.ex.ex to the resources in `res` and
    load it for the Color Settings page as it contains a test case for all
    valid syntax according to @alco.

--- src/org/elixir_lang/ElixirColorSettingsPage.java
+++ src/org/elixir_lang/ElixirColorSettingsPage.java
@@ -9,7 +9,11 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+
+import static com.intellij.openapi.util.io.FileUtil.loadTextAndClose;
 
 /**
  * Created by luke.imhoff on 8/3/14.
@@ -41,65 +45,16 @@ public class ElixirColorSettingsPage implements ColorSettingsPage {
     @NotNull
     @Override
     public String getDemoText() {
-        return "0b10 # binary\n" +
-                "0B10 # deprecated binary\n" +
-                "0o12345670 # octal\n" +
-                "012345670 # deprecated octal\n" +
-                "0x1234567890abcdefABCDEF # hexadecimal\n" +
-                "0X1234567890abcdefABCDEF # deprecated hexadecimal\n" +
-                ":literal_atom\n" +
-                ":\"double #{\"quoted\"} atom\"\n" +
-                ":'single #{\'quoted\'} atom'\n" +
-                "# operator atoms\n" +
-                ":|>\n" +
-                "'This CharList\\'s body contains #{'interpolation'} and \\#{escapes}'\n" +
-                "  '''\n" +
-                "  This CharList Heredoc's body contains #{'interpolation'} and \\#{escapes}\n" +
-                "  '''\n" +
-                "\"This String's body contains #{\"interpolation\"} and \\#{escapes}\"\n" +
-                "   \"\"\"\n" +
-                "   This String Heredoc's body contains #{\"interpolation\"} and \\#{escapes}\n" +
-                "   \"\"\"\n" +
-                "~c(CharList with #{\"interpolation\"})\n" +
-                "  ~c\"\"\"\n" +
-                "  CharList heredoc with #{\"interpolation\"}\n" +
-                "  \"\"\"\n" +
-                "~C/CharList without #{\"interpolation\"}/\n" +
-                "  ~C\"\"\"\n" +
-                "  CharList heredoc without #{\"interpolation\"}\n" +
-                "  \"\"\"\n" +
-                "~r{Regex with #{\"interpolation\"}\n" +
-                "  ~r\"\"\"\n" +
-                "  Regex heredoc with #{\"interpolation\"}\n" +
-                "  \"\"\"\n" +
-                "~R[Regex without #{\"interpolation\"}]\n" +
-                "  ~R'''\n" +
-                "  Regex heredoc without #{\"interpolation\"}\n" +
-                "  '''\n" +
-                "~s<String with #{\"interpolation\"}>\n" +
-                "  ~s'''\n" +
-                "  String heredoc with #{\"interpolation\"}\n" +
-                "  '''\n" +
-                "~S|String without #{\"interpolation\"}|\n" +
-                "  ~S'''\n" +
-                "  String heredoc without #{\"interpolation\"}\n" +
-                "  '''\n" +
-                "~w'Words with #{\"interpolation\"}'\n" +
-                "  ~w'''\n" +
-                "  Words heredoc with #{\"interpolation\"}\n" +
-                "  '''\n" +
-                "~W\"Words without #{'interpolation'}\"\n" +
-                "  ~W'''\n" +
-                "  Words heredoc without #{\"interpolation\"}\n" +
-                "  '''\n" +
-                "~x'Sigil with #{\"interpolation\"}'\n" +
-                "  ~x'''\n" +
-                "  Sigil heredoc with #{\"interpolation\"}\n" +
-                "  '''\n" +
-                "~X{Sigil without #{'interpolation'}}\n" +
-                "  ~X'''\n" +
-                "  Sigil heredoc without #{\"interpolation\"}\n" +
-                "  '''";
+        InputStream demoTextInputStream = getClass().getResourceAsStream("/Color Settings Page Demo Text.ex");
+        String demoText;
+
+        try {
+            demoText = loadTextAndClose(demoTextInputStream);
+        } catch (IOException e) {
+            demoText = "";
+        }
+
+        return demoText;
     }
 
     @Nullable
