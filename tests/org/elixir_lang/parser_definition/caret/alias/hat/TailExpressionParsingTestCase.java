7699537f6af14e27c8d9716f3e5accd569d33329
Luke Imhoff <Kronic.Deth@gmail.com>
2014-12-28 17:22:05 -0600

    caretAliasHatTailExpression tests

---
+++ tests/org/elixir_lang/parser_definition/caret/alias/hat/TailExpressionParsingTestCase.java
@@ -0,0 +1,19 @@
+package org.elixir_lang.parser_definition.caret.alias.hat;
+
+/**
+ * Created by luke.imhoff on 12/26/14.
+ */
+public class TailExpressionParsingTestCase extends ParsingTestCase {
+    public void testEmptyParentheses() {
+        doTest(true);
+    }
+
+    public void testNoParenthesesCall() {
+        doTest(true);
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/tail_expression_parsing_test_case";
+    }
+}
