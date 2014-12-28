0bddc20f0d5fcfff5368b515a721df24ae488377
Luke Imhoff <Kronic.Deth@gmail.com>
2014-12-28 17:32:25 -0600

    caretAliasHatMatchedExpression tests

---
+++ testData/org/elixir_lang/parser_definition/caret/alias/hat/matched_expression_parsing_test_case/CharList.ex
@@ -0,0 +1,2 @@
+# ^^^(^(Alias), 'CharList')
+^Alias ^^^ 'CharList'
---
+++ tests/org/elixir_lang/parser_definition/caret/alias/hat/MatchedExpressionParsingTestCase.java
@@ -0,0 +1,67 @@
+package org.elixir_lang.parser_definition.caret.alias.hat;
+
+/**
+ * Created by luke.imhoff on 12/24/14.
+ */
+public class MatchedExpressionParsingTestCase extends ParsingTestCase {
+    public void testAlias() {
+        doTest(true);
+    }
+
+    public void testCharList() {
+        doTest(true);
+    }
+
+    public void testCharListHeredoc() {
+        doTest(true);
+    }
+
+    public void testCharToken() {
+        doTest(true);
+    }
+
+    public void testEmptyBlock() {
+        doTest(true);
+    }
+
+    public void testFalse() {
+        doTest(true);
+    }
+
+    public void testIdentifier() {
+        doTest(true);
+    }
+
+    public void testList() {
+        doTest(true);
+    }
+
+    public void testNil() {
+        doTest(true);
+    }
+
+    public void testNumber() {
+        doTest(true);
+    }
+
+    public void testSigil() {
+        doTest(true);
+    }
+
+    public void testString() {
+        doTest(true);
+    }
+
+    public void testStringHeredoc() {
+        doTest(true);
+    }
+
+    public void testTrue() {
+        doTest(true);
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/matched_expression_parsing_test_case";
+    }
+}
