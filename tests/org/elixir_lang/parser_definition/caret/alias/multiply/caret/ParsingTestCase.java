f123151644e18794dba98d913701e141140529e6
Luke Imhoff <Kronic.Deth@gmail.com>
2014-12-29 07:24:01 -0600

    caretAliasMultiplyCaretAliasMultiplyCaretTailExpression tests

---
+++ tests/org/elixir_lang/parser_definition/caret/alias/multiply/caret/ParsingTestCase.java
@@ -0,0 +1,8 @@
+package org.elixir_lang.parser_definition.caret.alias.multiply.caret;
+
+public abstract class ParsingTestCase extends org.elixir_lang.parser_definition.caret.alias.multiply.ParsingTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/caret";
+    }
+}
