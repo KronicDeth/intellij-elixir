f9b7938bcadfc33fdfdcd8c1a57d72716b732faa
Luke Imhoff <Kronic.Deth@gmail.com>
2015-01-04 12:28:28 -0600

    Update CharListHeredoc testData

--- tests/org/elixir_lang/parser_definition/BinaryWholeNumberParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/BinaryWholeNumberParsingTestCase.java
@@ -29,7 +29,7 @@ public class BinaryWholeNumberParsingTestCase extends ParsingTestCase {
     }
 
     public void testObsoleteValidDigits() {
-        doTest(true);
+        assertParsedAndQuotedAroundError();
     }
 
     public void testValidDigits() {
