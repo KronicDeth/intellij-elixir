9e28bd80f7918a837c4ac70418ccd56d55713ef0
Luke Imhoff <Kronic.Deth@gmail.com>
2015-01-04 20:29:53 -0600

    Refactor Quoter
    Break up long methods into smaller methods and pull up pieces common to
    "ok" and "error" returns from intellij_elixir.

--- src/org/elixir_lang/intellij_elixir/Quoter.java
+++ src/org/elixir_lang/intellij_elixir/Quoter.java
@@ -4,6 +4,7 @@ import com.ericsson.otp.erlang.*;
 import com.intellij.psi.PsiFile;
 import org.elixir_lang.IntellijElixir;
 import org.elixir_lang.psi.impl.ElixirPsiImplUtil;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -16,6 +17,8 @@ import static org.junit.Assert.assertNotNull;
  * Created by luke.imhoff on 12/31/14.
  */
 public class Quoter {
+    public static final OtpErlangAtom CODE_KEY = new OtpErlangAtom("code");
+    public static final OtpErlangAtom QUOTED_KEY = new OtpErlangAtom("quoted");
     /* remote name is Elixir.IntellijElixir.Quoter because all aliases in Elixir look like atoms prefixed with
        with Elixir. from erlang's perspective. */
     public static final String REMOTE_NAME = "Elixir.IntellijElixir.Quoter";
@@ -49,6 +52,11 @@ public class Quoter {
         }
     }
 
+    public static void assertCodeEquals(String expectedCode, OtpErlangMap quotedMessageMap) {
+        String actualCode = code(quotedMessageMap);
+        assertEquals("code mismatch", expectedCode, actualCode);
+    }
+
     public static void assertQuotedCorrectly(PsiFile file) {
         final String text = file.getText();
 
@@ -58,29 +66,28 @@ public class Quoter {
 
             OtpErlangAtom status = (OtpErlangAtom) quotedMessage.elementAt(0);
             String statusString = status.atomValue();
+            OtpErlangMap map = (OtpErlangMap) quotedMessage.elementAt(1);
+            assertCodeEquals(text, map);
+            OtpErlangObject expectedQuoted = quoted(map);
 
             if (statusString.equals("ok")) {
-                OtpErlangMap map = (OtpErlangMap) quotedMessage.elementAt(1);
-
-                OtpErlangAtom codeKey = new OtpErlangAtom("code");
-                OtpErlangBinary receivedElixirString = (OtpErlangBinary) map.get(codeKey);
-                byte[] receivedBytes = receivedElixirString.binaryValue();
-                String receivedText = new String(receivedBytes, "UTF-8");
-                assertEquals("code mismatch", text, receivedText);
-
-                OtpErlangAtom quotedKey = new OtpErlangAtom("quoted");
-                OtpErlangObject receivedQuoted = map.get(quotedKey);
-
-                OtpErlangObject quoted = ElixirPsiImplUtil.quote(file);
-
-                assertEquals(receivedQuoted, quoted);
+                OtpErlangObject actualQuoted = ElixirPsiImplUtil.quote(file);
+                assertEquals(expectedQuoted, actualQuoted);
             } else if (statusString.equals("error")) {
-                OtpErlangTuple error = (OtpErlangTuple) quotedMessage.elementAt(1);
+                OtpErlangTuple error = (OtpErlangTuple) expectedQuoted;
+
+                OtpErlangLong line = (OtpErlangLong) error.elementAt(0);
+
+                OtpErlangBinary messageBinary = (OtpErlangBinary) error.elementAt(1);
+                String message = javaString(messageBinary);
+
+                OtpErlangBinary tokenBinary = (OtpErlangBinary) error.elementAt(2);
+                String token = javaString(tokenBinary);
 
                 throw new AssertionError(
-                        "intellij_elixir return an error, "
-                                + error
-                                + ", use assertQuotesAroundError if error is expect in Elixir natively, but not in intellij-elixir plugin"
+                        "intellij_elixir returned " + message + " on line " + line + " due to " + token  +
+                                ", use assertQuotesAroundError if error is expect in Elixir natively, " +
+                                "but not in intellij-elixir plugin"
                 );
             }
         }
@@ -91,6 +98,15 @@ public class Quoter {
         } catch (OtpErlangExit e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Contract(pure = true)
+    @NotNull
+    public static String code(@NotNull OtpErlangMap quotedMessageMap) {
+        OtpErlangBinary actualElixirString = (OtpErlangBinary) quotedMessageMap.get(CODE_KEY);
+        byte[] actualBytes = actualElixirString.binaryValue();
+
+        return new String(actualBytes, Charset.forName("UTF-8"));
     }
 
     @NotNull
@@ -113,6 +129,11 @@ public class Quoter {
         otpMbox.send(REMOTE_NAME, IntellijElixir.REMOTE_NODE, quoteMessage);
 
         return (OtpErlangTuple) otpMbox.receive(TIMEOUT_IN_MILLISECONDS);
+    }
+
+    @NotNull
+    public static OtpErlangObject quoted(@NotNull OtpErlangMap quotedMessageMap) {
+        return quotedMessageMap.get(QUOTED_KEY);
     }
 
     public static OtpErlangObject quoteMessage(final String text, final OtpErlangPid self) {
