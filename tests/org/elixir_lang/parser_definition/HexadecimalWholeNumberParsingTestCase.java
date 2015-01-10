a1be64d551839515f5e988c572807a4124aa31f3
Luke Imhoff <Kronic.Deth@gmail.com>
2015-01-10 15:26:25 -0600

    Test quoting and parsing of valid and invalid hexadecimal digits together

--- tests/org/elixir_lang/parser_definition/HexadecimalWholeNumberParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/HexadecimalWholeNumberParsingTestCase.java
@@ -4,6 +4,10 @@ package org.elixir_lang.parser_definition;
  * Created by luke.imhoff on 8/3/14.
  */
 public class HexadecimalWholeNumberParsingTestCase extends ParsingTestCase {
+    public void testInvalidAndValidDigits() {
+        assertParsedAndQuotedAroundError();
+    }
+
     public void testNoDigits() {
         assertParsedAndQuotedAroundError();
     }
