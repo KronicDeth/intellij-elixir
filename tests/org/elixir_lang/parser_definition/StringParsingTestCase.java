a8989cb4207ab623329d93fc73ed430c5383359e
Luke Imhoff <Kronic.Deth@gmail.com>
2015-01-13 20:38:28 -0600

    Test parsing and quoting of empty string

--- tests/org/elixir_lang/parser_definition/StringParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/StringParsingTestCase.java
@@ -5,7 +5,7 @@ package org.elixir_lang.parser_definition;
  */
 public class StringParsingTestCase extends ParsingTestCase {
     public void testEmpty() {
-        doTest(true);
+        assertParsedAndQuotedCorrectly();
     }
 
     public void testEscapeSequences() {
