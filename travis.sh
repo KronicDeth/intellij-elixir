4d5f2f0bec2fb81c661fac10f151cea58f45a0a9
Luke Imhoff <Kronic.Deth@gmail.com>
2015-02-08 23:44:12 -0600

    Add path to OtpErlang.jar to ant build

--- travis.sh
+++ travis.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ./fetchIdea.sh
+\curl -sSL https://raw.githubusercontent.com/taylor/kiex/master/install | bash -s
+~/.kiex/bin/kiex install 1.0.2
+source $HOME/.kiex/elixirs/elixir-1.0.2.env
+ERLANG_LIB=`elixir -e "IO.write :code.lib_dir"`
 
 # Run the tests
-ant -f intellij-elixir.xml -Didea.home=./idea-IC -Djdk.bin=${JAVA_HOME}/bin test.modules
+ant -f intellij-elixir.xml -Derlang.lib=${ERLANG_LIB} -Didea.home=./idea-IC -Djdk.bin=${JAVA_HOME}/bin test.modules
 
 # Was our build successful?
 stat=$?
