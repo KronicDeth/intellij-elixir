9462ddd0365806e34410452852d868c21be4fa03
Luke Imhoff <Kronic.Deth@gmail.com>
2015-02-08 20:31:57 -0600

    Ignore abstract test class
    Add @Ignore to abstract test classes so that ant won't try to run them.

--- tests/org/elixir_lang/parser_definition/ParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/ParsingTestCase.java
@@ -1,10 +1,12 @@
 package org.elixir_lang.parser_definition;
 
 import org.elixir_lang.ElixirParserDefinition;
+import org.junit.Ignore;
 
 /**
  * Created by luke.imhoff on 8/7/14.
  */
+@Ignore("abstract")
 public abstract class ParsingTestCase extends com.intellij.testFramework.ParsingTestCase {
     public ParsingTestCase() {
         super("", "ex", new ElixirParserDefinition());
