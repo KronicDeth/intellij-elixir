fe55a8574b0938665963c355777414de659c00aa
Luke Imhoff <Kronic.Deth@gmail.com>
2015-02-21 22:17:31 -0600

    Merge REGEX_FRAGMENTs
    Add REGEX_FRAGMENTs to list of merged FRAGMENTS in ElixirLexer to match
    behavior of ATOM, CHAR_LIST, SIGIL, and STRING.

--- src/org/elixir_lang/ElixirLexer.java
+++ src/org/elixir_lang/ElixirLexer.java
@@ -12,6 +12,7 @@ public class ElixirLexer extends LookAheadLexer {
     public static final TokenSet FRAGMENTS = TokenSet.create(
             ElixirTypes.ATOM_FRAGMENT,
             ElixirTypes.CHAR_LIST_FRAGMENT,
+            ElixirTypes.REGEX_FRAGMENT,
             ElixirTypes.SIGIL_FRAGMENT,
             ElixirTypes.STRING_FRAGMENT
     );
