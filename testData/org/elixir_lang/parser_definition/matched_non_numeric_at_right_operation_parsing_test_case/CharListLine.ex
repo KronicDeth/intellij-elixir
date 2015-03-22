dd53bae33793d2666d0d77fa78f1f44e2c45b620
Luke Imhoff <Kronic.Deth@gmail.com>
2015-03-22 09:14:43 -0500

    Test all nested rules for matchedNonNumericAtRightOperation

---
+++ testData/org/elixir_lang/parser_definition/matched_non_numeric_at_right_operation_parsing_test_case/CharListLine.ex
@@ -0,0 +1,1 @@
+@'CharList Line'
--- tests/org/elixir_lang/parser_definition/MatchedNonNumericAtRightOperationParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/MatchedNonNumericAtRightOperationParsingTestCase.java
@@ -24,6 +24,123 @@ public class MatchedNonNumericAtRightOperationParsingTestCase extends ParsingTes
         assertParsedAndQuotedCorrectly();
     }
 
+    /*
+     * matchedNonNumericAtLeftOperand
+     */
+
+    public void testCaptureLeftOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnaryLeftOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtLeftOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * matchedOperandExpression
+     */
+
+    public void testVariable() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * accessExpression
+     */
+
+    public void testAtNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCaptureNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnaryNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testEmptyBlock() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testList() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testSigil() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtomKeyword() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtom() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAlias() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * numeric
+     */
+
+    public void testCharToken() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testBinaryWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testDecimalFloat() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testDecimalWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testOctalWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnknownBaseWholeNumber() {
+        assertParsedAndQuotedAroundError();
+    }
+
+    /*
+     * binaryString
+     */
+
+    public void testStringLine() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testStringHeredoc() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * listString
+     */
+
+    public void testCharListLine() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCharListHeredoc() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+
     @Override
     protected String getTestDataPath() {
         return super.getTestDataPath() + "/matched_non_numeric_at_right_operation_parsing_test_case";
