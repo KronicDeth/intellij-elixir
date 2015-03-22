cfe18c4362b1591d74bb0006e657d0536c3fd1f7
Luke Imhoff <Kronic.Deth@gmail.com>
2015-03-22 14:50:30 -0500

    Test noParenthesesNoArgumentsCall in matchedDotExpression up-to failure
    First failure is `@1.identifier` not working because the `1.` is being
    parsed as a broken decimal float instead of `.(@(1), identifier)`.

---
+++ tests/org/elixir_lang/parser_definition/NoParenthesesNoArgumentsCallParsingTestCase.java
@@ -0,0 +1,137 @@
+package org.elixir_lang.parser_definition;
+
+/**
+ * Created by luke.imhoff on 9/17/14.
+ */
+public class NoParenthesesNoArgumentsCallParsingTestCase extends ParsingTestCase {
+    /*
+     *
+     * Vary Qualifier
+     * 
+     */
+    
+    public void testAtOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+    
+    /*
+     * matchedNonNumericAtLeftOperand
+     */
+
+    public void testCaptureLeftOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnaryLeftOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtLeftOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * matchedOperandExpression
+     */
+
+    public void testVariableDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * accessExpression
+     */
+
+    public void testAtNumericOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCaptureNumericOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnaryNumericOperationDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testEmptyBlockDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testListDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testSigilDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtomKeywordDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtomDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAliasDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * numeric
+     */
+
+    public void testCharTokenDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testBinaryWholeNumberDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testDecimalFloatDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testDecimalWholeNumberDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testOctalWholeNumberDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnknownBaseWholeNumberDotIdentifier() {
+        assertParsedAndQuotedAroundError();
+    }
+
+    /*
+     * binaryString
+     */
+
+    public void testStringLineDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testStringHeredocDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * listString
+     */
+
+    public void testCharListLineDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCharListHeredocDotIdentifier() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/no_parentheses_no_arguments_call_parsing_test_case";
+    }
+}
