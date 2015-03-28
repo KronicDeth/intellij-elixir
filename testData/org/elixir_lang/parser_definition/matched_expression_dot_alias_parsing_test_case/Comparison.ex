a16b9c8a596f00b4843a9258cf4fd0ed84ffee5b
Luke Imhoff <Kronic.Deth@gmail.com>
2015-03-28 08:38:24 -0700

    First round matchedDotOperation testing
    A lot of the test left operands are invalid in native Elixir, which
    means a new rule is needed for left most operands, but there are also
    tests where the quoting differs between Elixir an intellij-elixir due to
    certain tokens being lexed as identifiers only when after '.'.

--- testData/org/elixir_lang/parser_definition/matched_expression_dot_alias_parsing_test_case/Comparison.ex
+++
@@ -1,2 +0,0 @@
-# invalid
-Alias.1 != 2
---
+++ tests/org/elixir_lang/parser_definition/MatchedDotOperationParsingTestcase.java
@@ -0,0 +1,116 @@
+package org.elixir_lang.parser_definition;
+
+public class MatchedDotOperationParsingTestcase extends ParsingTestCase {
+  public void testAssociativity() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * matchedDotOperand
+     */
+
+    public void testAtNonNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testMatchedCallOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testVariable() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * accessExpression
+     */
+
+    public void testAtNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCaptureNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnaryNumericOperation() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testEmptyBlock() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testList() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testSigil() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtomKeyword() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAtom() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testAlias() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    /*
+     * numeric
+     */
+
+    public void testCharToken() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testBinaryWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testHexadecimalWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testOctalWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testUnknownBaseWholeNumber() {
+        assertParsedAndQuotedAroundError();
+    }
+
+    public void testDecimalFloat() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testDecimalWholeNumber() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testStringLine() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testStringHeredoc() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCharListLine() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    public void testCharListHeredoc() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/matched_dot_operation_parsing_test_case";
+    }
+}
