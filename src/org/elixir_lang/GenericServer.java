cc0010db72b191660b5f961ad44d96ca41377abe
Luke Imhoff <Kronic.Deth@gmail.com>
2015-04-05 23:14:48 -0500

    Throw OtpErlangExit if server pid cannot be obtained
    Matches behavior of `GenServer.call nil, args`.

--- src/org/elixir_lang/GenericServer.java
+++ src/org/elixir_lang/GenericServer.java
@@ -1,6 +1,7 @@
 package org.elixir_lang;
 
 import com.ericsson.otp.erlang.*;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Emulates GenServer
@@ -11,10 +12,14 @@ public class GenericServer {
     public static OtpErlangObject call(OtpMbox callerMbox, OtpNode callerNode, String serverName, String serverNode, OtpErlangObject request, int timeout) throws OtpErlangExit, OtpErlangDecodeException {
         OtpErlangPid serverPid = Process.whereis(callerMbox, callerNode, serverName, serverNode, timeout);
 
+        if (serverPid == null) {
+            throw new OtpErlangExit("no process");
+        }
+
         return call(callerMbox,  callerNode, serverPid, request, timeout);
     }
 
-    private static OtpErlangObject call(OtpMbox callerMbox, OtpNode callerNode, OtpErlangPid serverPid, OtpErlangObject request, int timeout) throws OtpErlangExit, OtpErlangDecodeException {
+    private static OtpErlangObject call(@NotNull OtpMbox callerMbox, @NotNull OtpNode callerNode, @NotNull OtpErlangPid serverPid, @NotNull OtpErlangObject request, int timeout) throws OtpErlangExit, OtpErlangDecodeException {
         callerMbox.link(serverPid);
 
         OtpErlangObject response = unmonitoredCall(callerMbox, callerNode, serverPid, request, timeout);
