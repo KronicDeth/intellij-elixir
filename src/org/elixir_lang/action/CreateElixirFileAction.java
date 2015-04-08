c70eaba58d5bf849be8937e11c2bcbbccdac57a4
Luke Imhoff <Kronic.Deth@gmail.com>
2015-04-07 22:46:36 -0500

    Allow dotted module names for New Elixir File

--- src/org/elixir_lang/action/CreateElixirFileAction.java
+++ src/org/elixir_lang/action/CreateElixirFileAction.java
@@ -9,13 +9,19 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiDirectory;
 import org.elixir_lang.ElixirIcon;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * Provides
  */
 public class CreateElixirFileAction extends CreateFileFromTemplateAction {
+  private static final String ALIAS_REGEXP = "[A-Z][0-9a-zA-Z_]*";
+  private static final String MODULE_NAME_REGEXP = ALIAS_REGEXP + "(\\." + ALIAS_REGEXP + ")*";
+  private static final Pattern MODULE_NAME_PATTERN = Pattern.compile(MODULE_NAME_REGEXP);
   private static final String NEW_ELIXIR_FILE = "New Elixir File";
-  private static final String INVALID_MODULE_MESSAGE_FMT = "'%s' is not a valid Elixir module name";
+  private static final String INVALID_MODULE_MESSAGE_FMT = "'%s' is not a valid Elixir module name. Valid pattern for Elixir modules is dot sequences of aliases (" + MODULE_NAME_REGEXP + ")";
 
   public CreateElixirFileAction() {
     super(NEW_ELIXIR_FILE, "", ElixirIcon.FILE);
@@ -49,9 +55,12 @@ public class CreateElixirFileAction extends CreateFileFromTemplateAction {
             if (StringUtil.isEmpty(inputString))
               return null;
 
-            if (FileUtil.sanitizeFileName(inputString).equals(inputString)) {
+            Matcher matcher = MODULE_NAME_PATTERN.matcher(inputString);
+
+            if (matcher.matches()) {
               return null;
             }
+
             return String.format(INVALID_MODULE_MESSAGE_FMT, inputString);
           }
         })
