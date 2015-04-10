1c58387e6365ff34b61395c82afe1995a5070ca0
Luke Imhoff <Kronic.Deth@gmail.com>
2015-04-10 13:21:11 -0500

    Test Foo.Bar is created as foo/bar.ex

--- tests/org/elixir_lang/action/CreateElixirModuleActionTest.java
+++ tests/org/elixir_lang/action/CreateElixirModuleActionTest.java
@@ -1,6 +1,8 @@
 package org.elixir_lang.action;
 
 import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
@@ -15,21 +17,36 @@ public class CreateElixirModuleActionTest extends LightCodeInsightFixtureTestCas
     public static final String TEMPLATE_NAME = "Elixir Module";
 
     public void testCamelCaseAliasIsLowerCaseUnderscored() throws IOException {
+        checkModuleFile("FooBar", "foo_bar.ex");
+    }
+
+    public void testAliasDotAliasIsDirectorySlashFile()  throws IOException {
+        checkModuleFile("Foo.Bar", "foo/bar.ex");
+    }
+
+    private void checkModuleFile(final String moduleName, String path) throws IOException {
         ActionManager actionManager = ActionManager.getInstance();
-        CreateElixirModuleAction elixirNewFileAction = (CreateElixirModuleAction) actionManager.getAction("Elixir.NewFile");
+        final CreateElixirModuleAction elixirNewFileAction = (CreateElixirModuleAction) actionManager.getAction("Elixir.NewFile");
 
         // @see https://devnet.jetbrains.com/message/5539349#5539349
         VirtualFile directoryVirtualFile = myFixture.getTempDirFixture().findOrCreateDir("");
-        PsiDirectory directory = myFixture.getPsiManager().findDirectory(directoryVirtualFile);
+        final PsiDirectory directory = myFixture.getPsiManager().findDirectory(directoryVirtualFile);
 
-        ElixirFile file = elixirNewFileAction.createFile("FooBar", TEMPLATE_NAME, directory);
-        assertNotNull("Expected CreateElixirModuleAction.createFile to create an ElixirFile", file);
+        Application application = ApplicationManager.getApplication();
+        application.runWriteAction(new Runnable() {
+            @Override
+            public void run() {
+                ElixirFile file = elixirNewFileAction.createFile(moduleName, TEMPLATE_NAME, directory);
+                assertNotNull("Expected CreateElixirModuleAction.createFile to create an ElixirFile", file);
+
+            }
+        });
 
         boolean ignoreTrailingWhitespaces = true;
 
         myFixture.checkResultByFile(
-                "foo_bar.ex",
-                "foo_bar.ex",
+                path,
+                path,
                 ignoreTrailingWhitespaces
         );
     }
