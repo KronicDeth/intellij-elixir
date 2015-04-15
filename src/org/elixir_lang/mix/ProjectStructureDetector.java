af31fe5d5f774c10195462b574319677c8d41af0
Luke Imhoff <Kronic.Deth@gmail.com>
2015-04-14 22:40:13 -0500

    Mark src and lib directories in libraries as source roots

--- src/org/elixir_lang/mix/ProjectStructureDetector.java
+++ src/org/elixir_lang/mix/ProjectStructureDetector.java
@@ -1,10 +1,20 @@
 package org.elixir_lang.mix;
 
+import com.intellij.ide.util.importProject.LibraryDescriptor;
 import com.intellij.ide.util.importProject.ModuleDescriptor;
 import com.intellij.ide.util.importProject.ProjectDescriptor;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.ProjectJdkForModuleStep;
 import com.intellij.ide.util.projectWizard.importSources.ProjectFromSourcesBuilder;
+import com.intellij.ide.util.projectWizard.importSources.impl.ProjectFromSourcesBuilderImpl;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModifiableModelsProvider;
+import com.intellij.openapi.roots.OrderRootType;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.containers.ContainerUtil;
 import org.elixir_lang.SdkType;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +23,7 @@ import javax.swing.*;
 import java.io.File;
 import java.util.*;
 
-public class ProjectStructureDetector extends com.intellij.ide.util.projectWizard.importSources.ProjectStructureDetector {
+public class ProjectStructureDetector extends com.intellij.ide.util.projectWizard.importSources.ProjectStructureDetector implements ProjectFromSourcesBuilderImpl.ProjectConfigurationUpdater {
     @NotNull
     @Override
     public DirectoryProcessingResult detectRoots(@NotNull File dir, @NotNull File[] children, @NotNull File base, @NotNull List<com.intellij.ide.util.projectWizard.importSources.DetectedProjectRoot> result) {
@@ -53,8 +63,13 @@ public class ProjectStructureDetector extends com.intellij.ide.util.projectWizar
      */
     @Override
     public void setupProjectStructure(@NotNull Collection<com.intellij.ide.util.projectWizard.importSources.DetectedProjectRoot> roots, @NotNull ProjectDescriptor projectDescriptor, @NotNull ProjectFromSourcesBuilder builder) {
+        ProjectFromSourcesBuilderImpl builderImpl = (ProjectFromSourcesBuilderImpl) builder;
+        builderImpl.addConfigurationUpdater(this);
+
         if (!roots.isEmpty() && !builder.hasRootsFromOtherDetectors(this)) {
+            List<LibraryDescriptor> libraries = new ArrayList<LibraryDescriptor>();
             List<ModuleDescriptor> modules = projectDescriptor.getModules();
+
             if (modules.isEmpty()) {
                 modules = new ArrayList<ModuleDescriptor>(roots.size());
 
@@ -81,10 +96,57 @@ public class ProjectStructureDetector extends com.intellij.ide.util.projectWizar
                                     sourceRoots
                             )
                     );
+
+                    File deps = new File(rootDirectory, "deps");
+
+                    if (deps.isDirectory()) {
+                        for (File depChild : deps.listFiles()) {
+                            if (depChild.isDirectory()) {
+                                libraries.add(
+                                        new LibraryDescriptor(
+                                                depChild.getName(),
+                                                Collections.singletonList(depChild)
+                                        )
+                                );
+                            }
+                        }
+                    }
                 }
 
+                projectDescriptor.setLibraries(libraries);
                 projectDescriptor.setModules(modules);
             }
+        }
+    }
+
+    @Override
+    public void updateProject(@NotNull Project project, @NotNull ModifiableModelsProvider modelsProvider, @NotNull ModulesProvider modulesProvider) {
+        LibraryTable libraryTable = LibraryTablesRegistrar.getInstance().getLibraryTable(project);
+
+        for (Library library : libraryTable.getLibraries()) {
+            Library.ModifiableModel libraryModel = library.getModifiableModel();
+
+            // LibraryDescriptor 'jars' are marked as OrderRootType.CLASSES in NewProjectUtil.createFromWizard
+            VirtualFile[] rootVirtualFiles = libraryModel.getFiles(OrderRootType.CLASSES);
+
+            for (VirtualFile rootVirtualFile : rootVirtualFiles) {
+                // Remove the default assigned by NewProjectUtil.createFromWizard
+                libraryModel.removeRoot(rootVirtualFile.getUrl(), OrderRootType.CLASSES);
+
+                for (VirtualFile childVirtualFile : rootVirtualFile.getChildren()) {
+                    // mix project sources
+                    if (childVirtualFile.getName().equals("lib") && childVirtualFile.isDirectory()) {
+                        libraryModel.addRoot(childVirtualFile, OrderRootType.SOURCES);
+                    }
+
+                    // erlang sources
+                    if (childVirtualFile.getName().equals("src") && childVirtualFile.isDirectory()) {
+                        libraryModel.addRoot(childVirtualFile, OrderRootType.SOURCES);
+                    }
+                }
+            }
+
+            libraryModel.commit();
         }
     }
 }
