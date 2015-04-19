7c798c79855a20189c9f2bb772fe8973ef9a6b96
Luke Imhoff <Kronic.Deth@gmail.com>
2015-04-19 15:30:12 -0500

    Test matchedDotOperatorCallOperation with matchedCallOperation

---
+++ testData/org/elixir_lang/parser_definition/matched_dot_operator_call_operation/matched_call_operation_parsing_test_case/CharListHeredoc.ex
@@ -0,0 +1,76 @@
+'''
+CharList
+Heredoc
+'''.&& Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.|> Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.@ Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.& Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.== Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.+ Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.^^^ Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.<- Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.in Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.= Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.* Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.|| Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.| Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.<= Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.-> Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.% Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.<> Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.^ Qualifier.qualified positional, key: value
+'''
+CharList
+Heredoc
+'''.when Qualifier.qualified positional, key: value
