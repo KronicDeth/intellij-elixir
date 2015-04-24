057e8bf13520b457d08474d6f9f9b58ca7d6b40f
Luke Imhoff <Kronic.Deth@gmail.com>
2015-04-23 19:37:47 -0500

    Inspection for Keywords not at end of parenthesesArguments

---
+++ tests/org/elixir_lang/inspection/KeywordsNotAtEndTestCase.java
@@ -0,0 +1,19 @@
+package org.elixir_lang.inspection;
+
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+/**
+ * Created by luke.imhoff on 12/5/14.
+ */
+public class KeywordsNotAtEndTestCase extends LightCodeInsightFixtureTestCase {
+    public void testMatchedCallOperationInMiddle() {
+        myFixture.configureByFiles("MatchedCallOperationInMiddle.ex");
+        myFixture.enableInspections(KeywordsNotAtEnd.class);
+        myFixture.checkHighlighting();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "testData/org/elixir_lang/inspection/keywords_not_at_end_test_case";
+    }
+}
