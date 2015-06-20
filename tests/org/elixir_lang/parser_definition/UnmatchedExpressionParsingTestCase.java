4384e3563515d9c62359479f6379cc786ec0fd3b
Luke Imhoff <Kronic.Deth@gmail.com>
2015-06-20 12:45:42 -0400

    !<qualified>.<relative-identifier> p, k: v do end

--- tests/org/elixir_lang/parser_definition/UnmatchedExpressionParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/UnmatchedExpressionParsingTestCase.java
@@ -69,6 +69,10 @@ public class UnmatchedExpressionParsingTestCase extends ParsingTestCase {
         assertParsedAndQuotedCorrectly();
     }
 
+    public void testExclamationPointQualifiedNoParenthesesCallBlock() {
+        assertParsedAndQuotedCorrectly();
+    }
+
     public void testExclamationPointQualifiedParenthesesCallBlock() {
         assertParsedAndQuotedCorrectly();
     }
