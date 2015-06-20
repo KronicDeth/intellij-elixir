d3739b99ae043e49121bba0a6708f257bb1bca83
Luke Imhoff <Kronic.Deth@gmail.com>
2015-06-20 12:36:04 -0400

    !<qualifier>.<relative-identifier>() do end

--- tests/org/elixir_lang/parser_definition/UnmatchedExpressionParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/UnmatchedExpressionParsingTestCase.java
@@ -69,6 +69,10 @@ public class UnmatchedExpressionParsingTestCase extends ParsingTestCase {
         assertParsedAndQuotedCorrectly();
     }
 
+    public void testExclamationPointQualifiedParenthesesCallBlock() {
+        assertParsedAndQuotedCorrectly();
+    }
+
     public void testNotDotCallBlock() {
         assertParsedAndQuotedCorrectly();
     }
