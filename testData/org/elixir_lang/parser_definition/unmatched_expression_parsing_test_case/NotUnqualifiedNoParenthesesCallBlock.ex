4aa47bc1f194f4012a422a08d44dc7e1ae9df214
Luke Imhoff <Kronic.Deth@gmail.com>
2015-06-20 12:10:41 -0400

    not identifier p, k: v do end

---
+++ testData/org/elixir_lang/parser_definition/unmatched_expression_parsing_test_case/NotUnqualifiedNoParenthesesCallBlock.ex
@@ -0,0 +1,6 @@
+not one key: value do end
+not one two three, key: value do end
+not one two, key: value do end
+not one two, three do end
+not one two + three do end
+not one two three do end
--- tests/org/elixir_lang/parser_definition/UnmatchedExpressionParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/UnmatchedExpressionParsingTestCase.java
@@ -77,6 +77,10 @@ public class UnmatchedExpressionParsingTestCase extends ParsingTestCase {
         assertParsedAndQuotedCorrectly();
     }
 
+    public void testNotUnqualifiedNoParenthesesCallBlock() {
+        assertParsedAndQuotedCorrectly();
+    }
+
     public void testQualifiedNoArgumentsCallBlock() {
         assertParsedAndQuotedCorrectly();
     }
