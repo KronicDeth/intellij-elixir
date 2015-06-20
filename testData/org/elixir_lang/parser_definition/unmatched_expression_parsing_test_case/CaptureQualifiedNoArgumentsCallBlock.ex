d926e73e378e119be2052696f1f35d253f8c1ff7
Luke Imhoff <Kronic.Deth@gmail.com>
2015-06-20 07:15:42 -0400

    fixup! &<qualifier>.<relative-identifier> do end

--- testData/org/elixir_lang/parser_definition/unmatched_expression_parsing_test_case/CaptureQualifiedNoArgumentsCallBlock.ex
+++ testData/org/elixir_lang/parser_definition/unmatched_expression_parsing_test_case/CaptureQualifiedNoArgumentsCallBlock.ex
@@ -1,69 +1,66 @@
-@one[two].three do end
-@one[two].three.four do end
+&@one[two].three do end
+&@one[two].three.four do end
 
-@one.two do end
-@one.two.three do end
+&@one.two do end
+&@one.two.three do end
 
-one()().two do end
-one()().two.three do end
+&one()().two do end
+&one()().two.three do end
 
-one[two].three do end
-one[two].three.four do end
+&one[two].three do end
+&one[two].three.four do end
 
-one.two do end
-one.two.three do end
+&one.two do end
+&one.two.three do end
 
-@1.two do end
-@1.two.three do end
+&@1.two do end
+&@1.two.three do end
+
+&^1.two do end
+&^1.two.three do end
+
+&fn one end.two do end
+&fn one end.two.three do end
+
+&(->).one do end
+&(->).one.two do end
 
 &1.two do end
 &1.two.three do end
 
-^1.two do end
-^1.two.three do end
+&[].one do end
+&[].one.two do end
 
-fn one end.two do end
-fn one end.two.three do end
+&%{}.one do end
+&%{}.one.two do end
 
-(->).one do end
-(->).one.two do end
+&{}.one do end
+&{}.one.two do end
 
-1.two do end
-1.two.three do end
+&<<>>.one do end
+&<<>>.one.two do end
 
-[].one do end
-[].one.two do end
+&"one".two do end
+&"one".two.three do end
 
-%{}.one do end
-%{}.one.two do end
+&"""
+ one
+ """.two do end
+&"""
+ one
+ """.two.three do end
 
-{}.one do end
-{}.one.two do end
+&'one'.two do end
+&'one'.two.three do end
 
-<<>>.one do end
-<<>>.one.two do end
+&~x{one}.two do end
+&~x{one}.two.three do end
 
-"one".two do end
-"one".two.three do end
+&false.one do end
+&false.one.two do end
 
-"""
-one
-""".two do end
-"""
-one
-""".two.three do end
+&:one.two do end
+&:one.two.three do end
 
-'one'.two do end
-'one'.two.three do end
-
-~x{one}.two do end
-~x{one}.two.three do end
-
-false.one do end
-false.one.two do end
-
-:one.two do end
-:one.two.three do end
-
-One.two do end
-One.two.three do end
+&One.two do end
+&One.two.three do end
