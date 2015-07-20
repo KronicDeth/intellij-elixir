fa7006a4613af8517ed8152501e5fdde90ef14f7
Luke Imhoff <Kronic.Deth@gmail.com>
2015-07-19 22:25:49 -0500

    Test parallel_compiler/bat.ex as expected error
    Fixes #165
    
    Expand ElixirLangElixirParsingTestCase to allow look-up of whether the
    parse is expected to be correct or an error.

--- tests/org/elixir_lang/parser_definition/ElixirLangElixirParsingTestCase.java
+++ tests/org/elixir_lang/parser_definition/ElixirLangElixirParsingTestCase.java
@@ -3,39 +3,65 @@ package org.elixir_lang.parser_definition;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.vfs.CharsetToolkit;
-import org.elixir_lang.ElixirLanguage;
+import org.elixir_lang.intellij_elixir.Quoter;
 import org.jetbrains.annotations.NotNull;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Queue;
+import java.net.URI;
+import java.util.*;
 
 @RunWith(Parameterized.class)
 public class ElixirLangElixirParsingTestCase extends ParsingTestCase {
+    private enum Parse {
+        ERROR("with local and remote error"),
+        CORRECT("and quotes correctly");
+
+        /*
+         * Fields
+         */
+
+        private final String description;
+
+        Parse(String description) {
+            this.description = description;
+        }
+
+        public String toString() {
+            return description;
+        }
+    }
+
+    private static Map<String, Parse> parseByRelativePath;
+    static {
+        parseByRelativePath = new HashMap<String, Parse>();
+        parseByRelativePath.put(
+                "lib/elixir/test/elixir/fixtures/parallel_compiler/bat.ex",
+                Parse.ERROR
+        );
+    }
+
     /*
      * Fields
      */
 
     private File elixirFile;
+    private Parse parse;
 
     /*
      * Constructors
      */
-    public ElixirLangElixirParsingTestCase(File elixirFile) {
+    public ElixirLangElixirParsingTestCase(File elixirFile, Parse parse) {
         this.elixirFile = elixirFile;
+        this.parse = parse;
     }
 
     @Parameterized.Parameters(
-            name = "\"#{0}\" parses and quotes correctly"
+            name = "\"#{0}\" parses #{1}"
     )
-    public static Collection<File[]> generateData() {
+    public static Collection<Object[]> generateData() {
         String rootPath = System.getenv("ELIXIR_LANG_ELIXIR_PATH");
 
         assertNotNull("ELIXIR_LANG_ELIXIR_PATH environment variable not set", rootPath);
@@ -45,7 +71,7 @@ public class ElixirLangElixirParsingTestCase extends ParsingTestCase {
         Queue<File> treeQueue = new ArrayDeque<File>();
         treeQueue.add(rootFile);
 
-        Queue<File[]> elixirFileDeque = new ArrayDeque<File[]>();
+        Queue<Object[]> argumentQueue = new ArrayDeque<Object[]>();
 
         while(!treeQueue.isEmpty()) {
             File treeFile = treeQueue.remove();
@@ -64,15 +90,34 @@ public class ElixirLangElixirParsingTestCase extends ParsingTestCase {
                     treeQueue.add(childFile);
                 }
             } else if (FileUtilRt.extensionEquals(treeFile.toString(), "ex")) {
-                elixirFileDeque.add(new File[]{treeFile});
+                Object[] arguments = new Object[] {
+                        treeFile,
+                        parse(rootFile, treeFile)
+                };
+                argumentQueue.add(arguments);
             }
         }
 
-        return elixirFileDeque;
+        return argumentQueue;
+    }
+
+    private static Parse parse(File rootFile, File descendantFile) {
+        URI rootURI = rootFile.toURI();
+        URI descendantURI = descendantFile.toURI();
+
+        String relativePath = rootURI.relativize(descendantURI).getPath();
+
+        Parse relativePathParse = parseByRelativePath.get(relativePath);
+
+        if (relativePathParse == null) {
+            relativePathParse = Parse.CORRECT;
+        }
+
+        return relativePathParse;
     }
 
     @org.junit.Test
-    public void parsedAndQuotedCorrectly() throws Exception {
+    public void parse() throws Exception {
         // this does not fire automatically
         super.setUp();
 
@@ -88,8 +133,15 @@ public class ElixirLangElixirParsingTestCase extends ParsingTestCase {
             throw new RuntimeException(ioException);
         }
 
-        assertWithoutLocalError();
-        assertQuotedCorrectly();
+        switch (parse) {
+            case CORRECT:
+                assertWithoutLocalError();
+                assertQuotedCorrectly();
+                break;
+            case ERROR:
+                assertWithLocalError();
+                Quoter.assertError(myFile);
+        }
     }
 
     @Override
