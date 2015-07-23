56fc5668d4dd75be908c589fbcb0099c83449747
Luke Imhoff <Kronic.Deth@gmail.com>
2015-07-22 20:34:24 -0500

    Red quoting for `one (<dual>two)`
    Missing `ambiguous: nil`.

---
+++ tests/org/elixir_lang/parser_definition/NoParenthesesOneArgumentCallParsingTestCase.java
@@ -0,0 +1,15 @@
+package org.elixir_lang.parser_definition;
+
+/**
+ * Created by luke.imhoff on 9/17/14.
+ */
+public class NoParenthesesOneArgumentCallParsingTestCase extends ParsingTestCase {
+    public void testUnqualifiedSpaceOpeningParenthesisDualVariableClosingParentheses() {
+        assertParsedAndQuotedCorrectly();
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/no_parentheses_one_argument_call_parsing_test_case";
+    }
+}
