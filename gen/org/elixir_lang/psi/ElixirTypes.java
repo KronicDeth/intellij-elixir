087ca4f7eaf7a2cd27f9d24901aa2c0cb73e8271
Luke Imhoff <Kronic.Deth@gmail.com>
2015-07-26 14:12:12 -0500

    Name CHAR_LIST_TERMINATOR and rename CHAR_LIST_PROMOTER
    CHAR_LIST_PROMOTER was just "'", but that is the token for
    CHAR_LIST_TERMINATOR too, so since a name can only be used once, use the
    "<human-readable> (<tokens>)" format for both CHAR_LIST_TERMINATOR and
    CHAR_LIST_PROMOTER.

--- gen/org/elixir_lang/psi/ElixirTypes.java
+++ gen/org/elixir_lang/psi/ElixirTypes.java
@@ -238,12 +238,12 @@ public interface ElixirTypes {
   IElementType CHAR_LIST_FRAGMENT = new ElixirTokenType("CHAR_LIST_FRAGMENT");
   IElementType CHAR_LIST_HEREDOC_PROMOTER = new ElixirTokenType("Char List Heredoc Promoter (''')");
   IElementType CHAR_LIST_HEREDOC_TERMINATOR = new ElixirTokenType("Char List Heredoc Terminator (''')");
-  IElementType CHAR_LIST_PROMOTER = new ElixirTokenType("'");
+  IElementType CHAR_LIST_PROMOTER = new ElixirTokenType("Char List Promoter (')");
   IElementType CHAR_LIST_SIGIL_HEREDOC_PROMOTER = new ElixirTokenType("Char List Sigil Heredoc Promoter (\"\"\", ''')");
   IElementType CHAR_LIST_SIGIL_HEREDOC_TERMINATOR = new ElixirTokenType("Char List Sigil Heredoc Terminator (\"\"\", ''')");
   IElementType CHAR_LIST_SIGIL_PROMOTER = new ElixirTokenType("Char List Sigil Promoter ({, [, <, \", /, (, |, ')");
   IElementType CHAR_LIST_SIGIL_TERMINATOR = new ElixirTokenType("Char List Sigil Terminator ({, [, <, \", /, (, |, ')");
-  IElementType CHAR_LIST_TERMINATOR = new ElixirTokenType("CHAR_LIST_TERMINATOR");
+  IElementType CHAR_LIST_TERMINATOR = new ElixirTokenType("Char List Terminator (')");
   IElementType CHAR_TOKENIZER = new ElixirTokenType("?");
   IElementType CLOSING_BIT = new ElixirTokenType(">>");
   IElementType CLOSING_BRACKET = new ElixirTokenType("]");
