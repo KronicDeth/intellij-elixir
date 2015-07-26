04e69390a7d0e6edebe7e91394badc73d0be0a58
Luke Imhoff <Kronic.Deth@gmail.com>
2015-07-25 21:07:46 -0500

    Name INVALID_OCTAL_DIGITS

--- gen/org/elixir_lang/psi/ElixirTypes.java
+++ gen/org/elixir_lang/psi/ElixirTypes.java
@@ -281,7 +281,7 @@ public interface ElixirTypes {
   IElementType INVALID_BINARY_DIGITS = new ElixirTokenType("A-Z, a-z, 2-9");
   IElementType INVALID_DECIMAL_DIGITS = new ElixirTokenType("A-Z, a-z");
   IElementType INVALID_HEXADECIMAL_DIGITS = new ElixirTokenType("G-Z, g-z");
-  IElementType INVALID_OCTAL_DIGITS = new ElixirTokenType("INVALID_OCTAL_DIGITS");
+  IElementType INVALID_OCTAL_DIGITS = new ElixirTokenType("A-Z, a-z, 8-9");
   IElementType INVALID_UNKNOWN_BASE_DIGITS = new ElixirTokenType("INVALID_UNKNOWN_BASE_DIGITS");
   IElementType IN_MATCH_OPERATOR = new ElixirTokenType("IN_MATCH_OPERATOR");
   IElementType IN_OPERATOR = new ElixirTokenType("in");
