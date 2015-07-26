9de2fc93249b6e5164402a9985a3bb6985bbc2af
Luke Imhoff <Kronic.Deth@gmail.com>
2015-07-25 21:29:01 -0500

    Name KEYWORD_PAIR_COLON
    Can't use ":" because that's used for COLON.

--- gen/org/elixir_lang/psi/ElixirTypes.java
+++ gen/org/elixir_lang/psi/ElixirTypes.java
@@ -285,7 +285,7 @@ public interface ElixirTypes {
   IElementType INVALID_UNKNOWN_BASE_DIGITS = new ElixirTokenType("A-Z, a-z, 0-9");
   IElementType IN_MATCH_OPERATOR = new ElixirTokenType("<-, \\\\\\\\");
   IElementType IN_OPERATOR = new ElixirTokenType("in");
-  IElementType KEYWORD_PAIR_COLON = new ElixirTokenType("KEYWORD_PAIR_COLON");
+  IElementType KEYWORD_PAIR_COLON = new ElixirTokenType("Keyword Pair Colon (:)");
   IElementType LITERAL_CHAR_LIST_SIGIL_NAME = new ElixirTokenType("LITERAL_CHAR_LIST_SIGIL_NAME");
   IElementType LITERAL_REGEX_SIGIL_NAME = new ElixirTokenType("LITERAL_REGEX_SIGIL_NAME");
   IElementType LITERAL_SIGIL_NAME = new ElixirTokenType("LITERAL_SIGIL_NAME");
