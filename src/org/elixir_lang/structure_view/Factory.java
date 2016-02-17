202c1562093348e44c07df088fa4dbc83f9d0e8b
Luke Imhoff <limhoff@csd.org>
2016-02-16 23:16:22 -0600

    Remove unused import that fails on travis-ci

--- src/org/elixir_lang/structure_view/Factory.java
+++ src/org/elixir_lang/structure_view/Factory.java
@@ -6,7 +6,6 @@ import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import com.sun.xml.internal.bind.annotation.OverrideAnnotationOf;
 import org.elixir_lang.psi.ElixirFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
