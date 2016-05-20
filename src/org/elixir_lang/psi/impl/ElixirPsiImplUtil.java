aaea4189e8cd1cf372d8af94eaecf590c0b0b9c3
Luke Imhoff <limhoff@csd.org>
2016-05-20 16:08:10 -0500

    Don't process declarations for access expressions
    Access expressions are not declaring scopes, they should only be entered
    from a declaring scope using a PsiScopeProcessor.

--- src/org/elixir_lang/psi/impl/ElixirPsiImplUtil.java
+++ src/org/elixir_lang/psi/impl/ElixirPsiImplUtil.java
@@ -1247,19 +1247,6 @@ public class ElixirPsiImplUtil {
         return primaryArity;
     }
 
-    public static boolean processDeclarations(@NotNull final ElixirAccessExpression accessExpression,
-                                              @NotNull PsiScopeProcessor processor,
-                                              @NotNull ResolveState state,
-                                              PsiElement lastParent,
-                                              @NotNull PsiElement place) {
-        return processDeclarationsRecursively(
-                accessExpression,
-                processor,
-                state,
-                lastParent,
-                place
-        );
-    }
 
     public static boolean processDeclarations(@NotNull final ElixirAlias alias,
                                               @NotNull PsiScopeProcessor processor,
