fc02b8ce062950a5492eaa3c6f867d5227632af0
Luke Imhoff <limhoff@csd.org>
2016-05-20 15:39:14 -0500

    UnaryNonNumericOperation interface
    To match ElixirMatchedUnaryNonNumericOperation and
    ElixirUnmatchedUnaryNonNumericOperation.

---
+++ src/org/elixir_lang/psi/UnaryNonNumericOperation.java
@@ -0,0 +1,8 @@
+package org.elixir_lang.psi;
+
+/**
+ * unaryPrefixOperator !numeric ...
+ */
+public interface UnaryNonNumericOperation extends org.elixir_lang.psi.call.Named, org.elixir_lang.psi.operation.Prefix {
+
+}
