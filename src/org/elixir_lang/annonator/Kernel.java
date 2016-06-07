3c4a963db893734e4297035493b5a705771ea0f5
Luke Imhoff <limhoff@csd.org>
2016-06-07 08:33:13 -0500

    Add `with` to highlighted special forms

--- src/org/elixir_lang/annonator/Kernel.java
+++ src/org/elixir_lang/annonator/Kernel.java
@@ -159,7 +159,8 @@ public class Kernel implements Annotator, DumbAware {
                             "super",
                             "try",
                             UNQUOTE,
-                            "unquote_splicing"
+                            "unquote_splicing",
+                            "with"
                     }
             )
     );
