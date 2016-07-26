cf43cdf6720a3f2c25f1d439e7ee80bc96775c2a
Luke Imhoff <limhoff@csd.org>
2016-07-26 09:49:50 -0500

    Check all left or rigth operands instead of normalized operands in match
    Determine whether to check left, right, or both by doing isAncestor
    checks for all operands, not just the normalized operand.  The
    normalized operand is still used for PsiSceopProcessor#execute since
    execute is not expected to handle error elements.

---
+++ src/org/elixir_lang/psi/operation/infix/Position.java
@@ -0,0 +1,8 @@
+package org.elixir_lang.psi.operation.infix;
+
+/**
+ * The position of a child inside the {@link org.elixir_lang.psi.operation.Infix}.
+ */
+public enum Position {
+    LEFT, OPERATOR, RIGHT
+}
