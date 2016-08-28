72a886bec552d01b30662f158d3515d25ad4f436
Luke Imhoff <limhoff@csd.org>
2016-08-28 17:16:51 -0500

    Add nested aliased names to Module variants
    If I aslias `alias MyApp.SSH` and there exists `MyApp.SSH.Key`, `SSH.`
    should show `SSH.Key` as a completion now.

--- src/org/elixir_lang/psi/scope/module/Variants.java
+++ src/org/elixir_lang/psi/scope/module/Variants.java
@@ -2,14 +2,24 @@ package org.elixir_lang.psi.scope.module;
 
 import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Condition;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
+import com.intellij.psi.PsiReference;
 import com.intellij.psi.ResolveState;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.stubs.StubIndex;
+import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
+import org.elixir_lang.psi.NamedElement;
 import org.elixir_lang.psi.scope.Module;
+import org.elixir_lang.psi.stub.index.AllName;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static com.intellij.psi.util.PsiTreeUtil.treeWalkUp;
@@ -37,6 +47,8 @@ public class Variants extends Module {
      * Fields
      */
 
+    private Collection<String> nameCollection = null;
+    private Project project = null;
     private List<LookupElement> lookupElementList = null;
 
     /*
@@ -53,7 +65,7 @@ public class Variants extends Module {
      * @return {@code true} to keep processing; {@code false} to stop processing.
      */
     @Override
-    protected boolean executeOnAliasedName(@NotNull PsiNamedElement match, @NotNull String aliasedName, @NotNull ResolveState state) {
+    protected boolean executeOnAliasedName(@NotNull PsiNamedElement match, @NotNull final String aliasedName, @NotNull ResolveState state) {
         if (lookupElementList == null) {
             lookupElementList = new ArrayList<LookupElement>();
         }
@@ -65,6 +77,30 @@ public class Variants extends Module {
                 )
         );
 
+        final String unaliasedName = match.getName();
+
+        if (unaliasedName != null) {
+            List<String> unaliasedNestedNames = ContainerUtil.findAll(nameCollection(match.getProject()), new Condition<String>() {
+                @Override
+                public boolean value(String indexedName) {
+                    return indexedName.startsWith(unaliasedName) && indexedName.length() > unaliasedName.length();
+                }
+            });
+            List<String> aliasedNestedNames = ContainerUtil.map(unaliasedNestedNames, new Function<String, String>() {
+                @Override
+                public String fun(String unaliasedNestedName) {
+                    return unaliasedNestedName.replaceFirst(unaliasedName, aliasedName);
+                }
+            });
+            List<LookupElement> aliasedNesteNameLookElements = ContainerUtil.map(aliasedNestedNames, new Function<String, LookupElement>() {
+                @Override
+                public LookupElement fun(String aliasedNestedName) {
+                    return LookupElementBuilder.create(aliasedNestedName);
+                }
+            });
+            lookupElementList.addAll(aliasedNesteNameLookElements);
+        }
+
         return true;
     }
 
@@ -74,5 +110,17 @@ public class Variants extends Module {
 
     private List<LookupElement> getLookupElementList() {
         return lookupElementList;
+    }
+
+    /**
+     * Caches {@code StubIndex.getAllKeys(AllName.KEY, project)}
+     * @return
+     */
+    private Collection<String> nameCollection(Project project) {
+        if (project != this.project || nameCollection == null) {
+            nameCollection = StubIndex.getInstance().getAllKeys(AllName.KEY, project);
+        }
+
+        return nameCollection;
     }
 }
