669d9fba34d96ce96d30b95ab722e28a2cac2aba
Luke Imhoff <limhoff@csd.org>
2016-10-20 23:28:06 -0500

    Failing regression test for #468

---
+++ testData/org/elixir_lang/structure_view/element/call_definition_head/issue_468.ex
@@ -0,0 +1,11 @@
+def create(state = %__MODULE__{ecto_schema_module: ecto_schema_module, view: view}, params) when when is_ is_map(params)  do
+  with :ok <- can(state, :create, ecto_schema_module),
+       {:ok, document} <- document(params, :create),
+       insertable_params = insertable_params(state, document),
+       {:ok, changeset} <- changeset(state, insertable_params),
+       :ok <- can(state, :create, changeset),
+       {:ok, created} <- create_changeset(state, changeset) do
+    authorized = authorized(state, created)
+    {:ok, view.show(authorized, params)}
+  end
+end
