e2e6706b46f5ed602bd72b86ee6a9f5c58c4b7a0
Luke Imhoff <limhoff@csd.org>
2016-12-24 08:42:07 -0600

    Break up Decompiler

--- src/org/elixir_lang/beam/Decompiler.java
+++ src/org/elixir_lang/beam/Decompiler.java
@@ -14,9 +14,13 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 
 import static com.intellij.openapi.util.Pair.pair;
+import static org.elixir_lang.psi.call.name.Function.DEF;
+import static org.elixir_lang.psi.call.name.Function.DEFMACRO;
 import static org.elixir_lang.psi.call.name.Module.ELIXIR_PREFIX;
 
 public class Decompiler implements BinaryFileDecompiler {
+    private static final String MACRO_EXPORT_PREFIX = "MACRO-";
+
     @NotNull
     private static CharSequence decompiled(@Nullable Beam beam) {
         StringBuilder decompiled = new StringBuilder("Decompilated Error");
@@ -28,13 +32,7 @@ public class Decompiler implements BinaryFileDecompiler {
                 String moduleName = atoms.moduleName();
 
                 if (moduleName != null) {
-                    String defmoduleArgument;
-
-                    if (moduleName.startsWith(ELIXIR_PREFIX)) {
-                        defmoduleArgument = moduleName.substring(ELIXIR_PREFIX.length());
-                    } else {
-                        defmoduleArgument = ":" + moduleName;
-                    }
+                    String defmoduleArgument = defmoduleArgument(moduleName);
 
                     decompiled = new StringBuilder(
                             "# Source code recreated from a .beam file by IntelliJ Elixir\n"
@@ -43,51 +41,7 @@ public class Decompiler implements BinaryFileDecompiler {
                             .append(defmoduleArgument)
                             .append(" do\n");
 
-                    Exports exports = beam.exports();
-
-                    if (exports != null) {
-                        Pair<SortedMap<String, SortedMap<Integer, Export>>, SortedSet<Export>>
-                                exportByArityByNameNamelessExportSet = exports.exportByArityByName(atoms);
-                        SortedMap<String, SortedMap<Integer, Export>> exportByArityByName =
-                                exportByArityByNameNamelessExportSet.first;
-
-                        boolean first = true;
-                        for (SortedMap.Entry<String, SortedMap<Integer, Export>> nameExportByArity :
-                                exportByArityByName.entrySet()) {
-                            String name = nameExportByArity.getKey();
-                            Pair<String, String> macroArgument = macroArgument(name);
-
-                            SortedMap<Integer, Export> exportByArity = nameExportByArity.getValue();
-
-                            for (SortedMap.Entry<Integer, Export> arityExport : exportByArity.entrySet()) {
-                                if (!first) {
-                                    decompiled.append("\n");
-                                }
-
-                                decompiled
-                                        .append("  ")
-                                        .append(macroArgument.first)
-                                        .append(" ")
-                                        .append(macroArgument.second)
-                                        .append("(");
-
-                                Integer arity = arityExport.getKey();
-
-                                if (arity != null) {
-                                    for (int i = 0; i < arity; i++) {
-                                        if (i != 0) {
-                                            decompiled.append(", ");
-                                        }
-
-                                        decompiled.append("p").append(i);
-                                    }
-                                }
-
-                                decompiled.append(") do\n").append("    # body not decompiled\n").append("  end\n");
-                                first = false;
-                            }
-                        }
-                    }
+                    appendExports(decompiled, beam, atoms);
 
                     decompiled.append("end\n");
                 }
@@ -97,15 +51,86 @@ public class Decompiler implements BinaryFileDecompiler {
         return decompiled;
     }
 
+    private static void appendExports(@NotNull StringBuilder decompiled, @NotNull Beam beam, @NotNull Atoms atoms) {
+        Exports exports = beam.exports();
+
+        if (exports != null) {
+            appendExports(decompiled, exports, atoms);
+        }
+    }
+
+    private static void appendExports(@NotNull StringBuilder decompiled,
+                                      @NotNull Exports exports,
+                                      @NotNull Atoms atoms) {
+        Pair<SortedMap<String, SortedMap<Integer, Export>>, SortedSet<Export>>
+                exportByArityByNameNamelessExportSet = exports.exportByArityByName(atoms);
+        SortedMap<String, SortedMap<Integer, Export>> exportByArityByName =
+                exportByArityByNameNamelessExportSet.first;
+
+        boolean first = true;
+        for (SortedMap.Entry<String, SortedMap<Integer, Export>> nameExportByArity :
+                exportByArityByName.entrySet()) {
+            String name = nameExportByArity.getKey();
+            Pair<String, String> macroArgument = macroArgument(name);
+
+            SortedMap<Integer, Export> exportByArity = nameExportByArity.getValue();
+
+            for (SortedMap.Entry<Integer, Export> arityExport : exportByArity.entrySet()) {
+                if (!first) {
+                    decompiled.append("\n");
+                }
+
+                appendExport(decompiled, macroArgument, arityExport.getKey());
+
+                first = false;
+            }
+        }
+    }
+
+    private static void appendExport(@NotNull StringBuilder decompiled,
+                                     @NotNull Pair<String, String> macroArgument,
+                                     @Nullable Integer arity) {
+        decompiled
+                .append("  ")
+                .append(macroArgument.first)
+                .append(" ")
+                .append(macroArgument.second)
+                .append("(");
+
+
+        if (arity != null) {
+            for (int i = 0; i < arity; i++) {
+                if (i != 0) {
+                    decompiled.append(", ");
+                }
+
+                decompiled.append("p").append(i);
+            }
+        }
+
+        decompiled.append(") do\n").append("    # body not decompiled\n").append("  end\n");
+    }
+
+    @NotNull
+    private static String defmoduleArgument(String moduleName) {
+        String defmoduleArgument;
+        if (moduleName.startsWith(ELIXIR_PREFIX)) {
+            defmoduleArgument = moduleName.substring(ELIXIR_PREFIX.length());
+        } else {
+            defmoduleArgument = ":" + moduleName;
+        }
+        return defmoduleArgument;
+    }
+
     @Contract(pure = true)
     @NotNull
     private static Pair<String, String> macroArgument(@NotNull String name) {
         String argument = name;
-        String macro = "def";
+        String macro = DEFMACRO;
 
-        if (name.startsWith("MACRO-")) {
-            argument = name.substring("MACRO-".length());
-            macro = "defmacro";
+        if (name.startsWith(MACRO_EXPORT_PREFIX)) {
+            argument = name.substring(MACRO_EXPORT_PREFIX.length());
+            macro = DEF;
         }
 
         return pair(macro, argument);
