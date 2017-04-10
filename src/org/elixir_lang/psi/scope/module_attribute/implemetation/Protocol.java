886d245d900b70cc66843768f68779b3fb0207df
Luke Imhoff <Kronic.Deth@gmail.com>
2017-04-09 22:53:26 -0500

    Check if Protocol.resolveResultList is null

--- src/org/elixir_lang/psi/scope/module_attribute/implemetation/Protocol.java
+++ src/org/elixir_lang/psi/scope/module_attribute/implemetation/Protocol.java
@@ -24,6 +24,7 @@ public class Protocol implements PsiScopeProcessor {
      *
      */
 
+    @Nullable
     public static List<ResolveResult> resolveResultList(boolean validResult, @NotNull PsiElement entrance) {
         Protocol protocol = new Protocol(validResult);
         PsiTreeUtil.treeWalkUp(
