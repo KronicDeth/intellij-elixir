10a4db660529807aeaceb14846bf733c8f9a2f75
Luke Imhoff <Kronic.Deth@gmail.com>
2017-07-12 17:24:27 -0500

    Convert test compilation error to test failed

--- src/org/elixir_lang/mix/runner/Status.java
+++ src/org/elixir_lang/mix/runner/Status.java
@@ -1,25 +1,28 @@
 package org.elixir_lang.mix.runner;
 
+import com.google.common.base.CaseFormat;
 import com.intellij.execution.testframework.sm.ServiceMessageBuilder;
+import gnu.trove.THashMap;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Status {
     private static final String COMPILATION_ERROR_PREFIX = "    (elixir) lib/kernel/parallel_require.ex:";
     private static final String COMPILATION_FINISHED = compilationMessage("Finished");
     private static final String COMPILATION_STARTED = compilationMessage("Started");
+    private static final Pattern TEST_MODULE_PATH_PATTERN = Pattern.compile("(?<file>test/.*_test.exs):(?<line>\\d+): \\(module\\)\\z");
     @NotNull
     private final String status;
     @NotNull
     private final String text;
     @Nullable
     private List<String> stackTraceLines;
+
     private Status(@NotNull String status, @NotNull String text) {
         this.status = status;
         this.text = text;
@@ -82,11 +85,67 @@ public class Status {
         stackTraceLines.add(withoutNewline);
     }
 
+    @NotNull
+    private String fileToTestName(@NotNull String file) {
+        String[] parts = file
+                .substring(
+                        0,
+                        file.length() - ".exs".length()
+                )
+                .split("/");
+        List<String> relativeModuleNames = new ArrayList<>(parts.length);
+
+        for (String part : parts) {
+            relativeModuleNames.add(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, part));
+        }
+
+        return StringUtils.join(relativeModuleNames, ".");
+    }
+
     private boolean isCompilationError() {
         return stackTraceLines != null &&
                 stackTraceLines
                         .get(stackTraceLines.size() - 1)
                         .startsWith(COMPILATION_ERROR_PREFIX);
+    }
+
+    @Nullable
+    private Map<String, String> maybeTestStartedAttributes() {
+        Map<String, String> attributes = null;
+
+        if (stackTraceLines != null && stackTraceLines.size() > 0) {
+            String firstStackTraceLine = stackTraceLines.get(0);
+
+            Matcher matcher = TEST_MODULE_PATH_PATTERN.matcher(firstStackTraceLine);
+
+            if (matcher.find()) {
+                String file = matcher.group("file");
+                String line = matcher.group("line");
+                String locationHint = "file://" + file + ":" + line;
+                attributes = new THashMap<>(2);
+                attributes.put("locationHint", locationHint);
+
+                String name = fileToTestName(file);
+                attributes.put("name", name);
+            }
+        }
+
+        return attributes;
+    }
+
+    @NotNull
+    private String teamCityTestFinished(@NotNull Map<String, String> attributes) {
+        return ServiceMessageBuilder
+                .testFinished(attributes.get("name"))
+                .toString();
+    }
+
+    @NotNull
+    private String teamCityTestStarted(@NotNull Map<String, String> attributes) {
+        return ServiceMessageBuilder
+                .testStarted(attributes.get("name"))
+                .addAttribute("locationHint", attributes.get("locationHint"))
+                .toString();
     }
 
     @NotNull
@@ -114,11 +173,11 @@ public class Status {
     }
 
     @NotNull
-    private String toTeamCityTestFailure() {
+    private String toTeamCityTestFailed(@NotNull String name) {
         String details = StringUtils.join(stackTraceLines, "\n");
 
         return ServiceMessageBuilder
-                .testFailed("mix test")
+                .testFailed(name)
                 .addAttribute("details", details)
                 .addAttribute("message", text)
                 .toString();
@@ -129,9 +188,22 @@ public class Status {
         List<String> messageList;
 
         if (isCompilationError()) {
-            messageList = Collections.singletonList(
-                    toTeamCityTestFailure()
-            );
+            messageList = new ArrayList<>();
+            Map<String, String> maybeTestStartedAttributes = maybeTestStartedAttributes();
+            String name;
+
+            if (maybeTestStartedAttributes != null) {
+                messageList.add(teamCityTestStarted(maybeTestStartedAttributes));
+                name = maybeTestStartedAttributes.get("name");
+            } else {
+                name = "mix test";
+            }
+
+            messageList.add(toTeamCityTestFailed(name));
+
+            if (maybeTestStartedAttributes != null) {
+                messageList.add(teamCityTestFinished(maybeTestStartedAttributes));
+            }
         } else {
             messageList = Collections.emptyList();
         }
