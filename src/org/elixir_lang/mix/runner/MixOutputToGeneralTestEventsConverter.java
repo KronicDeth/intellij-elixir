ba595d6b9e137a0da0ffd551cfebd9bf56795f2c
Luke Imhoff <Kronic.Deth@gmail.com>
2017-07-12 22:43:34 -0500

    Use reflection for processConsistentText
    Prior to 2016.3, processConsistentText was private, so to support 14.1
    through 2017, need to be able to call the super method with reflection
    whether it's private or protected.  Since reflection can't call a super
    method, MixOutputToGeneralTestEventsConverter can't "override"
    processConsistentText or a loop will occur in 2016.3+.

--- src/org/elixir_lang/mix/runner/MixOutputToGeneralTestEventsConverter.java
+++ src/org/elixir_lang/mix/runner/MixOutputToGeneralTestEventsConverter.java
@@ -2,17 +2,52 @@ package org.elixir_lang.mix.runner;
 
 import com.intellij.execution.process.ProcessOutputTypes;
 import com.intellij.execution.testframework.TestConsoleProperties;
+import com.intellij.execution.testframework.sm.runner.OutputLineSplitter;
 import com.intellij.execution.testframework.sm.runner.OutputToGeneralTestEventsConverter;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Key;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MixOutputToGeneralTestEventsConverter extends OutputToGeneralTestEventsConverter {
+    private static final Logger LOGGER = Logger.getInstance(MixOutputToGeneralTestEventsConverter.class);
+    @NotNull
+    private static final Method SUPER_PROCESS_CONSISTENT_TEXT_METHOD;
 
+    static {
+        try {
+            SUPER_PROCESS_CONSISTENT_TEXT_METHOD = OutputToGeneralTestEventsConverter.class.getDeclaredMethod(
+                    "processConsistentText",
+                    String.class,
+                    Key.class,
+                    boolean.class
+            );
+            // counter `private` in IntelliJ IDEA prior to 2016.2
+            SUPER_PROCESS_CONSISTENT_TEXT_METHOD.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    private OutputLineSplitter splitter;
+    @Nullable
     private Status stderrStatus = null;
+    @Nullable
     private Status stdoutStatus = null;
 
     MixOutputToGeneralTestEventsConverter(@NotNull String testFrameworkName, @NotNull TestConsoleProperties consoleProperties) {
         super(testFrameworkName, consoleProperties);
+
+        splitter = new OutputLineSplitter(consoleProperties.isEditable()) {
+            @Override
+            protected void onLineAvailable(@NotNull String text, @NotNull Key outputType, boolean tcLikeFakeOutput) {
+                subProcessConsistentText(text, outputType, tcLikeFakeOutput);
+            }
+        };
     }
 
     /**
@@ -25,7 +60,35 @@ public class MixOutputToGeneralTestEventsConverter extends OutputToGeneralTestEv
     }
 
     @Override
-    protected void processConsistentText(@NotNull String text, @NotNull Key outputType, boolean tcLikeFakeOutput) {
+    public void process(String text, Key outputType) {
+        splitter.process(text, outputType);
+    }
+
+    private void processStatus(@NotNull Status status, @NotNull Key outputType) {
+        for (String text : status.toTeamCityCompilationMessageList()) {
+            superProcessConsistentText(text, outputType, false);
+        }
+
+        if (outputType == ProcessOutputTypes.STDERR) {
+            for (String text : status.toTeamCityTestMessageList()) {
+                superProcessConsistentText(text, outputType, false);
+            }
+        }
+    }
+
+    private void processStatuses() {
+        if (stderrStatus != null) {
+            processStatus(stderrStatus, ProcessOutputTypes.STDERR);
+            stderrStatus = null;
+        }
+
+        if (stdoutStatus != null) {
+            processStatus(stdoutStatus, ProcessOutputTypes.STDOUT);
+            stdoutStatus = null;
+        }
+    }
+
+    private void subProcessConsistentText(@NotNull String text, @NotNull Key outputType, boolean tcLikeFakeOutput) {
         if (outputType == ProcessOutputTypes.STDERR) {
             if (stderrStatus != null) {
                 if (text.startsWith("  ")) {
@@ -34,13 +97,13 @@ public class MixOutputToGeneralTestEventsConverter extends OutputToGeneralTestEv
                     processStatus(stderrStatus, outputType);
                     stderrStatus = null;
                 } else {
-                    super.processConsistentText(text, outputType, tcLikeFakeOutput);
+                    superProcessConsistentText(text, outputType, tcLikeFakeOutput);
                 }
             } else {
                 stderrStatus = Status.fromStderrLine(text);
 
                 if (stderrStatus == null) {
-                    super.processConsistentText(text, outputType, tcLikeFakeOutput);
+                    superProcessConsistentText(text, outputType, tcLikeFakeOutput);
                 }
             }
         } else if (outputType == ProcessOutputTypes.STDOUT) {
@@ -55,35 +118,19 @@ public class MixOutputToGeneralTestEventsConverter extends OutputToGeneralTestEv
                 stdoutStatus = Status.fromStdoutLine(text);
 
                 if (stdoutStatus == null) {
-                    super.processConsistentText(text, outputType, tcLikeFakeOutput);
+                    superProcessConsistentText(text, outputType, tcLikeFakeOutput);
                 }
             }
         } else {
-            super.processConsistentText(text, outputType, tcLikeFakeOutput);
+            superProcessConsistentText(text, outputType, tcLikeFakeOutput);
         }
     }
 
-    private void processStatus(@NotNull Status status, @NotNull Key outputType) {
-        for (String text : status.toTeamCityCompilationMessageList()) {
-            super.processConsistentText(text, outputType, false);
-        }
-
-        if (outputType == ProcessOutputTypes.STDERR) {
-            for (String text : status.toTeamCityTestMessageList()) {
-                super.processConsistentText(text, outputType, false);
-            }
-        }
-    }
-
-    private void processStatuses() {
-        if (stderrStatus != null) {
-            processStatus(stderrStatus, ProcessOutputTypes.STDERR);
-            stderrStatus = null;
-        }
-
-        if (stdoutStatus != null) {
-            processStatus(stdoutStatus, ProcessOutputTypes.STDOUT);
-            stdoutStatus = null;
+    private void superProcessConsistentText(@NotNull String text, @NotNull Key outputType, boolean tcLikeFakeOutput) {
+        try {
+            SUPER_PROCESS_CONSISTENT_TEXT_METHOD.invoke(this, text, outputType, tcLikeFakeOutput);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            LOGGER.error(e);
         }
     }
 }
