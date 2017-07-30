e12d1c619842166d0887a577f556f23a3ad92c4a
Luke Imhoff <Kronic.Deth@gmail.com>
2017-07-30 16:31:47 -0500

    Failing regression test for #694

--- tests/org/elixir_lang/annotator/ModuleAttributeTest.java
+++ tests/org/elixir_lang/annotator/ModuleAttributeTest.java
@@ -59,6 +59,11 @@ public class ModuleAttributeTest extends LightCodeInsightFixtureTestCase {
         myFixture.checkHighlighting(false, false, true);
     }
 
+    public void testIssue694() {
+        myFixture.configureByFile("issue_694.ex");
+        myFixture.checkHighlighting(false, false, true);
+    }
+
     public void testMatch() {
         myFixture.configureByFile("match.ex");
         myFixture.checkHighlighting(false, false, true);
