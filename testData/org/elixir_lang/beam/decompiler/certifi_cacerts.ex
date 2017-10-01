f0b0ac5a495d0ba60620b9dd0dc3d3152f4e49ef
Luke Imhoff <Kronic.Deth@gmail.com>
2017-10-01 13:32:02 -0500

    Regression test for #803

---
+++ testData/org/elixir_lang/beam/decompiler/certifi_cacerts.ex
@@ -0,0 +1,17 @@
+# Source code recreated from a .beam file by IntelliJ Elixir
+defmodule :certifi_cacerts do
+
+  # Functions
+
+  def ders() do
+    # body not decompiled
+  end
+
+  def module_info() do
+    # body not decompiled
+  end
+
+  def module_info(p0) do
+    # body not decompiled
+  end
+end
