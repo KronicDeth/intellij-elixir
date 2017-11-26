828e49ae0c73b879c09e5c9701ec1c2ce0019f86
Luke Imhoff <Kronic.Deth@gmail.com>
2017-11-25 21:36:14 -0600

    EEx.TokenizerTest strings with more than one line and expression with more than one line

--- tests/org/elixir_lang/eex/psi/parser/Test.java
+++ tests/org/elixir_lang/eex/psi/parser/Test.java
@@ -76,6 +76,10 @@ public class Test extends ParsingTestCase {
         doTest(true);
     }
 
+    public void testEExTokenizerTestStringsWithMoreThanOneLineAndExpressionWithMoreThanOneLine(){
+        doTest(true);
+    }
+
     @Override
     protected String getTestDataPath() {
         return "testData/org/elixir_lang/eex/psi/parser/test";
