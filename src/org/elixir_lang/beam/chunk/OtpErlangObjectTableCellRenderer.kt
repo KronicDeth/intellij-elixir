b70e9f49c453db47b9b5213a97c60e019066b9b3
Luke Imhoff <Kronic.Deth@gmail.com>
2018-01-05 11:07:08 -0600

    Add quotes around string values in LitT Term column

--- src/org/elixir_lang/beam/chunk/OtpErlangObjectTableCellRenderer.kt
+++ src/org/elixir_lang/beam/chunk/OtpErlangObjectTableCellRenderer.kt
@@ -1,12 +1,14 @@
 package org.elixir_lang.beam.chunk
 
 import com.ericsson.otp.erlang.OtpErlangObject
-import com.intellij.xdebugger.impl.ui.tree.nodes.XValuePresentationUtil
 import org.elixir_lang.debugger.ElixirXValuePresentation
 import javax.swing.table.DefaultTableCellRenderer
 
 fun inspect(term: OtpErlangObject): String =
-    XValuePresentationUtil.computeValueText(ElixirXValuePresentation(term))
+    XValueRenderer().let { renderer ->
+        ElixirXValuePresentation(term).renderValue(renderer)
+        renderer.getText()
+    }
 
 class OtpErlangObjectTableCellRenderer: DefaultTableCellRenderer() {
     override fun setValue(value: Any?) {
