7f1ac19cda7dd6c5377d45076e81dfa240ff5744
Luke Imhoff <Kronic.Deth@gmail.com>
2018-01-16 22:36:52 -0600

    Show code for module and definition too

--- src/org/elixir_lang/beam/chunk/debug_info/v1/elixir_erl/v1/definitions/definition/clause/Panel.kt
+++ src/org/elixir_lang/beam/chunk/debug_info/v1/elixir_erl/v1/definitions/definition/clause/Panel.kt
@@ -5,51 +5,119 @@ import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFileFactory
+import com.intellij.util.containers.ContainerUtil
 import org.elixir_lang.ElixirFileType
 import org.elixir_lang.ElixirLanguage
 import org.elixir_lang.Macro
 import org.elixir_lang.Macro.adjustNewLines
+import org.elixir_lang.beam.chunk.debug_info.v1.elixir_erl.V1
+import org.elixir_lang.beam.chunk.debug_info.v1.elixir_erl.v1.definitions.Definition
 import org.elixir_lang.beam.chunk.debug_info.v1.elixir_erl.v1.definitions.Tree
 import org.elixir_lang.beam.chunk.debug_info.v1.elixir_erl.v1.definitions.definition.Clause
 import java.awt.GridLayout
+import java.lang.ref.WeakReference
 import javax.swing.JPanel
 import javax.swing.event.TreeSelectionEvent
 import javax.swing.event.TreeSelectionListener
 
-const val DEFAULT_TEXT = "# Select a clause to view its AST as code"
+const val DEFAULT_TEXT = "# Select a module, definition, or clause to view its AST as code"
 
-class Panel(definitionsTree: Tree, project: Project): JPanel(GridLayout(1, 1)), TreeSelectionListener {
+class Panel(private val definitionsTree: Tree, project: Project): JPanel(GridLayout(1, 1)), TreeSelectionListener {
     private val psiFile = PsiFileFactory.getInstance(project).createFileFromText(ElixirLanguage.INSTANCE, DEFAULT_TEXT)
     private val document = PsiDocumentManager.getInstance(project).getDocument(psiFile)!!
     private val editor = EditorFactory.getInstance().createEditor(document, project, ElixirFileType.INSTANCE, true)
+    private val clauseHeadByClause = ContainerUtil.createWeakValueMap<Clause, String>()
+    private val clauseHeadModuleByClause = ContainerUtil.createWeakValueMap<Clause, String>()
+    private val definitionByDefinition = ContainerUtil.createWeakValueMap<Definition, String>()
+    private val definitionModuleByClause = ContainerUtil.createWeakValueMap<Definition, String>()
+    private var module: WeakReference<String> = WeakReference<String>(null)
 
     init {
         definitionsTree.addTreeSelectionListener(this)
         add(editor.component)
     }
 
+    private fun clauseHead(clause: Clause): String =
+            clauseHeadByClause.computeIfAbsent(clause) { key ->
+                headContext(key) {
+                    Macro.toString(key.block)
+                }
+            }
+
+    private fun clauseHeadModule(clause: Clause): String =
+            clauseHeadModuleByClause.computeIfAbsent(clause) { key ->
+                moduleContext(key) {
+                    clauseHead(key)
+                }
+            }
+
+    private fun definition(definition: Definition): String =
+            definitionByDefinition.computeIfAbsent(definition) { key ->
+                val definitionsTreeModel = definitionsTree.model
+                val clauseCount = definitionsTreeModel.getChildCount(key)
+
+                (0 until clauseCount).joinToString("\n\n") { index ->
+                    val clause = definitionsTreeModel.getChild(key, index) as Clause
+
+                    clauseHead(clause)
+                }
+            }
+
+    private fun definitionModule(definition: Definition): String =
+            definitionModuleByClause.computeIfAbsent(definition) { key ->
+                moduleContext(key) {
+                    definition(key)
+                }
+            }
+
+    private fun module(debugInfo: V1): String {
+        val module = this.module.get()
+
+        return if (module != null) {
+            module
+        } else {
+            val newModule = moduleContext(debugInfo) {
+                val definitionsTreeModel = definitionsTree.model
+                val definitionCount = definitionsTreeModel.getChildCount(debugInfo)
+
+                (0 until definitionCount).joinToString("\n\n") { index ->
+                    val definition = definitionsTreeModel.getChild(debugInfo, index) as Definition
+
+                    definition(definition)
+                }
+            }
+
+            this.module = WeakReference(newModule)
+
+            newModule
+        }
+    }
+
     override fun valueChanged(event: TreeSelectionEvent) {
-        val text = (event.path.lastPathComponent as? Clause)?.let { clause ->
-            blockToStringWithContext(clause)
-        } ?: DEFAULT_TEXT
+        val lastPathComponent = event.path.lastPathComponent
+
+        val text: String = when (lastPathComponent) {
+            is Clause -> clauseHeadModule(lastPathComponent)
+            is Definition -> definitionModule(lastPathComponent)
+            is V1 -> module(lastPathComponent)
+            else -> DEFAULT_TEXT
+        }
 
         ApplicationManager.getApplication().runWriteAction {
             document.setText(text)
         }
     }
 
-    private fun blockToStringWithContext(clause: Clause) =
-            moduleContext(clause) {
-                headContext(clause) {
-                    Macro.toString(clause.block)
-                }
-            }
-
     private fun headContext(clause: Clause, body: () -> String): String =
             "def ${clause.head} do\n  ${adjustNewLines(body(), "\n  ")}\nend"
 
-    private fun moduleContext(clause: Clause, inner: () -> String): String =
-            "defmodule ${clause.definition.debugInfo.inspectedModule!!} do\n" +
+    private fun moduleContext(clause: Clause, inner: () -> String): String = moduleContext(clause.definition, inner)
+
+    private fun moduleContext(definition: Definition, inner: () -> String): String =
+            moduleContext(definition.debugInfo, inner)
+
+    private fun moduleContext(debugInfo: V1, inner: () -> String): String =
+            "defmodule ${debugInfo.inspectedModule!!} do\n" +
                     "  # ...\n" +
                     "  ${adjustNewLines(inner(), "\n  ")}\n" +
                     "  #...\n" +
