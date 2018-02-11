aaf6e453433bf0deeea2e6b8a2e1c227e2452d44
Luke Imhoff <Kronic.Deth@gmail.com>
2018-02-11 11:02:13 -0600

    Format debugger

--- resources/debugger/lib/debug_task.ex
+++ resources/debugger/lib/debug_task.ex
@@ -3,6 +3,9 @@ defmodule Mix.Tasks.IntellijElixir.DebugTask do
 
   use Mix.Task
 
+  # Functions
+
+  @impl Mix.Task
   def run(argv) do
     {options, command_argv} = parse_args(argv)
     {task_name, task_args} = get_task(command_argv)
@@ -13,18 +16,28 @@ defmodule Mix.Tasks.IntellijElixir.DebugTask do
     Mix.Task.run("compile", [])
 
     # Interpret project modules and project dependencies
-    interpret_modules_in(Mix.Project.load_paths)
-    interpret_modules_in(Mix.Project.build_path)
+    interpret_modules_in(Mix.Project.load_paths())
+    interpret_modules_in(Mix.Project.build_path())
 
     # Check version, but continue anyway in case they've patched Erlang < OTP 19 to allow debugging Elixir
     version = String.to_integer(to_string(:erlang.system_info(:otp_release)))
+
     if version < 19 do
-      IO.warn("Erlang version OTP 19 or higher required to debug Elixir. (Current version: #{version})")
+      IO.warn(
+        "Erlang version OTP 19 or higher required to debug Elixir. (Current version: #{version})"
+      )
     end
 
     # Start debugger server with the task and debugger port
     init_args = {{task_name, task_args}, Keyword.get(options, :debugger_port)}
-    {:ok, pid} = GenServer.start_link(IntellijElixir.DebugServer, init_args, [name: IntellijElixir.DebugServer])
+
+    {:ok, pid} =
+      GenServer.start_link(
+        IntellijElixir.DebugServer,
+        init_args,
+        name: IntellijElixir.DebugServer
+      )
+
     ref = Process.monitor(pid)
 
     # Wait for debugger server to finish
@@ -33,22 +46,42 @@ defmodule Mix.Tasks.IntellijElixir.DebugTask do
     end
   end
 
-  defp parse_args(argv) do
-    {debug_argv, command_argv} = Enum.split_while(argv, &(&1 != "--"))
-    command_argv =
-      case command_argv do
-        ["--" | rest] -> rest
-        _ -> command_argv
+  ## Private Functions
+
+  defp change_env(task) do
+    if env = preferred_cli_env(task) do
+      Mix.env(env)
+
+      if project = Mix.Project.pop() do
+        %{name: name, file: file} = project
+        Mix.Project.push(name, file)
       end
-
-    {options, _} = OptionParser.parse!(debug_argv, strict: [debugger_port: :integer])
-
-    unless Keyword.get(options, :debugger_port) do
-      Mix.shell.error "Option --debugger-port required"
-      exit({:shutdown, 1})
     end
+  end
 
-    {options, command_argv}
+  defp elixir_module_name_to_erlang_module_name(":" <> erlang_module_name), do: erlang_module_name
+
+  defp elixir_module_name_to_erlang_module_name(erlang_module_name = "Elixir." <> _),
+    do: erlang_module_name
+
+  defp elixir_module_name_to_erlang_module_name(elixir_module_name),
+    do: "Elixir." <> elixir_module_name
+
+  defp get_task(["-" <> _ | _]) do
+    Mix.shell().error(
+      "** (Mix) Cannot implicitly pass flags to default Mix task, " <>
+        "please invoke instead \"mix #{Mix.Project.config()[:default_task]}\""
+    )
+
+    exit({:shutdown, 1})
+  end
+
+  defp get_task([h | t]) do
+    {h, t}
+  end
+
+  defp get_task([]) do
+    {Mix.Project.config()[:default_task], []}
   end
 
   defp interpret_modules_in(paths) when is_list(paths) do
@@ -65,43 +98,26 @@ defmodule Mix.Tasks.IntellijElixir.DebugTask do
     |> Enum.reject(&MapSet.member?(reject_name_set, &1))
     |> Stream.map(&String.to_atom/1)
     |> Enum.filter(&(:int.interpretable(&1) == true && !:code.is_sticky(&1) && &1 != __MODULE__))
-    |> Enum.each(&(:int.ni(&1)))
+    |> Enum.each(&:int.ni(&1))
   end
 
-  defp elixir_module_name_to_erlang_module_name(":" <> erlang_module_name), do: erlang_module_name
-  defp elixir_module_name_to_erlang_module_name(erlang_module_name = "Elixir." <> _), do: erlang_module_name
-  defp elixir_module_name_to_erlang_module_name(elixir_module_name), do: "Elixir." <> elixir_module_name
+  defp parse_args(argv) do
+    {debug_argv, command_argv} = Enum.split_while(argv, &(&1 != "--"))
 
-  defp reject_name_set() do
-    comma_separated_elixir_module_names = System.get_env("INTELLIJ_ELIXIR_DEBUG_BLACKLIST") || ""
-
-    comma_separated_elixir_module_names
-    |> String.split(",")
-    |> Enum.into(MapSet.new(), &elixir_module_name_to_erlang_module_name/1)
-  end
-
-  defp get_task(["-" <> _ | _]) do
-    Mix.shell.error "** (Mix) Cannot implicitly pass flags to default Mix task, " <>
-                    "please invoke instead \"mix #{Mix.Project.config[:default_task]}\""
-    exit({:shutdown, 1})
-  end
-
-  defp get_task([h | t]) do
-    {h, t}
-  end
-
-  defp get_task([]) do
-    {Mix.Project.config[:default_task], []}
-  end
-
-  defp change_env(task) do
-    if env = preferred_cli_env(task) do
-      Mix.env(env)
-      if project = Mix.Project.pop do
-        %{name: name, file: file} = project
-        Mix.Project.push name, file
+    command_argv =
+      case command_argv do
+        ["--" | rest] -> rest
+        _ -> command_argv
       end
+
+    {options, _} = OptionParser.parse!(debug_argv, strict: [debugger_port: :integer])
+
+    unless Keyword.get(options, :debugger_port) do
+      Mix.shell().error("Option --debugger-port required")
+      exit({:shutdown, 1})
     end
+
+    {options, command_argv}
   end
 
   defp preferred_cli_env(task) do
@@ -109,7 +125,15 @@ defmodule Mix.Tasks.IntellijElixir.DebugTask do
       nil
     else
       task = String.to_atom(task)
-      Mix.Project.config[:preferred_cli_env][task] || Mix.Task.preferred_cli_env(task)
+      Mix.Project.config()[:preferred_cli_env][task] || Mix.Task.preferred_cli_env(task)
     end
+  end
+
+  defp reject_name_set() do
+    comma_separated_elixir_module_names = System.get_env("INTELLIJ_ELIXIR_DEBUG_BLACKLIST") || ""
+
+    comma_separated_elixir_module_names
+    |> String.split(",")
+    |> Enum.into(MapSet.new(), &elixir_module_name_to_erlang_module_name/1)
   end
 end
