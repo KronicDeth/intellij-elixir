fa9230f77d0cc57c7f41f15eee7a44f8a3875692
Luke Imhoff <Kronic.Deth@gmail.com>
2018-03-02 22:52:15 -0600

    Lower testData required elixir to 1.4

--- jps-builder/testData/mix_compiled/mix.exs
+++ jps-builder/testData/mix_compiled/mix.exs
@@ -5,7 +5,7 @@ defmodule MixCompiled.MixProject do
     [
       app: :mix_compiled,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
