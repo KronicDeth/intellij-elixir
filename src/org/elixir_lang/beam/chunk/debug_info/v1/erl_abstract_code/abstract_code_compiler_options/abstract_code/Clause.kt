99a233d96c350fc36330f6af798f00d77cdeaf58
Luke Imhoff <Kronic.Deth@gmail.com>
2018-03-26 23:21:43 -0500

    Fix guard clauses in :cowboy

--- src/org/elixir_lang/beam/chunk/debug_info/v1/erl_abstract_code/abstract_code_compiler_options/abstract_code/Clause.kt
+++ src/org/elixir_lang/beam/chunk/debug_info/v1/erl_abstract_code/abstract_code_compiler_options/abstract_code/Clause.kt
@@ -7,15 +7,15 @@ import org.elixir_lang.Macro.adjustNewLines
 
 private const val TAG = "clause"
 
-class Clause(val attributes: Attributes, val function: Function, term: OtpErlangTuple): Node(term) {
+class Clause(val attributes: Attributes, val function: Function, val term: OtpErlangTuple): Node(term) {
     private val patterns by lazy {
         (term.elementAt(2) as? OtpErlangList)?.filterIsInstance<OtpErlangTuple>()?.mapNotNull { Node.from(it) }
     }
-    private val guards by lazy {
-        (term.elementAt(3) as? OtpErlangList)?.filterIsInstance<OtpErlangTuple>()?.mapNotNull { Node.from(it) }
+    private val guardSequence by lazy {
+        (term.elementAt(3) as? OtpErlangList)?.filterIsInstance<OtpErlangList>()?.mapNotNull { it.mapNotNull { Node.from(it) } }
     }
     val head by lazy {
-        "${function.name}(${patternsToMacroString(this.patterns)})${guardsToMacroString(this.guards)}"
+        "${function.name}(${patternsToMacroString(this.patterns)})${guardSequenceToMacroString(this.guardSequence)}"
     }
     val body by lazy {
         (term.elementAt(4) as? OtpErlangList)?.filterIsInstance<OtpErlangTuple>()?.mapNotNull { Node.from(it) } ?:
@@ -31,16 +31,22 @@ class Clause(val attributes: Attributes, val function: Function, term: OtpErlang
         fun from(term: OtpErlangObject, attributes: Attributes, function: Function): Clause? =
                 ifTag(term, TAG) { Clause(attributes, function, it) }
 
-        private fun guardsToMacroString(guards: List<Node>?): String =
-                if (guards != null) {
-                    if (guards.isEmpty()) {
+        private fun guardSequenceToMacroString(guardSequence: List<List<Node>>?): String =
+                if (guardSequence != null) {
+                    if (guardSequence.isEmpty()) {
                         ""
                     } else {
-                        " when ${guards.map { it.toMacroString() }.joinToString(" and ")}"
+                        " when ${guardSequence.joinToString(" or ") { guard ->
+                            guardToMacroString(guard)
+                        }}"
                     }
                 } else {
                     " when ?"
                 }
+
+        private fun guardToMacroString(guard: List<Node>): String =
+                guard.joinToString(" and ") { it.toMacroString() }
+
         private fun patternsToMacroString(patterns: List<Node>?): String =
                 patterns?.joinToString(", ") { pattern -> pattern.toMacroString() } ?: ""
     }
