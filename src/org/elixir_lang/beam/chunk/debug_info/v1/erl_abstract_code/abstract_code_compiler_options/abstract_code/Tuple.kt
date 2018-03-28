2609e0c210bb66fd1546b522f34e8119c797135a
Luke Imhoff <Kronic.Deth@gmail.com>
2018-03-27 21:05:37 -0500

    Show elements in tuples in clauses

--- src/org/elixir_lang/beam/chunk/debug_info/v1/erl_abstract_code/abstract_code_compiler_options/abstract_code/Tuple.kt
+++ src/org/elixir_lang/beam/chunk/debug_info/v1/erl_abstract_code/abstract_code_compiler_options/abstract_code/Tuple.kt
@@ -1,16 +1,23 @@
 package org.elixir_lang.beam.chunk.debug_info.v1.erl_abstract_code.abstract_code_compiler_options.abstract_code
 
+import com.ericsson.otp.erlang.OtpErlangList
 import com.ericsson.otp.erlang.OtpErlangObject
 import com.ericsson.otp.erlang.OtpErlangTuple
 
 private const val TAG = "tuple"
 
 class Tuple(term: OtpErlangTuple): Node(term) {
-    val elements by lazy { term.elements().drop(2).mapNotNull { Node.from(it) } }
+    val elements by lazy {
+        (term.elementAt(2) as? OtpErlangList)?.mapNotNull { Node.from(it) } ?:
+        emptyList()
+    }
 
-    override fun toMacroString(): String = "{${elements.joinToString(", ", transform = Node::toMacroString)}}"
+    override fun toMacroString(): String = elementsToMacroString(elements)
 
     companion object {
+        fun elementsToMacroString(elements: List<Node>): kotlin.String =
+                "{${elements.joinToString(", ", transform = Node::toMacroString)}}"
+
         fun from(term: OtpErlangObject) = ifTag(term, TAG, ::Tuple)
     }
 }
