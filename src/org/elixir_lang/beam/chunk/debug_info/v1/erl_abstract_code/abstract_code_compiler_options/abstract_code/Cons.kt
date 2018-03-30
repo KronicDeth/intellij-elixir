91464c25a67c1e1051af1ba3740b341a36aed555
Luke Imhoff <Kronic.Deth@gmail.com>
2018-03-29 23:04:07 -0500

    Collapse [head | [...]] to [head, ...]

--- src/org/elixir_lang/beam/chunk/debug_info/v1/erl_abstract_code/abstract_code_compiler_options/abstract_code/Cons.kt
+++ src/org/elixir_lang/beam/chunk/debug_info/v1/erl_abstract_code/abstract_code_compiler_options/abstract_code/Cons.kt
@@ -6,7 +6,9 @@ import org.elixir_lang.beam.chunk.debug_info.v1.erl_abstract_code.abstract_code_
 
 
 object Cons {
-    fun ifToMacroString(term: OtpErlangObject?): String? = AbstractCode.ifTag(term, TAG) { toMacroString(it) }
+    fun <T> ifTo(term: OtpErlangObject?, ifTrue: (OtpErlangTuple) -> T): T? = AbstractCode.ifTag(term, TAG, ifTrue)
+    fun ifToMacroString(term: OtpErlangObject?): String? = ifTo(term) { toMacroString(it) }
+    fun `is`(term: OtpErlangObject?): Boolean = ifTo(term) { true } ?: false
 
     fun toMacroString(term: OtpErlangObject): String =
             when (term) {
@@ -15,16 +17,9 @@ object Cons {
             }
 
     fun toMacroString(term: OtpErlangTuple): String {
-        val headMacroString = headMacroString(term)
-        val tail = toTail(term)
+        val headTailMacroString = headTailMacroString(term)
 
-        return if (Nil.`is`(tail)) {
-            "[$headMacroString]"
-        } else {
-            val tailMacroString = tailMacroString(term)
-
-            "[$headMacroString | $tailMacroString]"
-        }
+        return "[$headTailMacroString]"
     }
 
     private const val TAG = "cons"
@@ -33,6 +28,26 @@ object Cons {
             toHead(term)
                     ?.let { headToMacroString(it) }
                     ?: "missing_head"
+
+    private fun headTailMacroString(term: OtpErlangTuple): String {
+        val headMacroString = headMacroString(term)
+        val tail = toTail(term)
+
+        return when {
+            Nil.`is`(tail) -> headMacroString
+            Cons.`is`(tail) -> {
+                val tailHeadTailMacroString = Cons.headTailMacroString(tail as OtpErlangTuple)
+
+                "$headMacroString, $tailHeadTailMacroString"
+            }
+            else -> {
+                val tailMacroString = tailMacroString(term)
+
+                "$headMacroString | $tailMacroString"
+            }
+        }
+    }
+
 
     private fun headToMacroString(term: OtpErlangObject) = AbstractCode.toMacroString(term)
 
