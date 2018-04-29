bac9d23df7b0716adc8ff59e98039e91c625b110
Luke Imhoff <Kronic.Deth@gmail.com>
2018-04-29 15:25:24 -0500

    Allow calls as targets for FindUsageProvider
    Needed to counter not allowing them as targets in
    TargetElementEvaluator.  TargetElementEvaluator change was to prevent
    Goto Declaration doing Find Usages when there was only one target.

--- src/org/elixir_lang/UsageTargetProvider.kt
+++ src/org/elixir_lang/UsageTargetProvider.kt
@@ -1,5 +1,6 @@
 package org.elixir_lang
 
+import com.intellij.codeInsight.TargetElementUtil.adjustOffset
 import com.intellij.find.findUsages.PsiElement2UsageTargetAdapter
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.PsiElement
@@ -7,16 +8,29 @@ import com.intellij.psi.PsiFile
 import com.intellij.usages.UsageTarget
 import org.elixir_lang.beam.psi.impl.ModuleImpl
 import org.elixir_lang.psi.QualifiableAlias
+import org.elixir_lang.psi.call.Call
 
 class UsageTargetProvider : com.intellij.usages.UsageTargetProvider {
-    override fun getTargets(editor: Editor, file: PsiFile): Array<UsageTarget>? = null
+    override fun getTargets(editor: Editor, file: PsiFile): Array<UsageTarget>? {
+        val document = editor.document
+        val offset = editor.caretModel.offset
+        val adjustedOffset = adjustOffset(file, document, offset)
+
+        return file.findReferenceAt(adjustedOffset)?.element?.let { element ->
+            getTargets(element)
+        }
+    }
 
     override fun getTargets(psiElement: PsiElement): Array<UsageTarget>? =
         when (psiElement) {
+            is Call -> getTargets(psiElement)
             is ModuleImpl<*> -> getTargets(psiElement)
             is QualifiableAlias -> getTargets(psiElement)
             else -> null
         }
+
+    private fun getTargets(call: Call): Array<UsageTarget> =
+            PsiElement2UsageTargetAdapter(call).let { arrayOf(it) }
 
     private fun getTargets(moduleImpl: ModuleImpl<*>): Array<UsageTarget> =
             PsiElement2UsageTargetAdapter(moduleImpl).let { arrayOf(it) }
