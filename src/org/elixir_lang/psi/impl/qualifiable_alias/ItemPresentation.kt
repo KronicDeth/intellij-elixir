2ec5ce3a592b075a0b85cc46ced725bd8500e361
Luke Imhoff <Kronic.Deth@gmail.com>
2018-05-20 11:31:16 -0500

    Show full-unaliased name for multi-alias alias presentation

--- src/org/elixir_lang/psi/impl/qualifiable_alias/ItemPresentation.kt
+++ src/org/elixir_lang/psi/impl/qualifiable_alias/ItemPresentation.kt
@@ -2,38 +2,47 @@ package org.elixir_lang.psi.impl.qualifiable_alias
 
 import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.PsiElement
-import org.elixir_lang.psi.ElixirAccessExpression
-import org.elixir_lang.psi.ElixirNoParenthesesOneArgument
-import org.elixir_lang.psi.QualifiableAlias
+import org.elixir_lang.psi.*
 import org.elixir_lang.psi.call.Call
 import org.elixir_lang.psi.call.name.Function.ALIAS
 import org.elixir_lang.psi.call.name.Module.KERNEL
 import org.elixir_lang.psi.impl.locationString
+import org.elixir_lang.reference.module.UnaliasedName
 import javax.swing.Icon
 
 class ItemPresentation(private val qualifiableAlias: QualifiableAlias) : ItemPresentation {
-    override fun getLocationString(): String =
-        qualifiableAlias.containingFile.locationString(qualifiableAlias.project)
-
+    override fun getLocationString(): String = _locationString
     override fun getIcon(unused: Boolean): Icon? = null
+    override fun getPresentableText(): String? = _presentableText
 
-    override fun getPresentableText(): String? =
-            qualifiableAlias.parent.let { parent ->
+    private val _locationString by lazy {
+        qualifiableAlias.containingFile.locationString(qualifiableAlias.project)
+    }
+
+    private val _presentableText by lazy {
+        qualifiableAlias.parent.let { parent ->
                 when (parent) {
                     is ElixirAccessExpression -> getPresentableText(parent)
                     else -> qualifiableAlias.fullyQualifiedName()
                 }
             }
+    }
 
     private tailrec fun getPresentableText(ancestor: PsiElement): String? =
             when (ancestor) {
                 is Call ->
                         if (ancestor.isCalling(KERNEL, ALIAS)) {
-                            "alias ${qualifiableAlias.fullyQualifiedName()}"
+                            "alias ${UnaliasedName.unaliasedName(qualifiableAlias)}"
                         } else {
                             qualifiableAlias.fullyQualifiedName()
                         }
-                is ElixirAccessExpression, is ElixirNoParenthesesOneArgument -> getPresentableText(ancestor.parent)
-                else -> qualifiableAlias.fullyQualifiedName()
+                is Arguments,
+                is ElixirAccessExpression,
+                is ElixirMatchedParenthesesArguments,
+                is ElixirMultipleAliases,
+                is QualifiedMultipleAliases->
+                    getPresentableText(ancestor.parent)
+                else ->
+                    qualifiableAlias.fullyQualifiedName()
             }
 }
