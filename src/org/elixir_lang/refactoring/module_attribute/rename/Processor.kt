ece30cfb2b483758f17cc40981f38344089191b0
Luke Imhoff <Kronic.Deth@gmail.com>
2018-05-26 15:01:35 -0500

    Restore rename module attributes

--- src/org/elixir_lang/refactoring/module_attribute/rename/Processor.kt
+++ src/org/elixir_lang/refactoring/module_attribute/rename/Processor.kt
@@ -2,8 +2,8 @@ package org.elixir_lang.refactoring.module_attribute.rename
 
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.rename.RenamePsiElementProcessor
-import org.elixir_lang.refactoring.module_attribute.rename.Handler.Companion.isAvailable
+import org.elixir_lang.refactoring.module_attribute.rename.Handler.Companion.isAvailableOnResolved
 
 class Processor : RenamePsiElementProcessor() {
-    override fun canProcessElement(element: PsiElement): Boolean = isAvailable(element)
+    override fun canProcessElement(element: PsiElement): Boolean = isAvailableOnResolved(element)
 }
