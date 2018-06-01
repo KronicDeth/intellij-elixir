edc53c0a5c6ccebf600b06251cc4b12f937324aa
Luke Imhoff <Kronic.Deth@gmail.com>
2018-06-01 12:07:46 -0500

    Disable BarDual test for < 1.6

--- tests/org/elixir_lang/parser_definition/Elixir_1_6_5ParsingTestCase.kt
+++ tests/org/elixir_lang/parser_definition/Elixir_1_6_5ParsingTestCase.kt
@@ -1,12 +1,23 @@
 package org.elixir_lang.parser_definition
 
+import org.elixir_lang.Level.V_1_6
+import org.elixir_lang.test.ElixirVersion.elixirSdkLevel
+
+@Suppress("ClassName")
 class Elixir_1_6_5ParsingTestCase : ParsingTestCase() {
     override fun getTestDataPath(): String {
         return "testData/org/elixir_lang/parser_definition/elixir_1_6_5_parsing_test_case"
     }
 
     fun testBareDual() {
-        assertParsedAndQuotedCorrectly(true)
+        @Suppress("LocalVariableName")
+        val atLeastV_1_6 = elixirSdkLevel() >= V_1_6
+
+        if (atLeastV_1_6) {
+            assertParsedAndQuotedCorrectly(true)
+        } else {
+            assertTrue(!atLeastV_1_6)
+        }
     }
 
     fun testEmptyParentheses() {
