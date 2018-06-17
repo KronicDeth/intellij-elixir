e54c39d5db5df69a120bb8a56315fcf376f22284
Luke Imhoff <Kronic.Deth@gmail.com>
2018-06-17 00:27:23 -0500

    Delete works
    Don't use ColorProcessHandler because it's AnsiEscapeDecoder strips all
    backspace characters, so line rewriting doesn't work.  I'll look into
    line-rewriting with ANSI color codes later.

--- src/org/elixir_lang/iex/ProcessHandler.kt
+++ src/org/elixir_lang/iex/ProcessHandler.kt
@@ -1,11 +1,11 @@
 package org.elixir_lang.iex
 
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.execution.process.ColoredProcessHandler
+import com.intellij.execution.process.KillableProcessHandler
 import com.intellij.openapi.util.Key
 
-class ProcessHandler(commandLine: GeneralCommandLine) : ColoredProcessHandler(commandLine) {
-    override fun coloredTextAvailable(text: String, attributes: Key<*>) {
-        super.coloredTextAvailable(text.replace("\n", "\r\n"), attributes)
+class ProcessHandler(commandLine: GeneralCommandLine) : KillableProcessHandler(commandLine) {
+    override fun notifyTextAvailable(text: String, outputType: Key<*>) {
+        super.notifyTextAvailable(text.replace("\n", "\r\n"), outputType)
     }
 }
