6d6eab908461fc64bb923ada3831ae03d0e3fe87
Luke Imhoff <Kronic.Deth@gmail.com>
2018-06-22 23:07:44 -0500

    Log waiting and successful attachment of debugger

--- resources/debugged/lib/intellij_elixir/debugged.ex
+++ resources/debugged/lib/intellij_elixir/debugged.ex
@@ -5,10 +5,11 @@ IO.puts "Waiting for debugger to attach..."
 receive do
   {:"$gen_call", from, :continue} ->
     GenServer.reply(from, :ok)
+    IO.puts "... and debugger has attached."
 
     :ok
 after
   60_000 ->
-    IO.puts "Debugger failed to attach after 60 seconds"
+    IO.puts "... but, debugger failed to attach after 60 seconds."
     System.stop(1)
 end
