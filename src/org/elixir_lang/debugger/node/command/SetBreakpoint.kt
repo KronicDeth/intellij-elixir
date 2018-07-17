f1dad9c98c28d6c1d9da30dca977e2cfdae7cd94
Luke Imhoff <Kronic.Deth@gmail.com>
2018-07-16 19:34:20 -0500

    Ensure SetBreakpoint file is sent as UTF-8

--- src/org/elixir_lang/debugger/node/command/SetBreakpoint.kt
+++ src/org/elixir_lang/debugger/node/command/SetBreakpoint.kt
@@ -5,6 +5,7 @@ import com.ericsson.otp.erlang.OtpErlangBinary
 import com.ericsson.otp.erlang.OtpErlangInt
 import com.ericsson.otp.erlang.OtpErlangTuple
 import org.elixir_lang.debugger.node.Command
+import java.nio.charset.Charset
 
 /**
  * [file] is passed to Elixir debugger node, so that it can pass it back, so that [module]<->[file] correspondence only
@@ -18,6 +19,6 @@ class SetBreakpoint(private val module: String, line: Int, private val file: Str
                     OtpErlangAtom("set_breakpoint"),
                     OtpErlangAtom(module),
                     OtpErlangInt(line),
-                    OtpErlangBinary(file)
+                    OtpErlangBinary(file.toByteArray(Charset.forName("UTF-8")))
             ))
 }
