60a316529bd3ce19e57048ee37b413761c902a97
Luke Imhoff <Kronic.Deth@gmail.com>
2018-07-27 18:49:05 -0500

    Compatibility across versions

---
+++ src/org/elixir_lang/ReferencesSearch.java
@@ -0,0 +1,48 @@
+package org.elixir_lang;
+
+import com.intellij.openapi.application.QueryExecutorBase;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiPolyVariantReference;
+import com.intellij.psi.search.searches.ReferencesSearch.SearchParameters;
+import com.intellij.util.Processor;
+import kotlin.collections.CollectionsKt;
+import org.elixir_lang.psi.call.Named;
+import org.elixir_lang.structure_view.element.modular.Implementation;
+import org.elixir_lang.structure_view.element.modular.Module;
+import org.elixir_lang.structure_view.element.modular.Protocol;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public final class ReferencesSearch extends QueryExecutorBase<PsiPolyVariantReference, SearchParameters> {
+    public void processQuery(@NotNull SearchParameters queryParameters,
+                             /* Cannot use generics as
+                                < 2018.2 needs `Processor<PsiPolyVariantReference>` while
+                                >= 2018.2 needs `Processor<? super PsiPolyVariantReference>` */
+                             @NotNull @SuppressWarnings("unused") Processor processor) {
+        PsiElement elementToSearch = queryParameters.getElementToSearch();
+
+        if (elementToSearch instanceof Named) {
+            Named elementToSearchNamed = (Named) elementToSearch;
+
+            if (Implementation.is(elementToSearchNamed)
+                    || Module.is(elementToSearchNamed)
+                    || Protocol.is(elementToSearchNamed)) {
+                String name = elementToSearchNamed.getName();
+
+                if (name != null) {
+                    List relativeNames = org.elixir_lang.Module.split(name);
+
+                    if (relativeNames.size() > 1) {
+                        queryParameters.getOptimizer().searchWord(
+                                (String) CollectionsKt.last(relativeNames),
+                                queryParameters.getEffectiveSearchScope(),
+                                elementToSearchNamed.getLanguage().isCaseSensitive(),
+                                elementToSearchNamed
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
