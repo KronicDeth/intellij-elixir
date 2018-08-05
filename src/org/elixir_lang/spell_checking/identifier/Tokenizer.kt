50ca2177ceaf8cb338bf4c93580dada8b975e190
Luke Imhoff <Kronic.Deth@gmail.com>
2018-08-05 13:02:52 -0500

    Split identifiers to ignore ? and !

--- src/org/elixir_lang/spell_checking/identifier/Tokenizer.kt
+++ src/org/elixir_lang/spell_checking/identifier/Tokenizer.kt
@@ -1,12 +1,11 @@
 package org.elixir_lang.spell_checking.identifier
 
 import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.spellchecker.inspections.IdentifierSplitter
 import com.intellij.spellchecker.tokenizer.TokenConsumer
 import com.intellij.spellchecker.tokenizer.Tokenizer
 
 object Tokenizer : Tokenizer<LeafPsiElement>() {
     override fun tokenize(element: LeafPsiElement, consumer: TokenConsumer) {
-        consumer.consumeToken(element, true, IdentifierSplitter.getInstance())
+        consumer.consumeToken(element, true, Splitter)
     }
 }
