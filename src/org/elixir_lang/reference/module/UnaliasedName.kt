78ae9faaf1d5564d6892f42c3f318862de6f66ca
Luke Imhoff <Kronic.Deth@gmail.com>
2018-09-29 16:47:26 -0500

    alias __MODULE__, as: Mod completion
    Return enclosing module's name for __MODULE__'s Unaliased.unaliasedName.

--- src/org/elixir_lang/reference/module/UnaliasedName.kt
+++ src/org/elixir_lang/reference/module/UnaliasedName.kt
@@ -5,6 +5,7 @@ import com.intellij.psi.PsiNamedElement
 import org.elixir_lang.errorreport.Logger
 import org.elixir_lang.psi.*
 import org.elixir_lang.psi.call.Call
+import org.elixir_lang.psi.call.name.Function
 import org.elixir_lang.psi.call.name.Function.ALIAS
 import org.elixir_lang.psi.call.name.Module.KERNEL
 import org.elixir_lang.psi.impl.call.finalArguments
@@ -15,6 +16,12 @@ object UnaliasedName {
     fun unaliasedName(namedElement: PsiNamedElement): String? =
             if (namedElement is QualifiableAlias) {
                 unaliasedName(namedElement)
+            } else if (namedElement is Call && namedElement.isCalling(KERNEL, Function.__MODULE__, 0)) {
+                __MODULE__
+                        .reference(namedElement, useCall = null)
+                        .resolve()
+                        ?.let { it as? PsiNamedElement }
+                        ?.name
             } else {
                 namedElement.name
             }
