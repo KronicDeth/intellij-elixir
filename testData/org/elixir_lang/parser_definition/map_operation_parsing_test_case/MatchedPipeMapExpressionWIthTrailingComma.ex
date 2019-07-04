8c167fcd94b8d728ae0af89301b1374a4378883c
Luke Imhoff <Kronic.Deth@gmail.com>
2019-07-04 16:03:04 -0500

    Remove map updates that break in Elixir 1.8

--- testData/org/elixir_lang/parser_definition/map_operation_parsing_test_case/MatchedPipeMapExpressionWIthTrailingComma.ex
+++ testData/org/elixir_lang/parser_definition/map_operation_parsing_test_case/MatchedPipeMapExpressionWIthTrailingComma.ex
@@ -1,7 +1,2 @@
 %{ current | Update.function(), }
 %{ current | Update.function, }
-%{ current | update(), }
-%{ current | update, }
-
-%{ current | @Update.function(), }
-%{ current | @Update.function, }
