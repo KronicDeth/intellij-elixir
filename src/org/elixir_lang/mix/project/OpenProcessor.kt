5c01c3407c128ddf45eaa93edc9971bef31b8b74
Luke Imhoff <Kronic.Deth@gmail.com>
2019-08-15 13:10:26 -0500

    Don't use dependency injection for OpenProcessor builder
    Fixes #1527
    
    JetBrains has removed dependency injection for Extensions in 2019.2.

--- src/org/elixir_lang/mix/project/OpenProcessor.kt
+++ src/org/elixir_lang/mix/project/OpenProcessor.kt
@@ -2,14 +2,17 @@ package org.elixir_lang.mix.project
 
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.projectImport.ProjectImportBuilder
 import com.intellij.projectImport.ProjectOpenProcessorBase
 import org.elixir_lang.mix.project._import.Builder
 
 /**
  * Created by zyuyou on 15/7/1.
  */
-class OpenProcessor(builder: Builder) : ProjectOpenProcessorBase<Builder>(builder) {
+class OpenProcessor : ProjectOpenProcessorBase<Builder>() {
     override fun getSupportedExtensions(): Array<String> = arrayOf("mix.exs")
+
+    override fun doGetBuilder(): Builder = ProjectImportBuilder.EXTENSIONS_POINT_NAME.findExtensionOrFail(Builder::class.java)
 
     override fun doQuickImport(exsFile: VirtualFile, wizardContext: WizardContext): Boolean {
         val projectRoot = exsFile.parent
