29759c711f706c7b4b7248889b87c27ae6f0dfd9
Luke Imhoff <Kronic.Deth@gmail.com>
2020-10-09 14:20:08 -0500

    Fix error report formatting
    * Fix new lines around excerpts so that code blocks quote correctly
    * Use FILE:LINE format for excerpt location, so it can be used in editors for rapid lookup.

--- src/org/elixir_lang/errorreport/Logger.java
+++ src/org/elixir_lang/errorreport/Logger.java
@@ -66,42 +66,40 @@ public class Logger {
                 "```\n" +
                 element.getClass().getName() +
                 '\n' +
-                "```\n";
+                "```";
     }
 
     @NotNull
     private static String excerpt(@NotNull PsiFile containingFile, @NotNull PsiElement element) {
         StringBuilder excerptBuilder = new StringBuilder();
-        excerptBuilder.append('\n');
-        excerptBuilder.append("### Excerpt\n");
-        excerptBuilder.append('\n');
+        excerptBuilder
+                .append('\n')
+                .append("### Excerpt\n")
+                .append('\n');
 
-        excerptBuilder.append("```\n");
-        excerptBuilder.append(element.getText());
-        excerptBuilder.append('\n');
-        excerptBuilder.append("```\n");
+        excerptBuilder
+                .append("```\n")
+                .append(element.getText())
+                .append('\n')
+                .append("```\n");
 
         FileViewProvider fileViewProvider = containingFile.getViewProvider();
         Document document = fileViewProvider.getDocument();
 
         if (document != null) {
-            TextRange textRange = element.getTextRange();
-            int startingLine = document.getLineNumber(textRange.getStartOffset());
-            int endingLine = document.getLineNumber(textRange.getEndOffset());
-
-            excerptBuilder.append(" Line(s) ");
-            excerptBuilder.append(startingLine);
-            excerptBuilder.append('-');
-            excerptBuilder.append(endingLine);
-
             VirtualFile virtualFile = containingFile.getVirtualFile();
 
             if (virtualFile != null) {
-                excerptBuilder.append(" in ");
-                excerptBuilder.append(virtualFile.getPath());
-            }
+                String path = virtualFile.getPath();
+                TextRange textRange = element.getTextRange();
+                int startingLine = document.getLineNumber(textRange.getStartOffset());
+                int endingLine = document.getLineNumber(textRange.getEndOffset());
 
-            excerptBuilder.append("\n");
+                excerptBuilder
+                        .append('\n')
+                        .append("From: `").append(path).append(':').append(startingLine).append('`').append('\n')
+                        .append("To: `").append(path).append(':').append(endingLine).append('`');
+            }
         }
 
         return excerptBuilder.toString();
