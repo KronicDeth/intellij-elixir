b6101b6d26d2f0fa35c6054399012f56a20f19e2
Luke Imhoff <Kronic.Deth@gmail.com>
2020-10-16 21:28:37 -0500

    Remove use of ErrorBean

--- src/org/elixir_lang/github/issues/create/Request.kt
+++ src/org/elixir_lang/github/issues/create/Request.kt
@@ -1,142 +1,122 @@
-package org.elixir_lang.github.issues.create;
+package org.elixir_lang.github.issues.create
 
-import com.intellij.errorreport.bean.ErrorBean;
-import com.intellij.openapi.diagnostic.Attachment;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.diagnostic.Attachment
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.util.ExceptionUtil
 
-import java.util.List;
+class Request private constructor(val title: String, val body: String) {
+    constructor(additionalInfo: String?, throwableList: List<Throwable>, attachmentList: List<Attachment>) : this(TITLE, body(additionalInfo, throwableList, attachmentList))
 
-public class Request {
-    /*
-     * CONSTANTS
-     */
+    companion object {
+        private const val TITLE = "[auto-generated]"
 
-    private static final String TITLE = "[auto-generated]";
+        private fun body(additionalInfo: String?, throwableList: List<Throwable>, attachmentList: List<Attachment>): String {
+            val stringBuilder = StringBuilder()
+            val level = 0
+            version(stringBuilder)
+            additionalInfo(stringBuilder, level + 1, additionalInfo)
+            exceptions(stringBuilder, level + 1, throwableList)
+            attachments(stringBuilder, level + 1, attachmentList)
+            return stringBuilder.toString()
+        }
 
-    /*
-     * Static Methods
-     */
+        private fun version(stringBuilder: StringBuilder) {
+            header(stringBuilder, 1, "Version")
+            stringBuilder.append(PluginManagerCore.getPlugin(PluginId.getId("org.elixir_lang"))!!.version)
+            stringBuilder.append("\n\n")
+        }
 
-    private static void attachment(@NotNull StringBuilder stringBuilder, int level, Attachment attachment) {
-        header(stringBuilder, level, "`" + attachment.getPath() + "`");
+        private fun additionalInfo(stringBuilder: StringBuilder, level: Int, additionalInfo: String?) {
+            textSection(stringBuilder, level, "What I was doing", additionalInfo ?: "I don't know what I was doing")
+        }
 
-        stringBuilder.append(
-                "Please copy the contents of the above path into this report: files are too long to include in the " +
-                        "URL when opening the browser.  You can get the exact contents of that path when the error " +
-                        "occurred from the Attachments tab of the IDE Fatal Errors dialog that you had open before " +
-                        "clicking the button to submit this issue."
-        );
-    }
+        private fun exceptions(stringBuilder: StringBuilder, level: Int, throwableList: List<Throwable>) {
+            header(stringBuilder, level, "Exceptions")
+            throwableList.forEachIndexed { index, throwable ->
+                exception(stringBuilder, level + 1, index, throwable)
+            }
 
-    private static void attachments(@NotNull StringBuilder stringBuilder, int level, List<Attachment> attachmentList) {
-        if (!attachmentList.isEmpty()) {
-            header(stringBuilder, level, "Attachments");
+        }
 
-            for (Attachment attachment : attachmentList) {
-                attachment(stringBuilder, level + 1, attachment);
+        private fun exception(stringBuilder: StringBuilder, level: Int, index: Int, throwable: Throwable) {
+            header(stringBuilder, level, "Exception $index")
+            message(stringBuilder, level + 1, throwable.message)
+            stacktrace(stringBuilder, level + 1, throwable)
+        }
+
+        private fun attachment(stringBuilder: StringBuilder, level: Int, attachment: Attachment) {
+            header(stringBuilder, level, "`" + attachment.path + "`")
+            stringBuilder.append(
+                    "Please copy the contents of the above path into this report: files are too long to include in the " +
+                            "URL when opening the browser.  You can get the exact contents of that path when the error " +
+                            "occurred from the Attachments tab of the IDE Fatal Errors dialog that you had open before " +
+                            "clicking the button to submit this issue."
+            )
+        }
+
+        private fun attachments(stringBuilder: StringBuilder, level: Int, attachmentList: List<Attachment>) {
+            if (!attachmentList.isEmpty()) {
+                header(stringBuilder, level, "Attachments")
+                for (attachment in attachmentList) {
+                    attachment(stringBuilder, level + 1, attachment)
+                }
             }
         }
-    }
 
-    private static String body(ErrorBean errorBean) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int level = 0;
-
-        version(stringBuilder, errorBean.getPluginVersion());
-        description(stringBuilder, level + 1, errorBean.getDescription());
-        exception(stringBuilder, level + 1, errorBean);
-        attachments(stringBuilder, level + 1, errorBean.getAttachments());
-
-        return stringBuilder.toString();
-    }
-
-    private static void codeBlock(@NotNull StringBuilder stringBuilder, @NotNull String code) {
-        codeFence(stringBuilder);
-        stringBuilder.append(code);
-        stringBuilder.append('\n');
-        codeFence(stringBuilder);
-    }
-
-    private static void codeBlockSection(@NotNull StringBuilder stringBuilder,
-                                         int level,
-                                         @NotNull String name,
-                                         @Nullable String code) {
-        if (code != null && !code.isEmpty()) {
-            header(stringBuilder, level, name);
-            codeBlock(stringBuilder, code);
-        }
-    }
-
-    private static void codeFence(@NotNull StringBuilder stringBuilder) {
-        stringBuilder.append("```\n");
-    }
-
-    private static void description(@NotNull StringBuilder stringBuilder, int level, @Nullable String description) {
-        textSection(stringBuilder, level, "Description", description);
-    }
-
-    private static void exception(@NotNull StringBuilder stringBuilder, int level, @NotNull ErrorBean errorBean) {
-        header(stringBuilder, level, "Exception");
-        message(stringBuilder, level + 1, errorBean.getMessage());
-        stacktrace(stringBuilder, level + 1, errorBean.getStackTrace());
-    }
-
-    private static void header(@NotNull StringBuilder stringBuilder, int level, @NotNull String name) {
-        stringBuilder.append("\n");
-
-        for (int i = 0; i < level; i++) {
-            stringBuilder.append('#');
+        private fun codeBlock(stringBuilder: StringBuilder, code: String) {
+            codeFence(stringBuilder)
+            stringBuilder.append(code)
+            stringBuilder.append('\n')
+            codeFence(stringBuilder)
         }
 
-        stringBuilder.append(' ');
-        stringBuilder.append(name);
-        stringBuilder.append("\n\n");
-    }
-
-
-    private static void message(@NotNull StringBuilder stringBuilder, int level, @Nullable String message) {
-        textSection(stringBuilder, level, "Message", message);
-    }
-
-    private static void stacktrace(@NotNull StringBuilder stringBuilder, int level, @Nullable String stacktrace) {
-        codeBlockSection(stringBuilder, level, "Stacktrace", stacktrace);
-    }
-
-    private static void textSection(@NotNull StringBuilder stringBuilder,
-                                    int level,
-                                    @NotNull String name,
-                                    @Nullable String text) {
-        if (text != null && !text.isEmpty()) {
-            header(stringBuilder, level, name);
-            stringBuilder.append(text);
-            stringBuilder.append("\n\n");
+        private fun codeBlockSection(stringBuilder: StringBuilder,
+                                     level: Int,
+                                     name: String,
+                                     code: String) {
+            if (code.isNotEmpty()) {
+                header(stringBuilder, level, name)
+                codeBlock(stringBuilder, code)
+            }
         }
-    }
 
-    private static void version(@NotNull StringBuilder stringBuilder, @Nullable String version) {
-        if (version != null) {
-            header(stringBuilder, 1, "Version");
-            stringBuilder.append(version);
-            stringBuilder.append("\n\n");
+        private fun codeFence(stringBuilder: StringBuilder) {
+            stringBuilder.append("```\n")
         }
-    }
 
-    /*
-     * Fields
-     */
+        private fun header(stringBuilder: StringBuilder, level: Int, name: String) {
+            stringBuilder.append("\n")
+            for (i in 0 until level) {
+                stringBuilder.append('#')
+            }
+            stringBuilder.append(' ')
+            stringBuilder.append(name)
+            stringBuilder.append("\n\n")
+        }
 
-    @NotNull
-    public final String body;
-    @NotNull
-    public final String title;
+        private fun message(stringBuilder: StringBuilder, level: Int, message: String?) {
+            textSection(stringBuilder, level, "Message", message)
+        }
 
-    public Request(@NotNull ErrorBean errorBean) {
-        this(TITLE, body(errorBean));
-    }
+        private fun stacktrace(stringBuilder: StringBuilder, level: Int, throwable: Throwable) {
+            stacktrace(stringBuilder, level, ExceptionUtil.getThrowableText(throwable))
+        }
 
-    private Request(@NotNull String title, @NotNull String body) {
-        this.body = body;
-        this.title = title;
+        private fun stacktrace(stringBuilder: StringBuilder, level: Int, stacktrace: String) {
+            codeBlockSection(stringBuilder, level, "Stacktrace", stacktrace)
+        }
+
+        private fun textSection(stringBuilder: StringBuilder,
+                                level: Int,
+                                name: String,
+                                text: String?) {
+            if (text != null && text.isNotEmpty()) {
+                header(stringBuilder, level, name)
+                stringBuilder.append(text)
+                stringBuilder.append("\n\n")
+            }
+        }
+
     }
 }
