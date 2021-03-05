1afff9d42ada4822a98b7f7731969677fe575415
Luke Imhoff <Kronic.Deth@gmail.com>
2021-03-04 23:05:10 -0600

    Limit Module resolution to same JetBrains Project Module
    * Change the `GlobalSearchScope` from `allProject` to `moduleWithDependenciesAndLibrariesScope` for faster searching on multi-module projects.
      * Set `includeTests` based whether the referring element is in a test directory.
    * Use `StubIndex#processElements` instead of home grown `forEachNavigationElement` as `processElements` is more efficient.

--- src/org/elixir_lang/reference/resolver/Module.kt
+++ src/org/elixir_lang/reference/resolver/Module.kt
@@ -1,11 +1,16 @@
 package org.elixir_lang.reference.resolver
 
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.module.ModuleUtil
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.impl.source.resolve.ResolveCache
-import org.elixir_lang.Reference.forEachNavigationElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.stubs.StubIndex
 import org.elixir_lang.navigation.isDecompiled
+import org.elixir_lang.psi.NamedElement
 import org.elixir_lang.psi.scope.module.MultiResolve
+import org.elixir_lang.psi.stub.index.ModularName
 
 
 object Module : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Module> {
@@ -22,25 +27,34 @@ object Module : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Modul
                         sameFileResolveResultList
                     } else {
                         multiResolveProject(
-                                element.project,
+                                element,
                                 name
                         )
                     }
                 }
             }
 
-    private fun multiResolveProject(project: Project,
+    private fun multiResolveProject(entrance: PsiElement,
                                     name: String): Array<PsiElementResolveResult> {
         val resolveResultList = mutableListOf<PsiElementResolveResult>()
 
-        forEachNavigationElement(
-                project,
-                name
-        ) { navigationElement ->
-            resolveResultList.add(PsiElementResolveResult(navigationElement))
+        val project = entrance.project
+        val globalSearchScope = ModuleUtil
+                .findModuleForPsiElement(entrance)
+                ?.let { module ->
+                    val includeTests = ProjectRootManager.getInstance(project).fileIndex.isInTestSourceContent(entrance.containingFile.virtualFile)
+                    GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module, includeTests)
+                }
+                ?:
+                GlobalSearchScope.allScope(project)
 
-            true
-        }
+        StubIndex
+                .getInstance()
+                .processElements(ModularName.KEY, name, project, globalSearchScope, null, NamedElement::class.java) { namedElement ->
+                    /* The namedElement may be a ModuleImpl from a .beam.  Using #getNaviationElement() ensures a source
+                       (either true source or decompiled) is used. */
+                    resolveResultList.add(PsiElementResolveResult(namedElement.navigationElement))
+                }
 
         val sourceResolveResultList = resolveResultList.filter { !it.element.isDecompiled() }
 
