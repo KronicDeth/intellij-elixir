52e50bf2950ec74679b1affb51b1dd2e66037e2c
Luke Imhoff <Kronic.Deth@gmail.com>
2021-04-11 11:21:12 -0500

    whileIn(Sequence<T>, ...)

--- gen/org/elixir_lang/psi/scope/WhileIn.kt
+++ gen/org/elixir_lang/psi/scope/WhileIn.kt
@@ -1,5 +1,7 @@
 package org.elixir_lang.psi.scope
 
+import com.intellij.psi.PsiElement
+
 object WhileIn {
     inline fun <T> whileIn(array: Array<T>, keepProcessing: (element: T) -> Boolean): Boolean {
         var accumlatedKeepProcessing = true
@@ -19,6 +21,20 @@ object WhileIn {
         var accumlatedKeepProcessing = true
 
         for (element in list) {
+            accumlatedKeepProcessing = keepProcessing(element)
+
+            if (!accumlatedKeepProcessing) {
+                break
+            }
+        }
+
+        return accumlatedKeepProcessing
+    }
+
+    inline fun <T> whileIn(sequence: Sequence<T>, keepProcessing: (element: T) -> Boolean): Boolean {
+        var accumlatedKeepProcessing = true
+
+        for (element in sequence) {
             accumlatedKeepProcessing = keepProcessing(element)
 
             if (!accumlatedKeepProcessing) {
