c9f1ed8ad7c063980c835e6a932a5f0e2ca3cb62
Luke Imhoff <Kronic.Deth@gmail.com>
2021-04-11 11:31:58 -0500

    Resolve type `t` for `defprotocol` to where it is defined in `Protocol.__protocol__`
    Unfortunately, this ties all protocol's `t` to the same element, so Find Usage finds all protocol's types instead of just a specific module's type, but this works for projects that have source, but not yet compiled.

--- gen/org/elixir_lang/psi/scope/Type.kt
+++ gen/org/elixir_lang/psi/scope/Type.kt
@@ -1,22 +1,30 @@
 package org.elixir_lang.psi.scope
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiPolyVariantReference
 import com.intellij.psi.ResolveState
 import com.intellij.psi.impl.source.tree.CompositeElement
 import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.util.isAncestor
 import com.intellij.psi.util.siblings
+import org.elixir_lang.errorreport.Logger
 import org.elixir_lang.psi.*
 import org.elixir_lang.psi.call.Call
+import org.elixir_lang.psi.call.name.Function.DEFMODULE
+import org.elixir_lang.psi.call.name.Module.KERNEL
+import org.elixir_lang.psi.call.qualification.Qualified
 import org.elixir_lang.psi.impl.ElixirPsiImplUtil.ENTRANCE
 import org.elixir_lang.psi.impl.call.macroChildCalls
 import org.elixir_lang.psi.impl.identifierName
 import org.elixir_lang.psi.operation.Type
+import org.elixir_lang.psi.scope.WhileIn.whileIn
+import org.elixir_lang.psi.stub.type.call.Stub.isModular
 import org.elixir_lang.reference.ModuleAttribute
 import org.elixir_lang.reference.ModuleAttribute.Companion.isCallbackName
 import org.elixir_lang.reference.ModuleAttribute.Companion.isSpecificationName
 import org.elixir_lang.reference.ModuleAttribute.Companion.isTypeName
 import org.elixir_lang.structure_view.element.modular.Module
+import org.elixir_lang.structure_view.element.modular.Protocol
 
 abstract class Type : PsiScopeProcessor {
     override fun execute(element: PsiElement, state: ResolveState): Boolean =
@@ -45,13 +53,17 @@ abstract class Type : PsiScopeProcessor {
             when (call) {
                 is UnqualifiedNoArgumentsCall<*> -> executeOnParameter(call, state)
                 is AtUnqualifiedNoParenthesesCall<*> -> execute(call, state)
-                else -> if (Module.`is`(call) && call.isAncestor(state.get(ENTRANCE), false)) {
+                else -> if (isModular(call) && call.isAncestor(state.get(ENTRANCE), false)) {
                     val childCalls = call.macroChildCalls()
+                    val childCallsKeepProcessing = whileIn(childCalls) {
+                        execute(it, state)
+                    }
 
-                    for (childCall in childCalls) {
-                        if (!execute(childCall, state)) {
-                            break
-                        }
+                    // specialization
+                    val defprotocolKeepProcessing = if (childCallsKeepProcessing && Protocol.`is`(call)) {
+                        executeOnDefprotocolCall(call, state)
+                    } else {
+                        childCallsKeepProcessing
                     }
 
                     keepProcessing()
@@ -86,6 +98,80 @@ abstract class Type : PsiScopeProcessor {
                     .takeWhile { it }
                     .lastOrNull()
                     ?: true
+
+    private fun executeOnDefprotocolCall(call: Call, state: ResolveState): Boolean =
+            call.reference?.let { reference ->
+                when (reference) {
+                    is PsiPolyVariantReference -> {
+                        val resolveResults = reference.multiResolve(false)
+
+                        whileIn(resolveResults) { resolveResult ->
+                            resolveResult.element?.let { it as? Call }?.let {
+                                executeOnDefprotocolDefinition(it, state)
+                            } ?: true
+                        }
+                    }
+                    else -> TODO()
+                }
+            } ?: true
+
+    private fun executeOnDefprotocolDefinition(descendant: PsiElement, state: ResolveState): Boolean =
+        when (descendant) {
+            is Call -> {
+                val doBlock = descendant.doBlock
+
+                if (doBlock != null) {
+                    doBlock
+                            .stab
+                            ?.stabBody
+                            ?.firstChild
+                            ?.siblings()
+                            ?.filter { it.node is CompositeElement }
+                            ?.map { executeOnDefprotocolDefinitionExpression(it, state) }
+                            ?.takeWhile { it }
+                            ?.lastOrNull()
+                            ?: true
+                } else {
+                    true
+                }
+            }
+            else -> {
+                true
+            }
+        }
+
+    private fun executeOnDefprotocolDefinitionExpression(expression: PsiElement, state: ResolveState): Boolean =
+        when (expression) {
+            is Qualified ->
+                if (expression.isCalling("Protocol", "__protocol__", 2)) {
+                    expression.reference?.let { it as PsiPolyVariantReference }?.multiResolve(false)?.let { resolveResults ->
+                        whileIn(resolveResults) { resolveResult ->
+                            resolveResult.element?.let { it as? Call }?.let { __protocol__ ->
+                                Using.treeWalkUp(
+                                        __protocol__,
+                                        useCall = null,
+                                        resolveState = state,
+                                        keepProcessing = ::executeOn__protocol__DefinitionExpression
+                                )
+                            } ?: true
+                        }
+                    } ?: true
+                } else {
+                    true
+                }
+            else -> true
+        }
+
+    private fun executeOn__protocol__DefinitionExpression(expression: PsiElement, state: ResolveState): Boolean =
+        expression.let { it as? Call }?.let { call ->
+            if (Module.`is`(call)) {
+                // treat the module the same as an inline quote block as the `defprotocol` will replace itself with
+                // the `defmodule`
+                QuoteMacro.treeWalkUp(call, state, ::execute)
+            } else {
+                true
+            }
+        } ?: true
 }
 
 internal tailrec fun PsiElement.ancestorTypeSpec(): AtUnqualifiedNoParenthesesCall<*>? =
@@ -134,12 +220,18 @@ internal tailrec fun PsiElement.ancestorTypeSpec(): AtUnqualifiedNoParenthesesCa
             is ElixirInterpolation,
                 // Map updates aren't used in type specifications unlike `ElixirMapConstructionArguments`
             is ElixirMapUpdateArguments,
+                // `@{:__aliases__, _meta, _args}` in `defmacro @{:__aliases__, _meta, _args} do...`
+            is ElixirMatchedAtOperation,
                 // types can't be defined at the file level and must be inside modules.
             is ElixirFile,
                 // Any stab body has to be parent of a type
-            is ElixirStabBody -> null
+            is ElixirStabBody,
+            // __MODULE__.* does not matter that it is in a type
+            is QualifiableAlias -> null
             else -> {
-                TODO()
+                Logger.error(PsiElement::class.java, "Don't know how to find ancestorTypeSpec", this)
+
+                null
             }
         }
 
