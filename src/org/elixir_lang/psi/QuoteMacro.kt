0abcf2aee705521131ef53229c5d86e307f0e91d
Luke Imhoff <Kronic.Deth@gmail.com>
2021-04-11 11:25:38 -0500

    Descend into body of `unless` in `quote` for treeWalkUp

--- src/org/elixir_lang/psi/QuoteMacro.kt
+++ src/org/elixir_lang/psi/QuoteMacro.kt
@@ -2,11 +2,14 @@ package org.elixir_lang.psi
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveState
+import com.intellij.psi.impl.source.tree.CompositeElement
+import com.intellij.psi.util.siblings
 import org.elixir_lang.psi.call.Call
 import org.elixir_lang.psi.call.name.Function.QUOTE
 import org.elixir_lang.psi.call.name.Module.KERNEL
 import org.elixir_lang.psi.impl.call.macroChildCallSequence
 import org.elixir_lang.psi.scope.CallDefinitionClause
+import org.elixir_lang.psi.scope.WhileIn.whileIn
 
 object QuoteMacro {
     fun treeWalkUp(quoteCall: Call, resolveState: ResolveState, keepProcessing: (PsiElement, ResolveState) -> Boolean): Boolean {
@@ -28,6 +31,15 @@ object QuoteMacro {
                     val childResolveState = resolveState.putVisitedElement(childCall)
 
                     Use.treeWalkUp(childCall, childResolveState, keepProcessing)
+                }
+                childCall.isCalling(KERNEL, "unless") -> {
+                    childCall.doBlock?.let { doBlock ->
+                        doBlock.stab?.stabBody?.firstChild?.siblings()?.filter { it.node is CompositeElement }?.let { unlessExpressions ->
+                            whileIn(unlessExpressions) { unlessExpression ->
+                                keepProcessing(unlessExpression, resolveState)
+                            }
+                        }
+                    } ?: true
                 }
                 else -> keepProcessing(childCall, resolveState)
             }
