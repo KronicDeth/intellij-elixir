dbcede45197dc0c1a91dad6a0d67eb4f8015cdf0
Luke Imhoff <Kronic.Deth@gmail.com>
2021-04-12 23:23:32 -0500

    Resolve qualified calls with unknown resolvable qualifier using only relative identifier and arity

--- src/org/elixir_lang/reference/resolver/Callable.kt
+++ src/org/elixir_lang/reference/resolver/Callable.kt
@@ -1,12 +1,18 @@
 package org.elixir_lang.reference.resolver
 
+import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.ResolveResult
 import com.intellij.psi.impl.source.resolve.ResolveCache
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.stubs.StubIndex
 import org.elixir_lang.errorreport.Logger
+import org.elixir_lang.psi.CallDefinitionClause.nameArityRange
+import org.elixir_lang.psi.NamedElement
 import org.elixir_lang.psi.UnqualifiedNoArgumentsCall
 import org.elixir_lang.psi.call.Call
 import org.elixir_lang.psi.call.qualification.Qualified
 import org.elixir_lang.psi.impl.call.qualification.qualifiedToModulars
+import org.elixir_lang.psi.stub.index.AllName
 
 object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Callable> {
     override fun resolve(callable: org.elixir_lang.reference.Callable, incompleteCode: Boolean): Array<ResolveResult> {
@@ -62,8 +68,11 @@ object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Cal
         return resolveResultList
     }
 
-    private fun resolveQualified(element: Qualified, name: String, incompleteCode: Boolean): List<ResolveResult> =
-            element.qualifiedToModulars().flatMap { modular ->
+    private fun resolveQualified(element: Qualified, name: String, incompleteCode: Boolean): List<ResolveResult> {
+        val modulars = element.qualifiedToModulars()
+
+        return if (modulars.isNotEmpty()) {
+            modulars.flatMap { modular ->
                 val resolvedFinalArity = element.resolvedFinalArity()
 
                 org.elixir_lang.psi.scope.call_definition_clause.MultiResolve.resolveResults(
@@ -73,4 +82,45 @@ object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Cal
                         modular
                 )
             }
+        } else {
+            val relativeIdentifier = element.relativeIdentifier
+
+            if (relativeIdentifier.children.isEmpty()) {
+                val name = relativeIdentifier.text
+                val project = element.project
+                val keys = mutableListOf<String>()
+
+                StubIndex.getInstance().processAllKeys(AllName.KEY, project) { key ->
+                    if ((incompleteCode && key.startsWith(name)) || key == name) {
+                        keys.add(key)
+                    }
+
+                    true
+                }
+
+                val arity = element.resolvedFinalArity()
+                val resolveResults = mutableListOf<ResolveResult>()
+                // results are never valid because the qualifier is unknown
+                val validResult = false
+
+                for (key in keys) {
+                    StubIndex.getInstance().processElements(AllName.KEY, key, project, GlobalSearchScope.allScope(project), NamedElement::class.java) { namedElement ->
+                        if (namedElement is Call &&
+                                org.elixir_lang.psi.CallDefinitionClause.`is`(namedElement) &&
+                                (incompleteCode || nameArityRange(namedElement)?.arityRange?.contains(arity) == true)) {
+                            resolveResults.add(PsiElementResolveResult(namedElement, validResult))
+                        }
+
+                        true
+                    }
+                }
+
+                resolveResults
+            } else {
+                Logger.error(Callable::class.java, "Don't know how to calculate name for relativeIdentifier in qualified element", element)
+
+                emptyList()
+            }
+        }
+    }
 }
