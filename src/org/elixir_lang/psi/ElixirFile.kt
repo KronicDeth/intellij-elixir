15930991c811d75efd88710f87e13ca4f28eae73
Luke Imhoff <Kronic.Deth@gmail.com>
2021-04-23 23:00:34 -0500

    viewFile for .eex files used in same directory as .ex using EEx.function_from_file

--- src/org/elixir_lang/psi/ElixirFile.kt
+++ src/org/elixir_lang/psi/ElixirFile.kt
@@ -3,14 +3,17 @@ package org.elixir_lang.psi
 import org.elixir_lang.psi.impl.ProcessDeclarationsImpl
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.openapi.fileTypes.FileType
-import com.intellij.psi.FileViewProvider
+import com.intellij.psi.*
 import org.elixir_lang.ElixirLanguage
 import org.elixir_lang.ElixirFileType
-import com.intellij.psi.ResolveState
-import com.intellij.psi.PsiElement
 import com.intellij.psi.scope.PsiScopeProcessor
 import org.elixir_lang.psi.call.StubBased
 import com.intellij.psi.util.PsiTreeUtil
+import org.elixir_lang.psi.call.Call
+import org.elixir_lang.psi.call.qualification.Qualified
+import org.elixir_lang.psi.impl.call.finalArguments
+import org.elixir_lang.psi.impl.call.stabBodyChildExpressions
+import org.elixir_lang.psi.impl.childExpressions
 import org.elixir_lang.structure_view.element.modular.Implementation
 import org.elixir_lang.structure_view.element.modular.Module
 import org.elixir_lang.structure_view.element.modular.Protocol
@@ -32,16 +35,73 @@ class ElixirFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, Eli
     /**
      * If this file is a LEEx template (`*.html.leex`), then this is the file that should contain the view module.
      */
-    fun viewFile(): ElixirFile? = if (virtualFile?.fileType == org.elixir_lang.leex.file.Type.INSTANCE) {
-        containingFile.parent?.let { directory ->
-            val nameWithoutExtension = containingFile.name.removeSuffix(".html.leex")
-            val liveName = "${nameWithoutExtension}.ex"
+    fun viewFile(): ElixirFile? =
+            when (virtualFile?.fileType) {
+                org.elixir_lang.eex.file.Type.INSTANCE -> {
+                    containingFile.parent?.let { directory ->
+                        val files = directory.files
 
-            directory.findFile(liveName) as? ElixirFile
+                        findUsesEExFile(files)
+                    }
+                }
+                org.elixir_lang.leex.file.Type.INSTANCE -> {
+                    containingFile.parent?.let { directory ->
+                        val nameWithoutExtension = containingFile.name.removeSuffix(".html.leex")
+                        val liveName = "${nameWithoutExtension}.ex"
+
+                        directory.findFile(liveName) as? ElixirFile
+                    }
+                }
+                else -> null
+            }
+
+    private fun findUsesEExFile(files: Array<PsiFile>): ElixirFile? =
+            files.filterIsInstance<ElixirFile>().find { elixirFile ->
+                usesEExFile(elixirFile)
+            }
+
+    private fun usesEExFile(file: ElixirFile): Boolean =
+        file.childExpressions().filterIsInstance<Call>().any { call ->
+            if (Module.`is`(call)) {
+                moduleUsesEExFile(call)
+            } else {
+                false
+            }
         }
-    } else {
-        null
-    }
+
+    private fun moduleUsesEExFile(module: Call): Boolean =
+            module.stabBodyChildExpressions()?.filterIsInstance<Qualified>()?.any { call ->
+                // `function_from_file(kind, name, file, args \\ [], options \\ [])`
+                if (call.qualifier().let { it as? ElixirAlias }?.name == "EEx" && call.functionName() == "function_from_file" &&
+                        call.resolvedFinalArity() in 3..5) {
+                    eexFunctionFromFileUsesEExFile(call)
+                } else {
+                    false
+                }
+            } ?: false
+
+    private fun eexFunctionFromFileUsesEExFile(call: Call): Boolean =
+        call.finalArguments()?.let { arguments ->
+            eexFunctionFromFileArgumentUsesEExFile(arguments[2])
+        } ?: false
+
+    private fun eexFunctionFromFileArgumentUsesEExFile(argument: PsiElement): Boolean =
+       when (argument) {
+           is Qualified -> {
+               argument.qualifier().let { it as? ElixirAlias }?.name == "Path" &&
+                       argument.functionName() == "expand" &&
+                       argument.resolvedFinalArity() == 2 &&
+                       pathExpandUsesEExFile(argument)
+           }
+           else -> {
+               false
+           }
+       }
+
+    private fun pathExpandUsesEExFile(pathExpand: Call): Boolean =
+        pathExpand.finalArguments()?.let { arguments ->
+            arguments[0].text == "\"${virtualFile?.name}\"" && arguments[1].text == "__DIR__"
+        } ?: false
 
     /**
      * @return modulars owned (declared) by this element.
