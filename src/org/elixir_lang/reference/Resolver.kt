7d2c96ee4744787e6f23e36db635d09b07dd4f14
Luke Imhoff <Kronic.Deth@gmail.com>
2021-05-10 22:01:20 -0500

    Find module for elements in libraries
    Elements in libraries don't have a Module from ModuleUtil.findModuleForPsiElement, so scan the element's project's module's content roots for the closest Module for libraries.

--- src/org/elixir_lang/reference/Resolver.kt
+++ src/org/elixir_lang/reference/Resolver.kt
@@ -1,8 +1,11 @@
 package org.elixir_lang.reference
 
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveResult
 import org.elixir_lang.navigation.isDecompiled
@@ -39,13 +42,55 @@ object Resolver {
             resolveResultList.filter(ResolveResult::isValidResult)
 
     private fun <T : ResolveResult> filterUnderSameModule(element: PsiElement, resolveResultList: List<T>): List<T> = if (resolveResultList.isNotEmpty()) {
-        ModuleUtil
-                .findModuleForPsiElement(element)
+        moduleForSourceOrLibrary(element)
                 ?.let { module -> filterUnderModule(module, resolveResultList) }
                 ?: emptyList()
     } else {
         resolveResultList
     }
+
+    // `ModuleUtil.findModuleForPsiElement` returns `null` for library source, so need to find module a different way if a library
+    private fun moduleForSourceOrLibrary(element: PsiElement): com.intellij.openapi.module.Module? =
+        ModuleUtil.findModuleForPsiElement(element) ?:
+        element
+                .containingFile
+                .virtualFile
+                ?.takeIf { ProjectFileIndex.getInstance(element.project).isInLibrary(it) }
+                ?.let { virtualFile ->
+                    ModuleManager
+                            .getInstance(element.project)
+                            .modules
+                            .asSequence()
+                            .mapNotNull { module ->
+                                val contentRootSet = ModuleRootManager.getInstance(module).contentRoots.toSet()
+
+                                depth(virtualFile, contentRootSet)
+                                        ?.let { depth -> module to depth }
+                            }
+                            .minBy { (_, depth) -> depth }
+                            ?.first
+                }
+
+    private fun depth(virtualFile: VirtualFile, roots: Set<VirtualFile>?): Int? =
+        if (roots != null && roots.isNotEmpty()) {
+            var ancestor: VirtualFile? = virtualFile
+            var finalDepth: Int? = null
+            var currentDepth = 0
+
+            while (ancestor != null) {
+                if (roots.contains(ancestor)) {
+                    finalDepth = currentDepth
+                    break
+                }
+
+                ancestor = ancestor.parent
+                currentDepth++
+            }
+
+            finalDepth
+        } else {
+            null
+        }
 
     private fun <T : ResolveResult> filterUnderModule(module: com.intellij.openapi.module.Module, resolveResultList: List<T>): List<T> = if (resolveResultList.isNotEmpty()) {
         val contentRootSet = ModuleRootManager.getInstance(module).contentRoots.toSet()
