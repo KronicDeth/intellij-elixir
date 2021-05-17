f4dde6ba18728e2648f875e05316bee29faca23f
Luke Imhoff <Kronic.Deth@gmail.com>
2021-05-17 12:03:39 -0500

    Always fallback to name/arity in any module if callable not resolved in scope

--- src/org/elixir_lang/reference/resolver/Callable.kt
+++ src/org/elixir_lang/reference/resolver/Callable.kt
@@ -6,11 +6,13 @@ import com.intellij.psi.impl.source.resolve.ResolveCache
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
 import org.elixir_lang.errorreport.Logger
+import org.elixir_lang.psi.CallDefinitionClause
 import org.elixir_lang.psi.CallDefinitionClause.nameArityRange
 import org.elixir_lang.psi.NamedElement
 import org.elixir_lang.psi.UnqualifiedNoArgumentsCall
 import org.elixir_lang.psi.call.Call
 import org.elixir_lang.psi.call.qualification.Qualified
+import org.elixir_lang.psi.impl.ElixirPsiImplUtil
 import org.elixir_lang.psi.impl.call.qualification.qualifiedToModulars
 import org.elixir_lang.psi.stub.index.AllName
 
@@ -18,25 +20,40 @@ object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Cal
     override fun resolve(callable: org.elixir_lang.reference.Callable, incompleteCode: Boolean): Array<ResolveResult> {
         val element = callable.element
 
-        /* DO NOT use `getName()` as it will return the NameIdentifier's text, which for `defmodule` is the Alias, not
-           `defmodule` */
-        val resolveResultList = element.functionName()?.let { name ->
-            try {
-                if (element is Qualified) {
-                    resolveQualified(element, name, incompleteCode)
-                } else {
-                    resolveUnqualified(element, name, incompleteCode)
-                }
-            } catch (stackOverflowError: StackOverflowError) {
-                Logger.error(Callable::class.java, "StackOverflowError when annotating Call", element)
-
-                null
-            }
-        } ?: emptyList()
-
-
-        return org.elixir_lang.reference.Resolver.preferred(element, incompleteCode, resolveResultList).toTypedArray()
+        return resolvePreferred(element, incompleteCode).toTypedArray()
     }
+
+    private fun resolvePreferred(element: Call, incompleteCode: Boolean): List<ResolveResult> {
+        val all = resolveAll(element, incompleteCode)
+
+        return org.elixir_lang.reference.Resolver.preferred(element, incompleteCode, all)
+    }
+
+    private fun resolveAll(element: Call, incompleteCode: Boolean) =
+            /* DO NOT use `getName()` as it will return the NameIdentifier's text, which for `defmodule` is the Alias,
+               not `defmodule` */
+            element
+                    .functionName()
+                    ?.let { name -> resolve(element, name, incompleteCode) }
+                    ?: emptyList()
+
+    private fun resolve(element: Call, name: String, incompleteCode: Boolean) =
+        resolveInScope(element, name, incompleteCode)
+                .takeIf(Collection<ResolveResult>::isNotEmpty)
+                ?: nameArityInAnyModule(element, name, incompleteCode)
+
+    private fun resolveInScope(element: Call, name: String, incompleteCode: Boolean): List<ResolveResult> =
+        try {
+            if (element is Qualified) {
+                resolveQualified(element, name, incompleteCode)
+            } else {
+                resolveUnqualified(element, name, incompleteCode)
+            }
+        } catch (stackOverflowError: StackOverflowError) {
+            Logger.error(Callable::class.java, "StackOverflowError when annotating Call", element)
+
+            emptyList()
+        }
 
     private fun resolveUnqualified(element: Call, name: String, incompleteCode: Boolean): List<ResolveResult> {
         val resolvedFinalArity = element.resolvedFinalArity()
@@ -51,9 +68,7 @@ object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Cal
                     element
             )
 
-            if (variableResolveList != null) {
-                resolveResultList.addAll(variableResolveList)
-            }
+            resolveResultList.addAll(variableResolveList)
         }
 
         val callDefinitionClauseResolveResultList = org.elixir_lang.psi.scope.call_definition_clause.MultiResolve.resolveResults(
@@ -68,11 +83,8 @@ object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Cal
         return resolveResultList
     }
 
-    private fun resolveQualified(element: Qualified, name: String, incompleteCode: Boolean): List<ResolveResult> {
-        val modulars = element.qualifiedToModulars()
-
-        return if (modulars.isNotEmpty()) {
-            modulars.flatMap { modular ->
+    private fun resolveQualified(element: Qualified, name: String, incompleteCode: Boolean): List<ResolveResult> =
+            element.qualifiedToModulars().flatMap { modular ->
                 val resolvedFinalArity = element.resolvedFinalArity()
 
                 org.elixir_lang.psi.scope.call_definition_clause.MultiResolve.resolveResults(
@@ -82,45 +94,38 @@ object Callable : ResolveCache.PolyVariantResolver<org.elixir_lang.reference.Cal
                         modular
                 )
             }
-        } else {
-            val relativeIdentifier = element.relativeIdentifier
 
-            if (relativeIdentifier.children.isEmpty()) {
-                val name = relativeIdentifier.text
-                val project = element.project
-                val keys = mutableListOf<String>()
+    private fun nameArityInAnyModule(element: Call, name: String, incompleteCode: Boolean): List<ResolveResult> {
+        val project = element.project
+        val keys = mutableListOf<String>()
+        val stubIndex = StubIndex.getInstance()
 
-                StubIndex.getInstance().processAllKeys(AllName.KEY, project) { key ->
-                    if ((incompleteCode && key.startsWith(name)) || key == name) {
-                        keys.add(key)
-                    }
+        stubIndex.processAllKeys(AllName.KEY, project) { key ->
+            if ((incompleteCode && key.startsWith(name)) || key == name) {
+                keys.add(key)
+            }
 
-                    true
+            true
+        }
+
+        val scope = GlobalSearchScope.allScope(project)
+        val arity = element.resolvedFinalArity()
+        val resolveResults = mutableListOf<ResolveResult>()
+        // results are never valid because the qualifier is unknown
+        val validResult = false
+
+        for (key in keys) {
+            stubIndex
+                    .processElements(AllName.KEY, key, project, scope, NamedElement::class.java) { namedElement ->
+                if (namedElement is Call && CallDefinitionClause.`is`(namedElement) &&
+                        (incompleteCode || nameArityRange(namedElement)?.arityRange?.contains(arity) == true)) {
+                    resolveResults.add(PsiElementResolveResult(namedElement, validResult))
                 }
 
-                val arity = element.resolvedFinalArity()
-                val resolveResults = mutableListOf<ResolveResult>()
-                // results are never valid because the qualifier is unknown
-                val validResult = false
-
-                for (key in keys) {
-                    StubIndex.getInstance().processElements(AllName.KEY, key, project, GlobalSearchScope.allScope(project), NamedElement::class.java) { namedElement ->
-                        if (namedElement is Call &&
-                                org.elixir_lang.psi.CallDefinitionClause.`is`(namedElement) &&
-                                (incompleteCode || nameArityRange(namedElement)?.arityRange?.contains(arity) == true)) {
-                            resolveResults.add(PsiElementResolveResult(namedElement, validResult))
-                        }
-
-                        true
-                    }
-                }
-
-                resolveResults
-            } else {
-                Logger.error(Callable::class.java, "Don't know how to calculate name for relativeIdentifier in qualified element", element)
-
-                emptyList()
+                true
             }
         }
+
+        return resolveResults
     }
 }
