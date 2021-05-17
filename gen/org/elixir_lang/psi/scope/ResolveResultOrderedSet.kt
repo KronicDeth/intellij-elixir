5d211192dccc925085c006071a1df1709a0110b5
Luke Imhoff <Kronic.Deth@gmail.com>
2021-05-17 14:07:54 -0500

    Don't prefer source over decompiled in ResolveResultOrderedSet
    Preference is now handled in `Resolver`, so that source preference only happens on Go To Declaration and not for `multiResolve` because that needs decompiled Modulars for compile-time `for` loops that are too complex to infer.

--- gen/org/elixir_lang/psi/scope/ResolveResultOrderedSet.kt
+++ gen/org/elixir_lang/psi/scope/ResolveResultOrderedSet.kt
@@ -8,57 +8,24 @@ import org.elixir_lang.navigation.isDecompiled
 class ResolveResultOrderedSet {
    fun add(element: PsiElement, name: String, validResult: Boolean) {
       if (element !in psiElementSet) {
-         val existingDecompiledPsiElementResolveResultList = decompiledPsiElementResolveResultListByName[name]
-         val decompiled = element.isDecompiled()
+         psiElementSet.add(element)
+         val psiElementResolveResult = PsiElementResolveResult(element, validResult)
+         val existingDecompiledPsiElementResolveResultList = psiElementResolveResultListByName[name]
 
          if (existingDecompiledPsiElementResolveResultList != null) {
-            val existingDecompiled = existingDecompiledPsiElementResolveResultList.decompiled
-
-            if (existingDecompiled) {
-               val psiElementResolveResult = PsiElementResolveResult(element, validResult)
-
-               // add more decompiled
-               if (decompiled) {
-                  existingDecompiledPsiElementResolveResultList.psiElementResolveResultList.add(psiElementResolveResult)
-                  psiElementSet.add(element)
-
-               }
-               // favor a source of the same name
-               else {
-                  val psiElemetResolveResultList = mutableListOf(psiElementResolveResult)
-                  val decompiledPsiElemetResolveResultList = DecompiledPsiElemetResolveResultList(decompiled, psiElemetResolveResultList)
-                  decompiledPsiElementResolveResultListByName[name] = decompiledPsiElemetResolveResultList
-                  psiElementSet.add(element)
-
-                  existingDecompiledPsiElementResolveResultList.psiElementResolveResultList.forEach { existingPsiElementResolveResult ->
-                     psiElementSet.remove(existingPsiElementResolveResult.element)
-                  }
-               }
-            } else {
-               // ignore decompiled since source is favored
-
-               // add more source
-               if (!decompiled) {
-                  val psiElementResolveResult = PsiElementResolveResult(element, validResult)
-                  existingDecompiledPsiElementResolveResultList.psiElementResolveResultList.add(psiElementResolveResult)
-                  psiElementSet.add(element)
-               }
-            }
+            existingDecompiledPsiElementResolveResultList.add(psiElementResolveResult)
          } else {
-            psiElementSet.add(element)
             nameOrder.add(name)
 
-            val psiElementResolveResult = PsiElementResolveResult(element, validResult)
-            val psiElemetResolveResultList = mutableListOf(psiElementResolveResult)
-            val decompiledPsiElemetResolveResultList = DecompiledPsiElemetResolveResultList(decompiled, psiElemetResolveResultList)
-            decompiledPsiElementResolveResultListByName[name] = decompiledPsiElemetResolveResultList
+            val psiElementResolveResultList = mutableListOf(psiElementResolveResult)
+            psiElementResolveResultListByName[name] = psiElementResolveResultList
          }
       }
    }
 
    fun addAll(other: ResolveResultOrderedSet) {
       other.nameOrder.forEach { name ->
-         other.decompiledPsiElementResolveResultListByName[name]!!.psiElementResolveResultList.forEach { psiElementResolveResult ->
+         other.psiElementResolveResultListByName[name]!!.forEach { psiElementResolveResult ->
             add(psiElementResolveResult.element, name, psiElementResolveResult.isValidResult)
          }
       }
@@ -67,21 +34,19 @@ class ResolveResultOrderedSet {
    fun keepProcessing(incompleteCode: Boolean): Boolean = incompleteCode || !hasValidResult()
 
    fun toTypedArray(): Array<ResolveResult> = toList().toTypedArray()
-           
+
    fun toList(): List<ResolveResult> =
            nameOrder
                    .flatMap { name ->
-                      decompiledPsiElementResolveResultListByName[name]!!.psiElementResolveResultList
+                      psiElementResolveResultListByName[name]!!
                    }
 
    private val psiElementSet = mutableSetOf<PsiElement>()
-   private val decompiledPsiElementResolveResultListByName = mutableMapOf<String, DecompiledPsiElemetResolveResultList>()
+   private val psiElementResolveResultListByName = mutableMapOf<String, MutableList<PsiElementResolveResult>>()
    private val nameOrder = mutableListOf<String>()
 
    private fun hasValidResult() =
-           decompiledPsiElementResolveResultListByName.values.any {
-              it.psiElementResolveResultList.any { it.isValidResult }
+           psiElementResolveResultListByName.values.any {
+              it.any { it.isValidResult }
            }
 }
-
-private data class DecompiledPsiElemetResolveResultList(val decompiled: Boolean, val psiElementResolveResultList: MutableList<PsiElementResolveResult>)
