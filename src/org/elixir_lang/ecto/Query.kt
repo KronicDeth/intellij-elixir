b38d08a6a44f4817b9e00a338ba5838137bfdd48
Elle Imhoff <Kronic.Deth@gmail.com>
2021-07-12 20:38:18 -0500

    hints cannot declare reference variables

--- src/org/elixir_lang/ecto/Query.kt
+++ src/org/elixir_lang/ecto/Query.kt
@@ -181,8 +181,9 @@ object Query {
                     "having", "or_having", "or_where", "where" ->
                         executeOnHavingOrWhereSelect(fromKeywords.keywordValue, state, keepProcessing)
                     // Cannot declare a reference variable
-                    "as", "distinct", "except", "except_all", "group_by", "intersect", "intersect_all", "limit", "lock",
-                    "offset", "on", "prefix", "preload", "union", "union_all", "update", "windows" -> true
+                    "as", "distinct", "except", "except_all", "hints", "group_by", "intersect", "intersect_all",
+                    "limit", "lock", "offset", "on", "prefix", "preload", "union", "union_all", "update",
+                    "windows" -> true
                     else -> {
                         Logger.error(logger, "Don't know how to find reference variables for keyword key $keywordKeyText", fromKeywords)
 
