baa2810ffe5cb01e4dd604f7101830757b831b7e
Elle Imhoff <Kronic.Deth@gmail.com>
2021-08-30 20:46:06 -0500

    Go To Implementation from individual functions in defimpl

--- src/org/elixir_lang/DefinitionsScopedSearch.kt
+++ src/org/elixir_lang/DefinitionsScopedSearch.kt
@@ -2,14 +2,14 @@ package org.elixir_lang
 
 import com.intellij.openapi.application.QueryExecutorBase
 import com.intellij.psi.PsiElement
+import com.intellij.psi.ResolveState
 import com.intellij.util.Processor
 import com.intellij.psi.search.searches.DefinitionsScopedSearch
-import org.elixir_lang.navigation.item_presentation.Implementation
-import org.elixir_lang.psi.Protocol
-import org.elixir_lang.psi.QualifiableAlias
+import org.elixir_lang.psi.*
 import org.elixir_lang.psi.call.Call
+import org.elixir_lang.psi.impl.call.macroChildCallList
 import org.elixir_lang.psi.impl.maybeModularNameToModulars
-import org.elixir_lang.psi.outerMostQualifiableAlias
+import org.elixir_lang.structure_view.element.CallDefinitionClause.Companion.enclosingModularMacroCall
 
 class DefinitionsScopedSearch : QueryExecutorBase<PsiElement, DefinitionsScopedSearch.SearchParameters>(/* readAction = */ true) {
     override fun processQuery(queryParameters: DefinitionsScopedSearch.SearchParameters, consumer: Processor<in PsiElement>) {
@@ -28,6 +28,27 @@ class DefinitionsScopedSearch : QueryExecutorBase<PsiElement, DefinitionsScopedS
     private fun processQuery(call: Call, consumer: Processor<in PsiElement>) {
         if (Protocol.`is`(call)) {
             Protocol.processImplementations(call, consumer)
+        } else if (CallDefinitionClause.`is`(call)) {
+            enclosingModularMacroCall(call)?.let { modularCall ->
+                CallDefinitionClause.nameArityInterval(call, ResolveState.initial())?.let { protocolNameArityInterval ->
+                    if (Protocol.`is`(modularCall)) {
+                        Protocol.processImplementations(modularCall) { defimpl ->
+                            for (defimplChild in (defimpl as Call).macroChildCallList()) {
+                                if (CallDefinitionClause.`is`(defimplChild)) {
+                                    CallDefinitionClause.nameArityInterval(defimplChild, ResolveState.initial())?.let { implNameArityInterval ->
+                                        if (implNameArityInterval.name == protocolNameArityInterval.name &&
+                                                implNameArityInterval.arityInterval.overlaps(protocolNameArityInterval.arityInterval)) {
+                                            consumer.process(defimplChild)
+                                        }
+                                    }
+                                }
+                            }
+
+                            true
+                        }
+                    }
+                }
+            }
         }
     }
 }
