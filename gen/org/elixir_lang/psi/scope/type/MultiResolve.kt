bcda43966c021e7fa3721327c4a4336bb44a744d
Elle Imhoff <Kronic.Deth@gmail.com>
2021-10-03 01:36:23 -0500

    Resolve type parameters used in inline anonymous function types

--- gen/org/elixir_lang/psi/scope/type/MultiResolve.kt
+++ gen/org/elixir_lang/psi/scope/type/MultiResolve.kt
@@ -6,8 +6,15 @@ import com.intellij.psi.ResolveState
 import com.intellij.psi.util.PsiTreeUtil
 import org.elixir_lang.errorreport.Logger
 import org.elixir_lang.psi.*
+import org.elixir_lang.psi.call.Call
+import org.elixir_lang.psi.call.qualification.Qualified
 import org.elixir_lang.psi.impl.ElixirPsiImplUtil
+import org.elixir_lang.psi.impl.call.finalArguments
+import org.elixir_lang.psi.impl.stripAccessExpression
+import org.elixir_lang.psi.operation.Pipe
+import org.elixir_lang.psi.operation.Type
 import org.elixir_lang.psi.scope.ResolveResultOrderedSet
+import org.elixir_lang.psi.scope.WhileIn.whileIn
 import org.elixir_lang.reference.Type.Companion.typeHead
 
 class MultiResolve
@@ -15,47 +22,91 @@ private constructor(private val name: String,
                     private val arity: Int,
                     private val incompleteCode: Boolean) : org.elixir_lang.psi.scope.Type() {
     override fun executeOnType(definition: AtUnqualifiedNoParenthesesCall<*>, state: ResolveState): Boolean =
-        typeHead(definition)
-                ?.let { typeHead ->
-                    typeHead.functionName()?.let { name ->
-                        if (name.startsWith(this.name)) {
-                            val arity = typeHead.resolvedFinalArity()
-                            val validResult = name == this.name && arity == this.arity
+            typeHead(definition)
+                    ?.let { executeOnTypeHead(definition, it, state) }
+                    ?: true
 
-                            resolveResultOrderedSet.add(definition, typeHead.text, validResult, state.visitedElementSet())
+    private fun executeOnTypeHead(definition: Call, typeHead: Call, state: ResolveState): Boolean =
+            executeOnTypeHeadArguments(typeHead, state)
+                    && executeOnTypeHeadName(definition, typeHead, state)
 
-                            keepProcessing()
-                        } else {
-                            null
-                        }
-                    }
+    private fun executeOnTypeHeadArguments(typeHead: Call, state: ResolveState): Boolean =
+            typeHead.finalArguments()?.let { executeOnTypeHeadArguments(it, state) }
+                    ?: true
+
+    private fun executeOnTypeHeadArguments(arguments: Array<PsiElement>, state: ResolveState): Boolean =
+            whileIn(arguments) {
+                executeOnParameter(it, state)
+            }
+
+    private fun executeOnTypeHeadName(definition: Call, typeHead: Call, state: ResolveState): Boolean =
+            typeHead.functionName()?.let { name ->
+                if (name.startsWith(this.name)) {
+                    val arity = typeHead.resolvedFinalArity()
+                    val validResult = name == this.name && arity == this.arity
+
+                    resolveResultOrderedSet.add(definition, typeHead.text, validResult, state.visitedElementSet())
+
+                    keepProcessing()
+                } else {
+                    null
                 }
-                ?: true
+            } ?: true
 
     override fun executeOnParameter(parameter: PsiElement, state: ResolveState): Boolean =
-        if (this.arity == 0) {
-            val name = when (parameter) {
-                is UnqualifiedNoArgumentsCall<*> -> parameter.name
-                is ElixirKeywordKey -> parameter.text
-                else -> {
-                    Logger.error(MultiResolve::class.java, "DOn't know how to get name of parameter", parameter)
+            when (parameter) {
+                is ElixirAccessExpression -> executeOnParameter(parameter.stripAccessExpression(), state)
+                is ElixirParentheticalStab ->
+                    parameter.stab?.stabOperationList?.singleOrNull()
+                            ?.leftOperand().let { it as? ElixirStabNoParenthesesSignature }
+                            ?.noParenthesesArguments?.noParenthesesOneArgument
+                            ?.children
+                            ?.let { anonymousFunctionParameters ->
 
-                    null
+                                whileIn(anonymousFunctionParameters) {
+                                    executeOnParameter(it, state)
+                                }
+                            } ?: true
+                is UnqualifiedNoArgumentsCall<*> -> executeOnParameter(parameter, parameter.name, state)
+                // A qualified type cannot be declared here
+                is Qualified,
+                // Parenthetical calls must be a usage instead of a declaration.
+                is UnqualifiedParenthesesCall<*>,
+                // Literals
+                is ElixirAtom,
+                is ElixirAtomKeyword,
+                is ElixirList,
+                is ElixirMapOperation,
+                is ElixirTuple -> true
+                is ElixirKeywordKey -> executeOnParameter(parameter, parameter.text, state)
+                is Pipe -> (parameter.leftOperand()?.let { executeOnParameter(it, state) } ?: true)
+                        && (parameter.rightOperand()?.let { executeOnParameter(it, state) } ?: true)
+                is Type -> {
+                    val nameElement = parameter.leftOperand()
+
+                    if (nameElement != null) {
+                        executeOnParameter(nameElement, state)
+                    } else {
+                        true
+                    }
+                }
+                else -> {
+                    Logger.error(MultiResolve::class.java, "Don't know how to get name of parameter", parameter)
+
+                    true
                 }
             }
 
-            if (name != null && name.startsWith(this.name)) {
+    private fun executeOnParameter(parameter: PsiElement, name: String?, state: ResolveState): Boolean =
+            if (this.arity == 0 && name != null && name.startsWith(this.name)) {
                 val validResult = name == this.name
 
                 resolveResultOrderedSet.add(parameter, name, validResult, state.visitedElementSet())
 
                 keepProcessing()
             } else {
-                null
+                true
             }
-        } else {
-            null
-        } ?: true
 
     override fun keepProcessing(): Boolean = resolveResultOrderedSet.keepProcessing(incompleteCode)
 
