27fefcc57f1a5dea4791dbc92bf400b28fc2dbf9
Elle Imhoff <Kronic.Deth@gmail.com>
2022-05-07 21:11:46 -0500

    Log unknown element for fetching docs from BEAM file
    Fixes #2593

--- src/org/elixir_lang/documentation/BeamDocsHelper.kt
+++ src/org/elixir_lang/documentation/BeamDocsHelper.kt
@@ -4,50 +4,55 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveState
 import org.elixir_lang.beam.Beam
 import org.elixir_lang.beam.psi.Module
+import org.elixir_lang.errorreport.Logger
 import org.elixir_lang.psi.AtUnqualifiedNoParenthesesCall
 import org.elixir_lang.psi.call.MaybeExported
 import org.elixir_lang.utils.ElixirModulesUtil.erlangModuleNameToElixir
 
 object BeamDocsHelper {
     fun fetchDocs(element: PsiElement): FetchedDocs? = Beam
-            .from(element.containingFile.originalFile.virtualFile)
-            ?.let { beam ->
-                beam.atoms()?.moduleName()?.let { erlangModuleNameToElixir(it) }?.let { module ->
-                    when (element) {
-                        is Module -> beam.documentation()?.moduleDocs?.englishDocs?.let { moduleDoc ->
-                            FetchedDocs.ModuleDocumentation(module, moduleDoc)
-                        }
-                        is MaybeExported -> {
-                            element.exportedName()?.let { name ->
-                                val arity = element.exportedArity(ResolveState.initial())
+        .from(element.containingFile.originalFile.virtualFile)
+        ?.let { beam ->
+            beam.atoms()?.moduleName()?.let { erlangModuleNameToElixir(it) }?.let { module ->
+                when (element) {
+                    is Module -> beam.documentation()?.moduleDocs?.englishDocs?.let { moduleDoc ->
+                        FetchedDocs.ModuleDocumentation(module, moduleDoc)
+                    }
+                    is MaybeExported -> {
+                        element.exportedName()?.let { name ->
+                            val arity = element.exportedArity(ResolveState.initial())
 
-                                beam.documentation()?.docs?.let { docs ->
-                                    docs.documented("function", name, arity)?.let { documented ->
+                            beam.documentation()?.docs?.let { docs ->
+                                docs.documented("function", name, arity)?.let { documented ->
 
-                                        val signatures = documented.signatures
-                                        val deprecated = documented.deprecated()
-                                        val doc = documented.doc
+                                    val signatures = documented.signatures
+                                    val deprecated = documented.deprecated()
+                                    val doc = documented.doc
 
-                                        if (deprecated != null || doc != null) {
-                                            FetchedDocs.FunctionOrMacroDocumentation(
-                                                    module,
-                                                    deprecated,
-                                                    doc,
-                                                    impls = emptyList(),
-                                                    specs = emptyList(),
-                                                    heads = signatures
-                                            )
-                                        } else {
-                                            null
-                                        }
+                                    if (deprecated != null || doc != null) {
+                                        FetchedDocs.FunctionOrMacroDocumentation(
+                                            module,
+                                            deprecated,
+                                            doc,
+                                            impls = emptyList(),
+                                            specs = emptyList(),
+                                            heads = signatures
+                                        )
+                                    } else {
+                                        null
                                     }
                                 }
                             }
                         }
-                        // types are only generated for builtins, so no docs
-                        is AtUnqualifiedNoParenthesesCall<*> -> null
-                        else -> TODO()
+                    }
+                    // types are only generated for builtins, so no docs
+                    is AtUnqualifiedNoParenthesesCall<*> -> null
+                    else -> {
+                        Logger.error(BeamDocsHelper.javaClass, "Don't know how to fetch docs", element)
+
+                        null
                     }
                 }
             }
+        }
 }
