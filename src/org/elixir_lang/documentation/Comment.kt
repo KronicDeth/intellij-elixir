214729aaffd963ad66e92c4cd9641f114b9451c0
Elle Imhoff <Kronic.Deth@gmail.com>
2022-07-26 12:40:25 -0500

    Resolve owner of docs as following call definition clause.

--- src/org/elixir_lang/documentation/Comment.kt
+++ src/org/elixir_lang/documentation/Comment.kt
@@ -7,9 +7,12 @@ import com.intellij.psi.impl.FakePsiElement
 import com.intellij.psi.tree.IElementType
 import org.elixir_lang.errorreport.Logger
 import org.elixir_lang.psi.AtUnqualifiedNoParenthesesCall
+import org.elixir_lang.psi.CallDefinitionClause
 import org.elixir_lang.psi.ElixirTypes
+import org.elixir_lang.psi.call.Call
 import org.elixir_lang.psi.impl.enclosingMacroCall
 import org.elixir_lang.psi.impl.identifierName
+import org.elixir_lang.psi.impl.siblingExpressions
 import org.elixir_lang.psi.stub.type.call.Stub.isModular
 
 class Comment(val moduleAttribute: AtUnqualifiedNoParenthesesCall<*>) : FakePsiElement(), PsiDocCommentBase {
@@ -20,6 +23,11 @@ class Comment(val moduleAttribute: AtUnqualifiedNoParenthesesCall<*>) : FakePsiE
     override fun getOwner(): PsiElement? =
         when (val identifierName = moduleAttribute.atIdentifier.identifierName()) {
             "moduledoc" -> moduleAttribute.enclosingMacroCall().takeIf(::isModular)
+            "doc" -> {
+                moduleAttribute.siblingExpressions(forward = true, withSelf = false).firstOrNull { expression ->
+                    expression is Call && CallDefinitionClause.`is`(expression)
+                }
+            }
             else -> {
                 Logger.error(javaClass, "Don't know how to calculate owner", moduleAttribute)
 
