98d8ea11dc50c1735a2b840e1d66e7b4e2283d95
Elle Imhoff <Kronic.Deth@gmail.com>
2023-03-10 14:11:13 -0600

    Filter out Elixir SDKs that don't have an Erlang SDK when importing projects
    Fixes #2918

--- src/org/elixir_lang/mix/project/_import/Provider.java
+++ src/org/elixir_lang/mix/project/_import/Provider.java
@@ -1,47 +1,46 @@
 package org.elixir_lang.mix.project._import;
 
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
-import com.intellij.ide.util.projectWizard.ProjectJdkForModuleStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportBuilder;
 import com.intellij.projectImport.ProjectImportProvider;
+import org.elixir_lang.mix.project._import.step.ElixirSdkForModuleStep;
 import org.elixir_lang.mix.project._import.step.Root;
 import org.elixir_lang.mix.project._import.step.SelectOtpApps;
-import org.elixir_lang.sdk.elixir.Type;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Provider extends ProjectImportProvider {
-  @Override
-  public ModuleWizardStep[] createSteps(@NotNull WizardContext context){
-    return new ModuleWizardStep[]{
-        new ProjectJdkForModuleStep(context, Type.getInstance()),
-        new Root(context),
-        new SelectOtpApps(context)
-    };
-  }
+    @Override
+    public ModuleWizardStep[] createSteps(@NotNull WizardContext context) {
+        return new ModuleWizardStep[]{
+                new ElixirSdkForModuleStep(context),
+                new Root(context),
+                new SelectOtpApps(context)
+        };
+    }
 
-  @Override
-  protected ProjectImportBuilder doGetBuilder() {
-    return ProjectImportBuilder.EXTENSIONS_POINT_NAME.findExtensionOrFail(Builder.class);
-  }
+    @Override
+    protected ProjectImportBuilder doGetBuilder() {
+        return ProjectImportBuilder.EXTENSIONS_POINT_NAME.findExtensionOrFail(Builder.class);
+    }
 
-  @Override
-  protected boolean canImportFromFile(VirtualFile file) {
-    // todo: import project from file(mix.exs)
-    //    return "mix.exs".equals(file.getName());
-    return false;
-  }
+    @Override
+    protected boolean canImportFromFile(VirtualFile file) {
+        // todo: import project from file(mix.exs)
+        //    return "mix.exs".equals(file.getName());
+        return false;
+    }
 
-  @Override
-  public String getPathToBeImported(VirtualFile file) {
-    return file.getPath();
-  }
+    @Override
+    public String getPathToBeImported(VirtualFile file) {
+        return file.getPath();
+    }
 
-  @Nullable
-  @Override
-  public String getFileSample() {
-    return "<b>Mix</b> ex-script file (mix.exs)";
-  }
+    @Nullable
+    @Override
+    public String getFileSample() {
+        return "<b>Mix</b> ex-script file (mix.exs)";
+    }
 }
