f7ebb3b84e108983dc3ef5b47cf7a62d4f3c60ab
Ashley Sommer <ashleysommer@gmail.com>
2024-07-16 04:42:30 +1000

    Add compatibility for 2024.1 IDEs (#3569)
    * Update gradlewrapper to v7.6.4
    ./gradlew wrapper --gradle-version=7.6.4
    To fix gradle issue: https://github.com/gradle/gradle/issues/27156
    
    * Remove use of `FileUtil.FILE_HASHING_STRATEGY` from Intellij FileUtil, it was removed in 2024.1. (Note, this is only used in the jps-build test suite).
    This also removes references to Trove THashSet, and no longer stores File directly in sets or collections.
    See https://github.com/JetBrains/intellij-community/commit/560e8fc36b7b889b0cd20b079560c374e86aa5c0
    
    * Add support for 2024.1 IDEs (and runs tests correctly against 2024.1)
    
    * Update usages group wording for 2024.1
    
    * Fix more sdk configuration commits in application RW thread, fixes compatibility with IDEs v2024.1
    
    * Fix whitespace in tests due to 2024.1 change
    
    I believe 2024.1 changed how the Usages work.
    
    In 2023.x:
    ```kotlin
    val usages =
    myFixture.testFindUsagesUsingAction("module_attribute_usage.ex",
    "kernel.ex")
    .map { it as UsageInfo2UsageAdapter }
    ```
    
    In the debugger shows:
    ```
    0 = {ReadWriteAccessUsageInfo2UsageAdapter@19239} "4|def usage, |do|:
    |@|module_attribute"
    1 = {ReadWriteAccessUsageInfo2UsageAdapter@19240} "2|@|module_attribute|
    |1"
    ```
    
    For 2024.1, this shows:
    ```
    0 = {ReadWriteAccessUsageInfo2UsageAdapter@19421} "2|
    |@module_attribute| 1"
    1 = {ReadWriteAccessUsageInfo2UsageAdapter@19422} "4|  def usage, do:
    |@module_attribute"
    ```
    
    I believe it not shows the whitespace for the file, where previously it
    didn't.
    
    * pin versions
    
    * use BasePlatformTestCase to stop warning
    
    * add do block match test
    
    * set 241.0 as the version, to fix certain intellij warnings
    
    * fix key
    
    * revert tests
    
    * use thread
    
    ---------
    
    Co-authored-by: Josh Taylor <joshuataylorx@gmail.com>

--- tests/org/elixir_lang/code_insight/highlighting/brace_matcher/Issue443.java
+++ tests/org/elixir_lang/code_insight/highlighting/brace_matcher/Issue443.java
@@ -1,21 +1,23 @@
 package org.elixir_lang.code_insight.highlighting.brace_matcher;
 
+import com.intellij.codeInsight.highlighting.BraceMatcher;
 import com.intellij.codeInsight.highlighting.BraceMatchingUtil;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.highlighter.HighlighterIterator;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import org.elixir_lang.ElixirFileType;
 
-public class Issue443 extends LightPlatformCodeInsightFixtureTestCase {
+import static com.intellij.codeInsight.highlighting.BraceMatchingUtil.getBraceMatcher;
+
+public class Issue443 extends BasePlatformTestCase {
     /*
      * Tests
      */
 
     public void testDoBlock() {
         myFixture.configureByFile("do_block.ex");
-
         assertTrue("`do` not matched to `end`", isLBraceTokenBrace());
     }
 
