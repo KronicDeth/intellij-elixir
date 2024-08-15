00a49fc5eeebf73d902796dd301b8f00701fdf2b
Josh Taylor <joshuataylorx@gmail.com>
2024-08-15 20:23:22 +0800

    Support sha, depth, subdir for mix.exs files

--- src/org/elixir_lang/mix/Dep.kt
+++ src/org/elixir_lang/mix/Dep.kt
@@ -49,7 +49,7 @@ data class Dep(val application: String, val path: String, val type: Type = Type.
                                     "app", "branch", "commit", "compile", "env", "git", "github", "hex", "manager",
                                     "only", "optional", "organization", "override", "ref", "repo", "runtime",
                                     GUARDIAN_RUNTIME_TYPO, "sparse", "submodules", "system_env", "tag", "targets",
-                                    EDELIVER_DISTILLERY_WARN_MISSING -> acc
+                                    EDELIVER_DISTILLERY_WARN_MISSING, "sha", "depth", "subdir" -> acc
 
                                     "in_umbrella" -> acc.copy(path = "apps/$name", type = Type.MODULE)
                                     "path" -> putPath(acc, keywordPair.keywordValue)
