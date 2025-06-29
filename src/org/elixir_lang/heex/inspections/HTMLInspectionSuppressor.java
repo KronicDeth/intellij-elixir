cc8a6b1ab7a95281d27b6e2eb07aaafb17a1058d
Simon J <2857218+mwnciau@users.noreply.github.com>
2025-06-29 10:04:30 +0100

    Add HTML inspection suppressor for HEEx to ignore warning for HEEx compnents.

---
+++ src/org/elixir_lang/heex/inspections/HTMLInspectionSuppressor.java
@@ -0,0 +1,45 @@
+package org.elixir_lang.heex.inspections;
+
+import com.intellij.codeInspection.InspectionSuppressor;
+import com.intellij.codeInspection.SuppressQuickFix;
+import com.intellij.codeInspection.htmlInspections.HtmlUnknownTagInspection;
+import com.intellij.codeInspection.htmlInspections.RequiredAttributesInspection;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.util.CheckEmptyTagInspection;
+import org.elixir_lang.heex.Language;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class HTMLInspectionSuppressor implements InspectionSuppressor {
+    public static final List<String> SUPPRESSED_INSPECTIONS = List.of(
+        new CheckEmptyTagInspection().getSuppressId(),
+        new HtmlUnknownTagInspection().getSuppressId()
+    );
+
+
+    public boolean isSuppressedFor(PsiElement element, String toolId) {
+        if (!SUPPRESSED_INSPECTIONS.contains(toolId)) {
+            return false;
+        }
+
+        PsiFile file = element.getContainingFile();
+        if (file != null && file.getViewProvider().hasLanguage(Language.INSTANCE)) {
+            XmlTag xmlTag = PsiTreeUtil.getParentOfType(element, XmlTag.class, false);
+
+            // Tag names that contain dots are HEEx components
+            return xmlTag != null && xmlTag.getName().contains(".");
+        }
+
+        return false;
+    }
+
+    @Override
+    public SuppressQuickFix @NotNull [] getSuppressActions(@Nullable PsiElement psiElement, @NotNull String s) {
+        return SuppressQuickFix.EMPTY_ARRAY;
+    }
+}
