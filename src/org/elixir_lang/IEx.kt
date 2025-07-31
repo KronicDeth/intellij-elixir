13acfdb2d7e303edaf76e2ddf9421bc32bd01f34
Steve Hall <sh41@users.noreply.github.com>
2025-08-01 00:35:07 +0100

    Add correct iex cli parameters for elixir v1.17+ (#3698)
    Fixes #3673

--- src/org/elixir_lang/IEx.kt
+++ src/org/elixir_lang/IEx.kt
@@ -8,15 +8,15 @@ import org.elixir_lang.Elixir.prependNewCodePaths
 
 object IEx {
     fun commandLine(
-            environment: Map<String, String>,
-            workingDirectory: String?,
-            elixirSdk: Sdk,
-            erlArgumentList: kotlin.collections.List<String>
+        environment: Map<String, String>,
+        workingDirectory: String?,
+        elixirSdk: Sdk,
+        erlArgumentList: kotlin.collections.List<String>
     ): GeneralCommandLine {
         /* `iex` is an alternative mode of the `elixir` script, which changes the `erl` options, so the state needs to
-           be built on top of `erl`, not `elixir`. */
+         * be built on top of `erl`, not `elixir`. */
         val erlangSdk = elixirSdkToEnsuredErlangSdk(elixirSdk)
-        val commandLine = org.elixir_lang.Erl.commandLine(true, environment, workingDirectory, erlangSdk)
+        val commandLine = Erl.commandLine(true, environment, workingDirectory, erlangSdk)
         prependNewCodePaths(commandLine, elixirSdk, erlangSdk)
         commandLine.addParameters("-elixir", "ansi_enabled", "true")
         commandLine.addParameters(erlArgumentList)
@@ -31,20 +31,30 @@ object IEx {
 
         val elixirVersion = elixirSdk.versionString?.let { Version.parseVersion(it) }
         if (elixirVersion?.lessThan(1, 15, 0) == true) {
-            /* Pre Elixir 1.15.0, IEx entrypoint was as Elixir.IEx.CLI start() */
+            /* Pre Elixir 1.15.0, IEx entrypoint was as Elixir.IEx.CLI start()
+             * Pattern: erl -pa <multiple-paths> -noshell -user Elixir.IEx.CLI -extra --no-halt +iex */
             commandLine.addParameters("-user", "Elixir.IEx.CLI")
             commandLine.addParameter("-extra")
         } else if (elixirVersion?.`is`(1, 15, 0) == true) {
             /* Weird case for 1.15.0-rc1 to 1.15.0
-             * It had a bugged start_iex that needs to be specified differently. */
+             * It had a bugged start_iex that needs to be specified differently.
+             * Pattern: erl -pa <multiple-paths> -noshell -user Elixir.IEx.CLI -extra --no-halt +iex
+             */
             commandLine.addParameters("-s", "elixir", "start_cli")
             commandLine.addParameters("-user", "elixir")
             commandLine.addParameter("-extra")
             commandLine.addParameters("-e", ":elixir.start_iex()")
-        } else {
-            /* Case for elixir 1.16.0+ (and 1.15.1+ from the 1.15 branch).
-             * We can call start_iex directly from elixir entrypoint. */
+        } else if (elixirVersion?.lessThan(1, 17, 0) == true) {
+            /* Elixir 1.15.1 - 1.16.x series (e.g., 1.15.1, 1.16.3)
+             * Pattern: erl -noshell -elixir_root elixir/lib -pa elixir/lib/elixir/ebin -s elixir start_iex -user elixir -extra --no-halt +iex
+             */
             commandLine.addParameters("-s", "elixir", "start_iex")
+            commandLine.addParameters("-user", "elixir")
+            commandLine.addParameter("-extra")
+        } else { // elixirVersion.isAtLeast(1, 17, 0)
+            /* Elixir 1.17.x - 1.19.x and potentially future versions if the pattern continues
+             * Pattern: erl -noshell -elixir_root elixir/lib -pa elixir/lib/elixir/ebin -user elixir -extra --no-halt +iex
+             */
             commandLine.addParameters("-user", "elixir")
             commandLine.addParameter("-extra")
         }
