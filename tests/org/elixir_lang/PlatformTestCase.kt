eff1767edbc4ea991840a08386cdc685a8508546
Steve Hall <sh41@users.noreply.github.com>
2025-11-27 22:38:29 +0000

    Allow tests to access testData

--- tests/org/elixir_lang/PlatformTestCase.kt
+++ tests/org/elixir_lang/PlatformTestCase.kt
@@ -1,14 +1,20 @@
 package org.elixir_lang
 
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.elixir_lang.injection.markdown.Injector
+import java.nio.file.Path
 
 abstract class PlatformTestCase : BasePlatformTestCase() {
 
     @Throws(Exception::class)
     override fun setUp() {
         super.setUp()
+
+        // Allow access to testData directory in tests
+        val testDataPath = Path.of(myFixture.testDataPath).toAbsolutePath().toString()
+        VfsRootAccess.allowRootAccess(myFixture.testRootDisposable, testDataPath)
 
         // Enable literal sigil injection for tests to maintain backward compatibility
         Registry.get(Injector.REG_KEY_ENABLE_LITERAL_SIGIL_INJECTION).setValue(true)
