85ccd9b81409e624bd1c7882e367ddce79195d71
Steve Hall <sh41@users.noreply.github.com>
2025-12-02 20:59:34 +0000

    Resolve `application library named XXX already exists` exception
    java.lang.IllegalStateException: application library named Erlang for Elixir 24 already exists
    	at com.intellij.workspaceModel.ide.impl.legacyBridge.library.GlobalLibraryTableDelegate.createLibrary$intellij_platform_projectModel_impl(GlobalLibraryTableDelegate.kt:155)
    	at com.intellij.workspaceModel.ide.impl.legacyBridge.library.GlobalLibraryTableBridgeImpl.createLibrary(GlobalLibraryTableBridgeImpl.kt:61)
    	at org.elixir_lang.facet.sdks.Configurable$addListeners$listener$1.sdkAdded$lambda$1$0(Configurable.kt:126)

--- src/org/elixir_lang/facet/sdks/Configurable.kt
+++ src/org/elixir_lang/facet/sdks/Configurable.kt
@@ -123,8 +123,9 @@ abstract class Configurable: SearchableConfigurable, com.intellij.openapi.option
             override fun sdkAdded(sdk: Sdk) {
                 LibraryTablesRegistrar.getInstance().libraryTable.let { libraryTable ->
                     ApplicationManager.getApplication().runWriteAction {
-                        libraryTable.createLibrary(sdk.name).modifiableModel.apply {
-                            addRoots(sdk)
+                            val library = libraryTable.getLibraryByName(sdk.name) ?: libraryTable.createLibrary(sdk.name)
+                            library.modifiableModel.apply {
+                                replaceRoots(sdk)
                             commit()
                         }
                     }
@@ -203,7 +204,7 @@ abstract class Configurable: SearchableConfigurable, com.intellij.openapi.option
 
 private fun Library.ModifiableModel.addRoots(roots: Array<VirtualFile>) =
         roots.forEach {
-            addRoot(it, com.intellij.openapi.roots.OrderRootType.CLASSES)
+            addRoot(it, OrderRootType.CLASSES)
         }
 
 private fun Library.ModifiableModel.clearRoots() {
