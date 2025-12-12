92e4dc838e4877397fbc97862c391224cd1bd5eb
Josh Taylor <joshuataylorx@gmail.com>
2025-12-13 02:10:14 +0800

    Tweak OpenProcessor to work around EDT issues with openProjectAsync

--- src/org/elixir_lang/mix/project/OpenProcessor.kt
+++ src/org/elixir_lang/mix/project/OpenProcessor.kt
@@ -1,25 +1,49 @@
 package org.elixir_lang.mix.project
 
 import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.projectImport.ProjectImportBuilder
 import com.intellij.projectImport.ProjectOpenProcessorBase
-import org.elixir_lang.mix.Project
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.elixir_lang.mix.Project as MixProject
 import org.elixir_lang.mix.project._import.Builder
+import com.intellij.openapi.project.Project as IdeaProject
 
+private val LOG = logger<OpenProcessor>()
 /**
  * Created by zyuyou on 15/7/1.
  */
 class OpenProcessor : ProjectOpenProcessorBase<Builder>() {
-    override val supportedExtensions = arrayOf(Project.MIX_EXS)
+    override val supportedExtensions = arrayOf(MixProject.MIX_EXS)
 
     override fun doGetBuilder(): Builder = ProjectImportBuilder.EXTENSIONS_POINT_NAME.findExtensionOrFail(Builder::class.java)
 
+    override suspend fun openProjectAsync(
+        virtualFile: VirtualFile,
+        projectToClose: IdeaProject?,
+        forceOpenInNewFrame: Boolean
+    ): IdeaProject? {
+        // Ensure EDT context for runBlockingModalWithRawProgressReporter in IntelliJ 2025.3
+        // See: https://github.com/JetBrains/intellij-community/commit/59da423bd3fef56a7929838fb9bfeee5beae8785
+        return withContext(Dispatchers.EDT) {
+            super.openProjectAsync(virtualFile, projectToClose, forceOpenInNewFrame)
+        }
+    }
+
     override fun doQuickImport(file: VirtualFile, wizardContext: WizardContext): Boolean {
+        LOG.debug("doQuickImport(file=${file.path})")
+
         val projectRoot = file.parent
         wizardContext.projectName = projectRoot.name
         builder.setProjectRoot(projectRoot)
-        // Return false to avoid the problematic runBlockingModalWithRawProgressReporter in IntelliJ 2025.3
-        return false
+
+        // Trigger deferred scanning and auto-select all found OTP apps
+        val foundApps = builder.list
+        builder.setList(foundApps)
+
+        return true
     }
 }
