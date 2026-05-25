ba4be258bd23e11ae30e87d501735d3d6f92f22a
Steve Hall <sh41@users.noreply.github.com>
2026-05-25 14:31:42 +0100

    fix(mix deps): fix starting mix deps from notification
    The action won't run if the notification isn't visible. Start the action *before* hiding the notification.

--- src/org/elixir_lang/notification/mix_deps/InstallAction.kt
+++ src/org/elixir_lang/notification/mix_deps/InstallAction.kt
@@ -9,15 +9,15 @@ import org.elixir_lang.notification.setup_sdk.Notifier
 
 class InstallAction() : NotificationAction("Install deps") {
     override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+        val action = ActionManager.getInstance().getAction("Elixir.InstallMixDependencies")
+        if (action != null) {
+            ActionUtil.performAction(action, e)
+        }
         val project = e.project
         if (project != null) {
             Notifier.clearMixDepsOutdated(project)
         } else {
             notification.expire()
-        }
-        val action = ActionManager.getInstance().getAction("Elixir.InstallMixDependencies")
-        if (action != null) {
-            ActionUtil.performAction(action, e)
         }
     }
 }
